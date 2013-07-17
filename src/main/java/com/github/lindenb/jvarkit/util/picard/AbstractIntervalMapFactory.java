aab577f543fa00ac2f26255392c7990ab7d1a5bb
Pierre Lindenbaum <plindenbaum@yahoo.fr>
2013-07-17 17:20:37 +0200

    many things, updating code, moving sources...

---
+++ src/main/java/com/github/lindenb/jvarkit/util/picard/AbstractIntervalMapFactory.java
@@ -0,0 +1,108 @@
+package com.github.lindenb.jvarkit.util.picard;
+
+import net.sf.picard.util.Interval;
+import net.sf.samtools.SAMSequenceDictionary;
+
+import com.github.lindenb.jvarkit.lang.Function;
+
+
+
+public abstract class AbstractIntervalMapFactory<T>
+	{
+	
+	private Function<String[],T> funValue=null;
+	private Function<String[],Interval> funKey=new DefaultIntervalFunction();
+	private SAMSequenceDictionary samSequenceDictionary=null;
+	
+	public static class DefaultIntervalFunction
+		implements Function<String[],Interval>
+		{
+		private int chromCol=0;
+		private int startCol=1;
+		private int endCol=2;
+		public DefaultIntervalFunction()
+			{
+			this(0,1,2);
+			}
+		
+		public DefaultIntervalFunction(int chromCol,int startCol,int endCol)
+			{
+			this.chromCol=chromCol;
+			this.startCol=startCol;
+			this.endCol=endCol;
+			}
+		@Override
+		public Interval apply(String[] param)
+			{
+			if(	chromCol>=param.length ||
+				startCol>=param.length ||
+				endCol>=param.length) return null;
+			
+			return new Interval(
+					param[0],
+					Integer.parseInt(param[1]),
+					Integer.parseInt(param[2])
+					);
+			}
+		}
+	
+	protected AbstractIntervalMapFactory()
+		{
+		
+		}
+
+	
+	public void setValueFunction(Function<String[],T> fun)
+		{
+		this.funValue=fun;
+		}
+	
+	public Function<String[],T> getValueFunction()
+		{
+		return this.funValue;
+		}
+	
+	public void setKeyFunction(Function<String[],Interval> fun)
+		{
+		this.funKey=fun;
+		}
+	
+	public Function<String[],Interval> getKeyFunction()
+		{
+		return this.funKey;
+		}
+	
+	public SAMSequenceDictionary getSamSequenceDictionary()
+		{
+		return samSequenceDictionary;
+		}
+	
+	public void setSamSequenceDictionary(
+			SAMSequenceDictionary samSequenceDictionary)
+		{
+		this.samSequenceDictionary = samSequenceDictionary;
+		}
+	
+	protected int compareIntervals(Interval i1,Interval i2)
+		{
+		int i=compareChromosomes(i1.getSequence(),i2.getSequence());
+		if(i!=0) return i;
+		i=i1.getStart()-i2.getStart();
+		if(i!=0) return i;
+		i=i1.getEnd()-i2.getEnd();
+		return i;
+		}
+
+	
+	protected int compareChromosomes(String c1,String c2)
+		{
+		SAMSequenceDictionary d;
+		if((d=getSamSequenceDictionary())!=null)
+			{
+			return d.getSequenceIndex(c1)-d.getSequenceIndex(c2);
+			}
+		return c1.compareTo(c2);
+		}
+	
+	
+	}
---
+++ src/main/java/com/github/lindenb/jvarkit/util/picard/TribbleFactory.java
@@ -0,0 +1,214 @@
+package com.github.lindenb.jvarkit.util.picard;
+
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.Comparator;
+import java.util.regex.Pattern;
+
+import net.sf.picard.util.Interval;
+import net.sf.samtools.util.CloseableIterator;
+import net.sf.samtools.util.SortingCollection;
+
+import org.broad.tribble.index.Index;
+import org.broad.tribble.index.IndexFactory;
+import org.broad.tribble.readers.LineReader;
+import org.broad.tribble.readers.PositionalBufferedStream;
+import org.broad.tribble.util.LittleEndianOutputStream;
+
+import org.broad.tribble.AbstractFeatureReader;
+import org.broad.tribble.Feature;
+import org.broad.tribble.FeatureCodecHeader;
+import org.broad.tribble.Tribble;
+
+
+public class TribbleFactory<T>
+	extends AbstractIntervalMapFactory<T>
+	{
+	public class MyFeature<X>
+		implements Feature
+		{
+		private Interval interval;
+		private X value;
+		public MyFeature(Interval interval,X value)
+			{
+			this.interval=interval;
+			this.value=value;
+			}
+		@Override
+		public String getChr() {
+			return interval.getSequence();
+			}
+		@Override
+		public int getStart() {
+			return interval.getStart();
+			}
+		@Override
+		public int getEnd() {
+			return interval.getEnd();
+			}
+		public X getValue()
+			{
+			return value;
+			}
+		}
+	
+	@SuppressWarnings("rawtypes")
+	private class MyCodec
+		implements org.broad.tribble.FeatureCodec<MyFeature>
+			{
+			private Pattern tab=Pattern.compile("[\t]");
+			@Override
+			public boolean canDecode(String f) {
+				return f.endsWith(".tsv");
+				}
+			@Override
+			public Class<MyFeature> getFeatureType()
+				{
+				return MyFeature.class;
+				}
+			private String readLine(PositionalBufferedStream stream) throws IOException
+				{
+				StringBuilder sb=null; 
+				int c;
+				while((c=stream.read())!=-1 && c!='\n')
+					{
+					if(sb==null) sb=new StringBuilder();
+					sb.append((char)c);
+					}	
+				return sb==null?null:sb.toString();
+				}
+			@Override
+			public MyFeature<T> decode(PositionalBufferedStream stream)
+				throws IOException
+				{
+				for(;;)
+					{
+					String line=readLine(stream);
+					if(line==null) return null;
+					String tokens[]=tab.split(line);
+					Interval interval=getKeyFunction().apply(tokens);
+					if(interval==null) continue;
+					T value=getValueFunction().apply(tokens);
+					if(value==null) continue;
+					return new MyFeature<T>(interval, value);
+					}
+				}
+			@Override
+			public Feature decodeLoc(PositionalBufferedStream stream)
+				throws IOException {
+				return decode(stream);
+				}
+			@Override
+			public FeatureCodecHeader readHeader(PositionalBufferedStream stream)
+				throws IOException {
+				return FeatureCodecHeader.EMPTY_HEADER;
+				}
+			}
+	
+	private int maxRecordsInRAM=1000000;
+	private static class StringCodec extends  AbstractDataCodec<String>
+		{
+		@Override
+		public String decode(DataInputStream dis) throws IOException 
+			{
+			try
+				{
+				return dis.readUTF();
+				}
+			catch(IOException err)
+				{
+				return null;
+				}
+			}
+		@Override
+		public void encode(DataOutputStream dos, String object)
+				throws IOException {
+			dos.writeUTF(object);
+			}
+		@Override
+		public StringCodec clone() {
+			return new StringCodec();
+			}
+		}
+	
+	private class StringCmp implements Comparator<String>
+		{
+		@Override
+		public int compare(String o1, String o2)
+			{
+			Pattern tab=Pattern.compile("[\t]");
+			Interval i1=getKeyFunction().apply(tab.split(o1));
+			Interval i2=getKeyFunction().apply(tab.split(o2));
+			int i=compareChromosomes(i1.getSequence(),i2.getSequence());
+			if(i!=0) return i;
+			i=i1.getStart()-i2.getStart();
+			if(i!=0) return i;
+			i=i1.getEnd()-i2.getEnd();
+			return i;
+			}
+		}
+	
+	public File createIndex(LineReader r,File file)  throws IOException
+		{
+		Pattern tab=Pattern.compile("[\t]");
+		SortingCollection<String> sorting=SortingCollection.newInstance(
+				String.class,
+				new StringCodec(),
+				new StringCmp(),
+				maxRecordsInRAM
+				);
+		sorting.setDestructiveIteration(true);
+		String line;
+		while((line=r.readLine())!=null)
+	    	{
+			 String tokens[]=tab.split(line);
+	         Interval interval=getKeyFunction().apply(tokens);
+	         T value=getValueFunction().apply(tokens);
+	         if(value==null) continue;
+	         if(interval==null) continue;
+	         sorting.add(line);
+	    	}
+		sorting.doneAdding();
+		
+		//save to file
+		
+		CloseableIterator<String> iter=sorting.iterator();
+		PrintWriter pw=new PrintWriter(file);
+		while(iter.hasNext())
+			{
+			pw.println(iter.next());
+			}
+		pw.flush();
+		pw.close();
+		iter.close();
+		sorting.cleanup();
+		
+		
+		MyCodec codec=new MyCodec();
+		File indexFile=Tribble.indexFile(file);
+		Index index=IndexFactory.createDynamicIndex(file, codec);
+		
+		LittleEndianOutputStream out=new LittleEndianOutputStream(new FileOutputStream(indexFile));
+		index.write(out);
+		out.close();
+		return indexFile;
+		}
+	
+	public AbstractFeatureReader<MyFeature<T>> getTribble(File baseFile)
+		{
+		File indexFile=Tribble.indexFile(baseFile);
+		Index index=IndexFactory.loadIndex(indexFile.toString());
+		@SuppressWarnings("unchecked")
+		AbstractFeatureReader<MyFeature<T>> reader= AbstractFeatureReader.getFeatureReader(
+				baseFile.getAbsolutePath(),
+				new MyCodec(),
+				index
+		         );
+		return reader;
+		}
+
+	}
