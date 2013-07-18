483c2c8b340c33753e6b45dfdc1577803c01135e
Pierre Lindenbaum <plindenbaum@yahoo.fr>
2013-07-18 10:23:15 +0200

    vcf bed

--- README.md
+++ README.md
@@ -579,4 +579,37 @@ chr1	69270	.	A	G	2694.18	MEMBRANE	AC=40;AF=1.000;AN=40;DP=83;Dels=0.00;EFF=SYNON
 
 ```
 
+<h3>VCFBed</h3>
+<h4>Motivation</h4>
+Annotate a VCF with the content of a BED file indexed with tabix
+<h4>Compilation</h4>
+```bash
+ant vcfbed
+```
+<h4>Options</h4>
+<table>
+<tr><th>Option</th><th>Description</th></tr>
+<tr><td>FORMAT=String</td><td>format. Field with ${number} will be replaced with the column of the BED.  Default value: ${1}:${2}-${3}. This option can be set to 'null' to clear the default value. </td></tr>
+<tr><td>TABIXFILE=String</td><td>BED file indexed with tabix  Required. </td></tr>
+<tr><td>TAG=String</td><td>Key for the INFO field  Default value: TAG. This option can be set to 'null' to clear the default value. </td></tr>
+<tr><td>IN=String</td><td>VCF file/URL to process. Default stdin.   Default value: null. </td></tr>
+<tr><td>OUT=File</td><td>VCF file to generate. Default stdout.   Default value: null. </td></tr>
+</table>
+
+<h4>Example</h4>
+Map the NCBI biosystems to a BED file using the following script:   
+<script src="https://gist.github.com/6024788.js"></script>
+
+Annotate a VCF with the data of NCBI biosystems.
+```bash
+curl "https://raw.github.com/arq5x/gemini/master/test/test1.snpeff.vcf" |\
+ sed 's/^chr//' |\
+ java -jar  TABIXFILE=~/ncbibiosystem.bed.gz TAG=NCBIBIOSYS  FMT='($4|$5|$6|$7)' |\
+ grep -E '(^#CHR|NCBI)'
+
+##INFO=<ID=NCBIBIOSYS,Number=.,Type=String,Description="metadata added from /home/lindenb/ncbibiosystem.bed.gz . Format was ($4|$5|$6|$7)">
+#CHROM	POS	ID	REF	ALT	QUAL	FILTER	INFO	FORMAT	1094PC0005	1094PC0009	1094PC0012	1094PC0013
+1	69270	.	A	G	2694.18	.	AC=40;AF=1.000;AN=40;DP=83;Dels=0.00;EFF=SYNONYMOUS_CODING(LOW|SILENT|tcA/tcG|S60|305|OR4F5|protein_coding|CODING|ENST00000335137|exon_1_69091_70008);FS=0.000;HRun=0;HaplotypeScore=0.0000;InbreedingCoeff=-0.0598;MQ=31.06;MQ0=0;NCBIBIOSYS=(79501|119548|40|GPCR_downstream_signaling),(79501|106356|30|Signaling_by_GPCR),(79501|498|40|Olfactory_transduction),(79501|83087|60|Olfactory_transduction),(79501|477114|30|Signal_Transduction),(79501|106383|50|Olfactory_Signaling_Pathway);QD=32.86	GT:AD:DP:GQ:PL	./.	./.	1/1:0,3:3:9.03:106,9,0	1/1:0,6:6:18.05:203,18,0
+1	69511	.	A	G	77777.27	.	AC=49;AF=0.875;AN=56;BaseQRankSum=0.150;DP=2816;DS;Dels=0.00;EFF=NON_SYNONYMOUS_CODING(MODERATE|MISSENSE|Aca/Gca|T141A|305|OR4F5|protein_coding|CODING|ENST00000335137|exon_1_69091_70008);FS=21.286;HRun=0;HaplotypeScore=3.8956;InbreedingCoeff=0.0604;MQ=32.32;MQ0=0;MQRankSum=1.653;NCBIBIOSYS=(79501|119548|40|GPCR_downstream_signaling),(79501|106356|30|Signaling_by_GPCR),(79501|498|40|Olfactory_transduction),(79501|83087|60|Olfactory_transduction),(79501|477114|30|Signal_Transduction),(79501|106383|50|Olfactory_Signaling_Pathway);QD=27.68;ReadPosRankSum=2.261	GT:AD:DP:GQ:PL	./.	./.	0/1:2,4:6:15.70:16,0,40	0/1:2,2:4:21.59:22,0,40
+``
 
--- src/main/java/com/github/lindenb/jvarkit/tools/vcfbed/VCFBed.java
+++ src/main/java/com/github/lindenb/jvarkit/tools/vcfbed/VCFBed.java
@@ -23,15 +23,17 @@ import org.broadinstitute.variant.vcf.VCFInfoHeaderLine;
 import com.github.lindenb.jvarkit.util.AbstractVCFFilter;
 
 
-
-
-
+/**
+ * 
+ * VCFBed
+ *
+ */
 public class VCFBed extends AbstractVCFFilter
 	{
 	@Usage(programVersion="1.0")
-	public String USAGE=getStandardUsagePreamble()+" Fix samtools indels (for @SolenaLS).";
+	public String USAGE=getStandardUsagePreamble()+" Cross information between a VCF and a BED .";
 
-	@Option(shortName="FMT",doc="format",optional=true)
+	@Option(shortName="FMT",doc="format. Field with ${number} will be replaced with the column of the BED.",optional=true)
 	public String FORMAT="${1}:${2}-${3}";
 	
 	@Option(shortName="TBX",doc="BED file indexed with tabix",optional=false)
@@ -50,7 +52,7 @@ public class VCFBed extends AbstractVCFFilter
 	private static class PlainChunk extends Chunk
 		{
 		String s;
-		PlainChunk(String s){ this.s=s;}
+		PlainChunk(String s){this.s=s;}
 		public String toString(String tokens[])
 			{
 			return s+(next==null?"":next.toString(tokens));
@@ -67,33 +69,40 @@ public class VCFBed extends AbstractVCFFilter
 		}
 
 	
-	private Chunk parseFormat(String s,int i)
+	private Chunk parseFormat(String s)
 		{
-		if(i>=s.length()) return null;
+		if(s==null || s.isEmpty()) return null;
 		if(s.startsWith("${"))
 			{
 			int j=s.indexOf('}',2);
-			if(j==-1) throw new IllegalArgumentException("bad format");
-			int col=Integer.parseInt(s.substring(i+2, j).trim());
-			if(col<1) throw new IllegalArgumentException();
-			ColChunk c=new ColChunk(col-1);
-			c.next=parseFormat(s, j+1);
-			return c;
+			if(j==-1) throw new IllegalArgumentException("bad format in \""+s+"\".");
+			try
+				{
+				int col=Integer.parseInt(s.substring(2, j).trim());
+				if(col<1) throw new IllegalArgumentException();
+				ColChunk c=new ColChunk(col-1);
+				c.next=parseFormat(s.substring(j+1));
+				return c;
+				}
+			catch(Exception err)
+				{
+				 throw new IllegalArgumentException("bad format in \""+s+"\".",err);
+				}
 			}
-		else if(s.startsWith("${"))
+		else if(s.startsWith("$"))
 			{
-			int j=i+1;
+			int j=1;
 			while(j<s.length() && Character.isDigit(s.charAt(j)))
 				{
 				++j;
 				}
-			int col=Integer.parseInt(s.substring(i+1, j).trim());
+			int col=Integer.parseInt(s.substring(1, j).trim());
 			if(col<1) throw new IllegalArgumentException();
 			ColChunk c=new ColChunk(col-1);
-			c.next=parseFormat(s, j+1);
+			c.next=parseFormat(s.substring(j));
 			return c;
 			}
-		
+		int i=0;
 		StringBuilder sb=new StringBuilder();
 		while(i< s.length() && s.charAt(i)!='$')
 			{
@@ -101,7 +110,7 @@ public class VCFBed extends AbstractVCFFilter
 			i++;
 			}
 		PlainChunk c=new PlainChunk(sb.toString());
-		c.next=parseFormat(s,i);
+		c.next=parseFormat(s.substring(i));
 		return c;
 		}
 	
@@ -116,28 +125,39 @@ public class VCFBed extends AbstractVCFFilter
 			throws IOException
 		{
 		Pattern tab=Pattern.compile("[\t]");
-		Chunk parsedFormat=parseFormat(this.FORMAT,0);
+		Chunk parsedFormat=parseFormat(this.FORMAT);
 		if(parsedFormat==null)parsedFormat=new PlainChunk("");
 		
 		TabixReader tabix= new TabixReader(this.TABIXFILE);
+		
 		VCFCodec codeIn=new VCFCodec();		
 		VCFHeader header=(VCFHeader)codeIn.readHeader(in);
 		String line;
 		VCFHeader h2=new VCFHeader(header.getMetaDataInInputOrder(),header.getSampleNamesInOrder());
-		h2.addMetaDataLine(new VCFInfoHeaderLine(TAG, VCFHeaderLineCount.UNBOUNDED, VCFHeaderLineType.String, "metadata added from "+TABIXFILE));
+		h2.addMetaDataLine(new VCFInfoHeaderLine(TAG, VCFHeaderLineCount.UNBOUNDED, VCFHeaderLineType.String, "metadata added from "+TABIXFILE+" . Format was "+FORMAT));
 		w.writeHeader(h2);
 		while((line=in.readLine())!=null)
 			{
 			VariantContext ctx=codeIn.decode(line);
 			Set<String> annotations=new HashSet<String>();
 			String line2;
-			TabixReader.Iterator iter=tabix.query(ctx.getChr()+":"+ctx.getStart()+"-"+ctx.getEnd());
+			
+			
+			
+			String q=ctx.getChr()+":"+ctx.getStart()+"-"+(ctx.getEnd());
+			int reg[]=tabix.parseReg(q);
+			if(reg[0]==-1)
+				{
+				w.add(ctx);
+				continue;
+				}
+			TabixReader.Iterator iter=tabix.query(reg[0],reg[1],reg[2]);
 			while(iter!=null && (line2=iter.next())!=null)
 				{
 				String tokens[]=tab.split(line2);
 				String newannot=parsedFormat.toString(tokens);
 				if(!newannot.isEmpty())
-					annotations.add(newannot);
+					annotations.add(newannot.replaceAll("[ , ;=]","_"));
 				}
 			if(annotations.isEmpty())
 				{
@@ -145,7 +165,7 @@ public class VCFBed extends AbstractVCFFilter
 				continue;
 				}
 			VariantContextBuilder vcb=new VariantContextBuilder(ctx);
-			vcb.attribute(TAG, annotations);
+			vcb.attribute(TAG, annotations.toArray());
 			w.add(vcb.make());
 			}
 		tabix.close();
