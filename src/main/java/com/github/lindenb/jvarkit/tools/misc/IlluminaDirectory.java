5cbcf8273b7d9767ad75fef90e059a6574a3e4ad
Pierre Lindenbaum <plindenbaum@yahoo.fr>
2013-11-12 18:24:48 +0100

    reading from stdin

--- src/main/java/com/github/lindenb/jvarkit/tools/misc/IlluminaDirectory.java
+++ src/main/java/com/github/lindenb/jvarkit/tools/misc/IlluminaDirectory.java
@@ -1,6 +1,8 @@
 package com.github.lindenb.jvarkit.tools.misc;
 
+import java.io.BufferedReader;
 import java.io.File;
+import java.io.InputStreamReader;
 import java.io.PrintStream;
 import java.math.BigInteger;
 import java.security.MessageDigest;
@@ -23,7 +25,6 @@ import com.github.lindenb.jvarkit.util.AbstractCommandLineProgram;
 public class IlluminaDirectory extends AbstractCommandLineProgram
 	{
 	private int ID_GENERATOR=0;
-
 	
     
     private MessageDigest md5;
@@ -51,7 +52,6 @@ public class IlluminaDirectory extends AbstractCommandLineProgram
     
     private class Folder
     	{
-    	File folder;
     	SortedMap<String, Sample> sampleMap=new TreeMap<String, IlluminaDirectory.Sample>();
     	List<Pair> undtermined=new ArrayList<Pair>();
     	void scan(File f)
@@ -59,53 +59,41 @@ public class IlluminaDirectory extends AbstractCommandLineProgram
     		if(f==null) return;
     		if(!f.canRead()) return;
     		IlluminaDirectory.this.info("Scanning "+f);
-    		if(f.isDirectory())
-    			{
-    			File children[]=f.listFiles();
-    			if(children==null) return;
-    			for(File c:f.listFiles())
-    				{
-    				scan(c);
-    				}
-    			}
-    		else
-    			{
-    			if(f.getName().endsWith(".fastq.gz"))
-    				{
-    				FastQName fq=FastQName.parse(f);
-    				if(!fq.isValid())
-    					{
-    					warning("invalid name:"+fq);
-    					return;
-    					}
-    				if(fq.isUndetermined())
-    					{
-    					for(int i=0;i< undtermined.size();++i)
-	    	    			{
-	    	    			Pair p=undtermined.get(i);
-	    	    			if(p.complement(fq)) return;
-	    	    			}
-    					undtermined.add(new Pair(fq));
-    					}
-    				else
-    					{
-    					Sample sample=this.sampleMap.get(fq.getSample());
-    					if(sample==null)
-    						{
-    						sample=new Sample();
-    						sample.name=fq.getSample();
-    						this.sampleMap.put(sample.name,sample);
-    						}
-    					sample.add(fq);
-    					}
-    				}
-    			}
+    		
+			FastQName fq=FastQName.parse(f);
+			if(!fq.isValid())
+				{
+				warning("invalid name:"+fq);
+				return;
+				}
+			if(fq.isUndetermined())
+				{
+				for(int i=0;i< undtermined.size();++i)
+	    			{
+	    			Pair p=undtermined.get(i);
+	    			if(p.complement(fq)) return;
+	    			}
+				undtermined.add(new Pair(fq));
+				}
+			else
+				{
+				Sample sample=this.sampleMap.get(fq.getSample());
+				if(sample==null)
+					{
+					sample=new Sample();
+					sample.name=fq.getSample();
+					this.sampleMap.put(sample.name,sample);
+					}
+				sample.add(fq);
+				}
+				
+    			
     		}
     	
     	void json(PrintStream out)
     		{
     		boolean first=true;
-    		out.print("{\"directory\":\""+this.folder.getPath()+"\",\"samples\":[");
+    		out.print("{\"samples\":[");
     		for(Sample S:this.sampleMap.values())
 				{
     			if(!first) out.print(',');
@@ -126,7 +114,6 @@ public class IlluminaDirectory extends AbstractCommandLineProgram
     	void write(XMLStreamWriter w) throws XMLStreamException
     		{
     		w.writeStartElement("directory");
-    		w.writeAttribute("path", this.folder.getPath());
     		w.writeStartElement("samples");
     		for(Sample S:this.sampleMap.values())
     			{
@@ -350,30 +337,44 @@ public class IlluminaDirectory extends AbstractCommandLineProgram
 				case 'h': printUsage();return 0;
 				case 'v': System.out.println(getVersion());return 0;
 				case 'L': getLogger().setLevel(java.util.logging.Level.parse(getopt.getOptArg()));break;
+				case 'J': JSON=true;break;
 				case ':': System.err.println("Missing argument for option -"+getopt.getOptOpt());return -1;
-				case 'J': JSON=true; break;
 				default: System.err.println("Unknown option -"+getopt.getOptOpt());return -1;
 				}
 			}
-		if(getopt.getOptInd()==args.length)
+		
+		
+		if(getopt.getOptInd()!=args.length)
 			{
-			error("No directory given");
+			error("Expected to read filenames on stdin.");
 			return -1;
 			}
 				
+		
+		
+		
 		try
 			{
-
 			List<Folder> folders=new ArrayList<Folder>();
-	    	for(int i=getopt.getOptInd();i< args.length;++i)
-	    		{
-	    		File f=new File(args[i]);
-	    		if(!f.isDirectory()) throw new PicardException("Not a directory:"+f);
-	    		Folder folder=new Folder();
-	    		folder.folder=f;
-	    		folder.scan(f);
-	    		folders.add(folder);
-	    		}
+			Folder folder=new Folder();
+			folders.add(folder);
+			String line;
+			BufferedReader in=new BufferedReader(new InputStreamReader(System.in));
+			while((line=in.readLine())!=null)
+				{
+				if(line.isEmpty() || line.startsWith("#")) continue;
+				if(!line.endsWith(".fastq.gz"))
+					{
+					warning("ignoring "+line);
+					continue;
+					}
+				File f=new File(line);
+				if(!f.exists()) throw new PicardException("Doesn't exist:"+f);
+				if(!f.isFile()) throw new PicardException("Not a file:"+f);
+				folder.scan(f);
+				}
+			in.close();
+	    	
 	    	if(JSON)
 	    		{
 	    		System.out.print("[");
