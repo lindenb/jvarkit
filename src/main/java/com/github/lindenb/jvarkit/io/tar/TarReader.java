96c961fccc9dece8f90d9f72786bee1ed6b32e4e
Pierre Lindenbaum <plindenbaum@yahoo.fr>
2014-02-01 11:31:36 +0100

    cont

---
+++ src/main/java/com/github/lindenb/jvarkit/io/tar/TarReader.java
@@ -0,0 +1,314 @@
+package com.github.lindenb.jvarkit.io.tar;
+
+import java.io.Closeable;
+import java.io.IOException;
+import java.io.InputStream;
+import java.math.BigInteger;
+import java.util.Arrays;
+
+import com.github.lindenb.jvarkit.io.IOUtils;
+
+public class TarReader implements Closeable
+	{
+	private static final int BLOCK_SIZE=512;
+	private static final byte zeroBlock[]=new byte[BLOCK_SIZE];
+	{{{
+		Arrays.fill(zeroBlock, (byte)0);
+	}}}
+	
+	private static byte[] _malloc(int size)
+		{
+		return new byte[size];
+		}
+	/* Posix tar header*/
+	private  class TARFileHeader
+		{
+		byte name[]=_malloc(100);
+		byte mode[]=_malloc(8);
+		byte uid[]=_malloc(8);
+		byte gid[]=_malloc(8);
+		byte size[]=_malloc(12);
+		byte mtime[]=_malloc(12);
+		byte checksum[]=_malloc(8);
+		byte typeflag[]=_malloc(1);
+		byte linkname[]=_malloc(100);
+		byte magic[]=_malloc(6);
+		byte version[]=_malloc(2);
+		byte uname[]=_malloc(32);
+		byte gname[]=_malloc(32);
+		byte devmajor[]=_malloc(8);
+		byte devminor[]=_malloc(8);
+		byte prefix[]=_malloc(155);
+		byte pad[]=_malloc(12);
+		
+		public String getName()
+			{
+			int length=0;
+			while(length< name.length && name[length]!=0)
+				{
+				length++;
+				}
+			return new String(name,0,length);
+			}
+		
+		public long getSize()
+			{
+			return parseOctalOrBinary(size,0,size.length);
+			}
+		}
+	
+	public  class Entry
+		extends InputStream
+		{
+		private long length;
+		private long nRead=0L;
+		private TARFileHeader meta;
+		private Entry(TARFileHeader meta)
+			{
+			this.meta=meta;
+			this.length=meta.getSize();
+			}
+		public long getOffset()
+			{
+			return nRead;
+			}
+		public long getLength()
+			{
+			return length;
+			}
+		@Override
+		public int read() throws IOException {
+			if(nRead>=length) return -1;
+			int c=TarReader.this.in.read();
+			if(c==-1) return -1;
+			nRead++;
+			if(nRead==length)
+				{
+				finish();
+				}
+			return c;
+			}
+		
+		
+		private void finish() throws IOException
+			{
+			while(nRead%BLOCK_SIZE!=0)
+				{
+				if(TarReader.this.in.read()==-1) throw new IOException();
+				++nRead;
+				}
+			}
+		
+		public void skip() throws IOException
+			{
+			while(read()!=-1) ;
+			}
+		
+		public String getName()
+			{
+			return meta.getName();
+			}
+		
+		@Override
+		public void close()
+			{
+			}
+		@Override
+		public String toString() {
+			return getName()+" "+nRead+"/"+length;
+			}
+		}
+	
+	
+	private InputStream in=null;
+	public TarReader(InputStream in) throws IOException
+		{
+		this.in=in;
+		}
+	static int copy(byte array[],byte field[],int off)
+		{
+		System.arraycopy(array, off,field,0,field.length);
+		return off+field.length;
+		}
+	private TARFileHeader readTARFileHeader() throws IOException
+		{
+		byte array[]=new byte[BLOCK_SIZE];
+		int nRead=0;
+		while(nRead< BLOCK_SIZE)
+			{
+			int x=in.read(array,nRead,BLOCK_SIZE-nRead);
+				
+			if(x<0)
+				{
+				throw new IOException("cannot read bytes "+nRead+" / "+array.length);
+				}
+			nRead+=x;
+			}
+		if(Arrays.equals(array, zeroBlock)) return null;
+		TARFileHeader h=new TARFileHeader();
+		int off=0;
+		off=copy(array,h.name,off);
+		off=copy(array,h.mode,off);
+		off=copy(array,h.uid,off);
+		off=copy(array,h.gid,off);
+		off=copy(array,h.size,off);
+		off=copy(array,h.mtime,off);
+		off=copy(array,h.checksum,off);
+		off=copy(array,h.typeflag,off);
+		off=copy(array,h.linkname,off);
+		off=copy(array,h.magic,off);
+		off=copy(array,h.version,off);
+		off=copy(array,h.uname,off);
+		off=copy(array,h.gname,off);
+		off=copy(array,h.devmajor,off);
+		off=copy(array,h.devminor,off);
+		off=copy(array,h.prefix,off);
+		off=copy(array,h.pad,off);
+		if(off!=BLOCK_SIZE) throw new IOException();
+		return h;
+		}
+	@Override
+	public void close() throws IOException {
+		if(in!=null) in.close();
+		in=null;
+		}
+	
+	public Entry next() throws IOException
+		{
+		TARFileHeader h= readTARFileHeader();
+		if(h==null) return null;
+		return new Entry(h);
+		}	
+/* 
+-rw-r--r-- tm/tm      11255269 2014-01-23 14:20 citations.dmp
+-rw-r--r-- tm/tm       2768900 2014-01-23 14:20 delnodes.dmp
+-rw-r--r-- tm/tm           419 2014-01-23 14:20 division.dmp
+-rw-r--r-- tm/tm         11559 2014-01-23 14:20 gc.prt
+-rw-r--r-- tm/tm          3566 2014-01-23 14:20 gencode.dmp
+-rw-r--r-- tm/tm        554847 2014-01-23 14:20 merged.dmp
+-rw-r--r-- tm/tm      96596932 2014-01-23 14:20 names.dmp
+ */
+	public static void main(String[] args) {
+		try {
+			System.setProperty("ftp.proxyHost", "proxy-upgrade.univ-nantes.prive");
+			System.setProperty("ftp.proxyPort", "3128");
+			
+			TarReader r=new TarReader(IOUtils.openURIForReading("ftp://ftp.ncbi.nih.gov/pub/taxonomy/taxdump.tar.gz"));
+			Entry E=null;
+			while((E=r.next())!=null)
+				{
+				System.out.println(E.toString());
+				E.skip();
+				}
+			
+			r.close();
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		}
+	
+    private static long parseOctalOrBinary(final byte[] buffer, final int offset,
+            final int length) {
+			
+			if ((buffer[offset] & 0x80) == 0) {
+			return parseOctal(buffer, offset, length);
+			}
+			final boolean negative = buffer[offset] == (byte) 0xff;
+			if (length < 9) {
+			return parseBinaryLong(buffer, offset, length, negative);
+			}
+			return parseBinaryBigInteger(buffer, offset, length, negative);
+			}
+    
+    private static long parseOctal(final byte[] buffer, final int offset, final int length) {
+        long    result = 0;
+        int     end = offset + length;
+        int     start = offset;
+
+        if (length < 2){
+            throw new IllegalArgumentException("Length "+length+" must be at least 2");
+        }
+
+        if (buffer[start] == 0) {
+            return 0L;
+        }
+
+        // Skip leading spaces
+        while (start < end){
+            if (buffer[start] == ' '){
+                start++;
+            } else {
+                break;
+            }
+        }
+
+        // Must have trailing NUL or space
+        byte trailer;
+        trailer = buffer[end-1];
+        if (trailer == 0 || trailer == ' '){
+            end--;
+        } else {
+            throw new IllegalArgumentException("");
+        }
+        // May have additional NUL or space
+        trailer = buffer[end-1];
+        if (trailer == 0 || trailer == ' '){
+            end--;
+        }
+
+        for ( ;start < end; start++) {
+            final byte currentByte = buffer[start];
+            // CheckStyle:MagicNumber OFF
+            if (currentByte < '0' || currentByte > '7'){
+                throw new IllegalArgumentException("");
+            }
+            result = (result << 3) + (currentByte - '0'); // convert from ASCII
+            // CheckStyle:MagicNumber ON
+        }
+
+        return result;
+    }
+
+    
+    private static long parseBinaryLong(final byte[] buffer, final int offset,
+                final int length,
+                final boolean negative)
+			  {
+			if (length >= 9) {
+			throw new IllegalArgumentException("At offset " + offset + ", "
+			                       + length + " byte binary number"
+			                       + " exceeds maximum signed long"
+			                       + " value");
+			}
+			long val = 0;
+			for (int i = 1; i < length; i++) {
+			val = (val << 8) + (buffer[offset + i] & 0xff);
+			}
+			if (negative) {
+			// 2's complement
+			val--;
+			val ^= ((long) Math.pow(2, (length - 1) * 8) - 1);
+			}
+			return negative ? -val : val;
+			  }
+        private static long parseBinaryBigInteger(final byte[] buffer,
+                final int offset,
+                final int length,
+                final boolean negative) {
+				byte[] remainder = new byte[length - 1];
+				System.arraycopy(buffer, offset + 1, remainder, 0, length - 1);
+				BigInteger val = new BigInteger(remainder);
+				if (negative) {
+				// 2's complement
+				val = val.add(BigInteger.valueOf(-1)).not();
+				}
+				if (val.bitLength() > 63) {
+				throw new IllegalArgumentException("At offset " + offset + ", "
+				                 + length + " byte binary number"
+				                 + " exceeds maximum signed long"
+				                 + " value");
+				}
+				return negative ? -val.longValue() : val.longValue();
+				}
+
+	}
---
+++ src/main/java/com/github/lindenb/jvarkit/tools/treepack/TreePackApp.java
@@ -0,0 +1,501 @@
+package com.github.lindenb.jvarkit.tools.treepack;
+
+import java.awt.Rectangle;
+import java.awt.geom.Rectangle2D;
+import java.io.InputStream;
+import java.io.PrintStream;
+import java.text.NumberFormat;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import javax.xml.XMLConstants;
+import javax.xml.namespace.QName;
+import javax.xml.stream.XMLEventReader;
+import javax.xml.stream.XMLInputFactory;
+import javax.xml.stream.XMLOutputFactory;
+import javax.xml.stream.XMLStreamException;
+import javax.xml.stream.XMLStreamWriter;
+import javax.xml.stream.events.Attribute;
+import javax.xml.stream.events.StartElement;
+import javax.xml.stream.events.XMLEvent;
+
+import net.sf.samtools.util.CloserUtil;
+
+
+import com.github.lindenb.jvarkit.io.IOUtils;
+import com.github.lindenb.jvarkit.util.AbstractCommandLineProgram;
+import com.github.lindenb.jvarkit.util.Hershey;
+
+
+public class TreePackApp
+	extends AbstractCommandLineProgram
+	{
+	protected Rectangle viewRect=new Rectangle(1000,1000);
+	protected final String SVG="http://www.w3.org/2000/svg";
+	protected final String XLINK="http://www.w3.org/1999/xlink";
+	protected final String JVARKIT_NS="https://github.com/lindenb/jvarkit";
+	protected Node root=null;
+	protected Hershey hershey=new Hershey();   
+
+	
+	private class Node
+		implements TreePack
+		{
+		private Node parent=null;
+		private Rectangle2D bounds=new Rectangle2D.Double(-10,-10,10,10);
+		private String label="";
+		private Map<String,Node> children=null;
+		private Double weight=null;
+		
+		protected Node(Node parent)
+			{
+			this.parent=parent;
+			}
+		
+		
+		public final boolean isLeaf()
+			{
+			return children==null || children.isEmpty();
+			}
+		
+		protected String convertWeightToString()
+			{
+			return TreePackApp.this.convertWeightToString(getWeight());
+			}
+
+
+		
+		public String getPath()
+			{
+			StringBuilder b=new StringBuilder();
+			Node curr=this;
+			while(curr!=null)
+				{
+				if(b.length()!=0) b.insert(0,"/");
+				b.insert(0, curr.getLabel());
+				curr=curr.parent;
+				}
+			return b.toString();
+			}
+		
+		public String getLabel()
+			{
+			return this.label;
+			}
+		
+		
+		@Override
+		public void setBounds(Rectangle2D bounds)
+			{
+			this.bounds=bounds;
+			}
+		
+		public Rectangle2D getInsetBounds()
+			{
+			final double ratio=0.9;
+			final Rectangle2D r0=getBounds();
+			Rectangle2D.Double r1=new Rectangle2D.Double();
+			r1.width=r0.getWidth()*ratio;
+			r1.x=r0.getX()+(r0.getWidth()-r1.width)/2;
+			r1.height=r0.getHeight()*ratio;
+			r1.y=r0.getY()+(r0.getHeight()-r1.height)/2;
+			return r1;
+			}
+	
+		@Override
+		public Rectangle2D getBounds()
+			{
+			return this.bounds;
+			}
+	
+		public double getWeight()
+			{
+			if(isLeaf())
+				{
+				return this.weight==null?1.0:this.weight;
+				}
+			else
+				{
+				double N=0;
+				for(Node c:this.children.values())
+					{
+					if(c.getWeight()<=0) throw new IllegalStateException(getPath());
+					N+=c.getWeight();
+					}
+				return N;
+				}
+			}
+
+		protected Rectangle2D getTitleFrame()
+			{
+			Rectangle2D r=getBounds(); 
+			Rectangle2D.Double frame= new Rectangle2D.Double();
+			frame.height=r.getHeight()*0.1;
+			frame.y=r.getY();
+			frame.width=r.getWidth();
+			frame.x=r.getX();
+			return frame;
+			}
+		
+		protected Rectangle2D getChildrenFrame()
+			{
+			Rectangle2D r=getBounds(); 
+			Rectangle2D.Double frame= new Rectangle2D.Double();
+			frame.height=r.getHeight()*0.9;//yes again after
+			frame.y=r.getMaxY()-frame.height;
+			frame.height=r.getHeight()*0.85;//yes again 
+			frame.width=r.getWidth()*0.95;
+			frame.x=r.getX()+(r.getWidth()-frame.width)/2.0;
+			return frame;
+			}
+		
+		public void layout(TreePacker packer)
+			{
+			if(getWeight()<=0 || isLeaf()) return ;
+			List<TreePack> L=new ArrayList<TreePack>(children.values());
+			packer.layout(L, getChildrenFrame() );
+			for(Node c:children.values())
+				{
+				c.layout(packer);
+				}
+			}
+		
+		public int getDepth()
+			{
+			int d=0;
+			Node p=this;
+			while(p.parent!=null)
+				{
+				p=p.parent;
+				++d;
+				}
+			return d;
+			}
+		
+		
+		public void svg(XMLStreamWriter w) throws XMLStreamException
+		   {
+		   if(getWeight()<=0)
+			   {
+			   info("weight < 0 for "+getPath());
+			   return ;
+			   }
+		   final Rectangle2D bounds=this.getBounds();
+		   Rectangle2D insets=this.getInsetBounds();
+		   Rectangle2D frameUsed=bounds;
+		   
+		   
+		   if(bounds.getWidth()<=1 || bounds.getHeight()<=1)
+			   {
+			   return;
+			   }
+		   
+		   w.writeStartElement("svg","g",SVG);
+		   w.writeAttribute("title",getPath()+"="+convertWeightToString());
+		   w.writeAttribute("jvarkit",JVARKIT_NS,"path",getPath());
+		   w.writeAttribute("jvarkit",JVARKIT_NS,"weight",convertWeightToString());
+		  	if(!isLeaf())
+		   		{
+		   		w.writeAttribute("jvarkit",JVARKIT_NS,"count",""+ this.children.size());
+		   		}
+		   
+		   w.writeEmptyElement("svg", "rect", SVG);
+		   w.writeAttribute("class", "r"+(getDepth()%2));
+		   w.writeAttribute("x",String.valueOf(frameUsed.getX()));
+		   w.writeAttribute("y",String.valueOf(frameUsed.getY()));
+		   w.writeAttribute("width",String.valueOf(frameUsed.getWidth()));
+		   w.writeAttribute("height",String.valueOf(frameUsed.getHeight()));
+			   
+
+		   if(!isLeaf())
+			   {
+			   
+			   w.writeEmptyElement("svg", "path", SVG);
+			   w.writeAttribute("d",
+						hershey.svgPath(getLabel()+"="+convertWeightToString(),
+								this.getTitleFrame())
+						);
+			   }
+		   
+
+		   
+		   
+		   
+		   if(isLeaf())
+			   {
+			   	Rectangle2D f_up=new Rectangle2D.Double(
+			   			insets.getX(),insets.getY(),
+			   			insets.getWidth(),insets.getHeight()/2.0
+			   			);
+			   	w.writeEmptyElement("svg", "path", SVG);
+				w.writeAttribute("d",
+						hershey.svgPath(getLabel(),f_up)
+						);	
+				w.writeAttribute("class","lbla"+(getDepth()%2));
+
+				Rectangle2D f_down=new Rectangle2D.Double(
+						insets.getX(),insets.getCenterY(),
+						insets.getWidth(),insets.getHeight()/2.0
+			   			);
+				w.writeEmptyElement("svg", "path", SVG);
+				w.writeAttribute("d",
+						hershey.svgPath(convertWeightToString(),f_down)
+						);
+				w.writeAttribute("class","lblb"+(getDepth()%2));
+			   }
+		   else
+			   {
+			   for(Node child:children.values())
+				   {
+				   child.svg(w);
+				   }
+			   }
+		   
+		   w.writeEndElement();//g
+		   }
+		
+		}
+	
+
+	
+	protected String getCascadingStylesheet()
+		{
+		return "svg {fill:none;stroke:black;stroke-width:0.5px;}\n"+
+				".r0 {fill:none;stroke:black;stroke-width:0.5px;}\n"+
+				".r1 {fill:none;stroke:black;stroke-width:0.5px;}\n"+
+				".lbla0 {stroke:black;stroke-width:0.3px;}\n"+
+				".lblb0 {stroke:red;stroke-width:0.3px;}\n"+
+				".lbla1 {stroke:gray;stroke-width:0.3px;}\n"+
+				".lblb1 {stroke:red;stroke-width:0.3px;}\n"+
+				""
+				;
+		}
+	
+	protected void svg() throws XMLStreamException
+	  	{
+	  	info("writing svg");
+		XMLOutputFactory xmlfactory= XMLOutputFactory.newInstance();
+		XMLStreamWriter w= xmlfactory.createXMLStreamWriter(System.out,"UTF-8");
+		w.writeStartDocument("UTF-8","1.0");
+		w.writeStartElement("svg","svg",SVG);
+		
+		
+		
+		w.writeAttribute("xmlns", XMLConstants.XML_NS_URI, "svg", SVG);
+		w.writeAttribute("xmlns", XMLConstants.XML_NS_URI, "xlink", XLINK);
+		w.writeAttribute("xmlns", XMLConstants.XML_NS_URI, "jvarkit", JVARKIT_NS);
+		w.writeAttribute("width",String.valueOf(this.viewRect.getWidth()));
+		w.writeAttribute("height",String.valueOf(this.viewRect.getHeight()));
+		
+		
+		w.writeStartElement("svg","defs",SVG);
+		w.writeStartElement("svg","style",SVG);
+		w.writeAttribute("type","text/css");
+		w.writeCharacters(getCascadingStylesheet());
+		w.writeEndElement();//svg:style
+		w.writeEndElement();//svg:def
+
+		
+		w.writeStartElement("svg","title",SVG);
+		w.writeCharacters(getProgramName());
+		w.writeEndElement();
+		w.writeComment("Cmd-Line:"+getProgramCommandLine());
+		w.writeComment("Version "+getVersion());
+		root.svg(w);
+		w.writeEndElement();//svg
+		w.writeEndDocument();
+		w.flush();
+	  	}
+	
+	protected TreePackApp()
+		{
+		
+		}
+	
+	protected String convertWeightToString(double weight)
+		{
+		NumberFormat format=NumberFormat.getInstance();
+		return format.format((long)weight);
+		}
+	
+	protected String intervalToString(int value,int step)
+		{
+		value=((int)(value/(double)step));
+		return String.valueOf((int)(value)*step+"-"+((value+1)*step));
+		}
+
+	private void layout()
+		{
+		info("layout");
+		this.root.setBounds(new Rectangle2D.Double(0, 0, this.viewRect.getWidth(), this.viewRect.getHeight()));
+		TreePacker packer=new TreePacker();
+		this.root.layout(packer);
+		}
+	
+	private void skip(XMLEventReader r) throws XMLStreamException
+		{
+		while(r.hasNext())
+			{
+			XMLEvent evt=r.nextEvent();
+			if(evt.isStartElement())
+				{
+				skip(r);
+				}
+			else if(evt.isEndElement())
+				{
+				return;
+				}
+			}
+		throw new XMLStreamException();
+		}
+
+	private Node parseNode(XMLEventReader r,StartElement E,Node parent) throws XMLStreamException
+		{
+		Node node=new Node(parent);
+		Attribute att=E.getAttributeByName(new QName("label"));
+		node.label=(att!=null?att.getValue():"");
+		while(r.hasNext())
+			{
+			XMLEvent evt=r.nextEvent();
+			if(evt.isStartElement())
+				{
+				QName qName=evt.asStartElement().getName();
+				if(qName.getLocalPart().equals("node"))
+					{
+					Node child=parseNode(r,evt.asStartElement(),node);
+					if(node.children==null) node.children=new  HashMap<String,Node>();
+					node.children.put(node.label,child);
+					}
+				else
+					{
+					skip(r);
+					}
+				}
+			else if(evt.isEndElement())
+				{
+				if(node.children==null || node.children.isEmpty())
+					{
+					att=E.getAttributeByName(new QName("weight"));
+					if(att==null)
+						{
+						node.weight=1.0;
+						}
+					else
+						{
+						node.weight=Double.parseDouble(att.getValue());
+						if(node.weight<=0) throw new XMLStreamException("bad @weight:"+node.weight, E.getLocation());
+						}
+					}
+				return node;
+				}
+			}
+		throw new IllegalStateException();
+		}
+	
+	
+	private Node scan(InputStream in) throws XMLStreamException
+		{
+		Node n=null;
+		XMLInputFactory xif=XMLInputFactory.newFactory();
+		XMLEventReader r=xif.createXMLEventReader(in);
+		while(r.hasNext())
+			{
+			XMLEvent evt=r.nextEvent();
+			if(evt.isStartElement())
+				{
+				QName qName=evt.asStartElement().getName();
+				if(qName.getLocalPart().equals("node"))
+					{
+					n=parseNode(r,evt.asStartElement(),null);
+					}
+				}
+			}
+		r.close();
+		return n;
+		}
+	
+	@Override
+	public void printOptions(PrintStream out)
+		{
+		out.println(" -x {width}x{height} dimension of the output rectangle. default:"+this.viewRect.width+"x"+this.viewRect.height);
+		super.printOptions(out);
+		}
+	
+	
+	
+	@Override
+	public int doWork(String[] args)
+		{
+		com.github.lindenb.jvarkit.util.cli.GetOpt opt=new com.github.lindenb.jvarkit.util.cli.GetOpt();
+		int c;
+		while((c=opt.getopt(args,getGetOptDefault()+"x:"))!=-1)
+			{
+			switch(c)
+				{
+				case 'x':
+					{
+					String s=opt.getOptArg();
+					int x=s.indexOf('x');
+					if(x==-1)
+						{
+						error("'x' missing in "+s);
+						return -1;
+						}
+					this.viewRect.x=Integer.parseInt(s.substring(0, x));
+					this.viewRect.y=Integer.parseInt(s.substring(x+1));
+					if(viewRect.x<=0 ||this.viewRect.y<=0 )
+						{
+						error("bad viewRect "+viewRect);
+						return -1;
+						}
+					break;
+					}
+				default:
+					{
+					switch(handleOtherOptions(c, opt))
+						{
+						case EXIT_FAILURE: return -1;
+						case EXIT_SUCCESS: return 0;
+						default:break;
+						}
+					}
+				}
+			}
+		
+		
+		try
+			{
+			if(opt.getOptInd()==args.length)
+				{
+				info("Reading from stdin");
+				this.root=scan(System.in);
+				}
+			else if(opt.getOptInd()+1==args.length)
+				{
+				String filename=args[opt.getOptInd()];
+				info("Reading from "+filename);
+				InputStream in=IOUtils.openURIForReading(filename);
+				this.root=scan(in);
+				CloserUtil.close(in);
+				}
+			else
+				{
+				error("Illegal number of arguments.");
+				return -1;
+				}
+			layout();
+			return 0;
+			}
+		catch(Exception err)
+			{
+			error(err);
+			return -1;
+			}
+		finally
+			{
+
+			}
+		}
+	}
