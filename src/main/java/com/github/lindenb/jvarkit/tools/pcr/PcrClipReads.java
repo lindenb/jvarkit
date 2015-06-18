4ea3c3aa7e8870f34bd12b8337be9b976d38b9db
Pierre Lindenbaum <plindenbaum@yahoo.fr>
2015-06-18 23:06:45 +0200

    cont

--- src/main/java/com/github/lindenb/jvarkit/tools/pcr/PcrClipReads.java
+++ src/main/java/com/github/lindenb/jvarkit/tools/pcr/PcrClipReads.java
@@ -30,7 +30,6 @@ package com.github.lindenb.jvarkit.tools.pcr;
 
 import java.io.BufferedReader;
 import java.io.File;
-import java.io.IOException;
 import java.io.PrintStream;
 import java.util.ArrayList;
 import java.util.Collection;
