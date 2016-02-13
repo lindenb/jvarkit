d2af50e4a3308c33e52d86dec9d96a6393162970
Pierre Lindenbaum <plindenbaum@yahoo.fr>
2016-02-13 14:38:42 +0100

    cont

--- src/main/java/com/github/lindenb/jvarkit/tools/vcfannot/VCFStopCodon.java
+++ src/main/java/com/github/lindenb/jvarkit/tools/vcfannot/VCFStopCodon.java
@@ -17,8 +17,8 @@ import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Collection;
 import java.util.Comparator;
-import java.util.Map;
-import java.util.TreeMap;
+import java.util.HashSet;
+import java.util.Set;
 import java.util.regex.Pattern;
 
 import com.github.lindenb.jvarkit.io.IOUtils;
@@ -40,7 +40,12 @@ import htsjdk.samtools.util.IntervalTreeMap;
 import htsjdk.samtools.util.SortingCollection;
 import htsjdk.variant.variantcontext.Allele;
 import htsjdk.variant.variantcontext.VariantContext;
+import htsjdk.variant.variantcontext.VariantContextBuilder;
+import htsjdk.variant.variantcontext.writer.VariantContextWriter;
 import htsjdk.variant.vcf.VCFHeader;
+import htsjdk.variant.vcf.VCFHeaderLineCount;
+import htsjdk.variant.vcf.VCFHeaderLineType;
+import htsjdk.variant.vcf.VCFInfoHeaderLine;
 
 
 
@@ -52,31 +57,58 @@ import htsjdk.variant.vcf.VCFHeader;
 public class VCFStopCodon extends AbstractVCFStopCodon
 	{
 	private static final org.slf4j.Logger LOG = com.github.lindenb.jvarkit.util.log.Logging.getLog(VCFStopCodon.class);
-
-	private IntervalTreeMap<KnownGene> knownGenes=new IntervalTreeMap<KnownGene>();
+	/** known Gene collection */
+	private final IntervalTreeMap<KnownGene> knownGenes=new IntervalTreeMap<KnownGene>();
+	/** reference genome */
 	private IndexedFastaSequenceFile indexedFastaSequenceFile=null;
+	/** current genomic sequence */
 	private GenomicSequence genomicSequence=null;
+	/** all variants */
 	private SortingCollection<Variant> variants= null;
+	/** genetic Code used */
+	private static final GeneticCode GENETIC_CODE = GeneticCode.getStandard();
 	
+	/** mutated cdna */
 	private static class MutedSequence extends DelegateCharSequence
 		{
-		private Map<Integer, Character> pos2char=new TreeMap<Integer, Character>();
+		private int begin=-1;
+		private int end=-1;
+		private String newseq=null;
+	
 		MutedSequence(CharSequence wild)
 			{
 			super(wild);
 			}
 		
-		void put(int pos,char c)
+		void setMutation(int begin,int end,final String newseq)
 			{
-			if(pos2char.containsKey(pos)) throw new IllegalStateException();
-			this.pos2char.put(pos, c);
+			if(this.newseq!=null) throw new IllegalStateException();
+			this.newseq = newseq;
+			this.begin=begin;
+			this.end=end;
+			if(this.begin>this.end) throw new IllegalArgumentException();
+			if(this.end> getDelegate().length()) throw new IndexOutOfBoundsException();
 			}
-		
 		@Override
-		public char charAt(int i)
+		public int length() {
+			int L = getDelegate().length();
+			if(this.newseq!=null) {
+				L-=(this.end-this.begin);
+				L+=this.newseq.length();
+				}
+			return L;
+			}
+		@Override
+		public char charAt(int index)
 			{
-			Character c= pos2char.get(i);
-			return c==null?getDelegate().charAt(i):c;
+			if(this.newseq==null || this.begin<index) return getDelegate().charAt(index);
+			int idx2= index-this.begin;
+			if(idx2 < this.newseq.length())
+				{
+				return this.newseq.charAt(idx2);
+				}
+			idx2-=this.newseq.length();
+			return getDelegate().charAt(this.end+idx2);
 			}
 		
 		}
@@ -84,17 +116,15 @@ public class VCFStopCodon extends AbstractVCFStopCodon
 	
 	private static class ProteinCharSequence extends DelegateCharSequence
 		{
-		private GeneticCode geneticCode;
-		ProteinCharSequence(GeneticCode geneticCode,CharSequence cDNA)
+		ProteinCharSequence(final CharSequence cDNA)
 			{
 			super(cDNA);
-			this.geneticCode=geneticCode;
 			}
 		
 		@Override
 		public char charAt(int i)
 			{
-			return geneticCode.translate(
+			return GENETIC_CODE.translate(
 				getDelegate().charAt(i*3+0),
 				getDelegate().charAt(i*3+1),
 				getDelegate().charAt(i*3+2));
@@ -105,8 +135,10 @@ public class VCFStopCodon extends AbstractVCFStopCodon
 			{
 			return getDelegate().length()/3;
 			}
-		}
 		
+		}
+	
+	/** load KnownGenes */
 	private void loadKnownGenesFromUri() throws IOException
 		{
 		BufferedReader in = null;
@@ -146,17 +178,21 @@ public class VCFStopCodon extends AbstractVCFStopCodon
 	
 	
 	private static int ID_GENERATOR=0;
-	static private class Variant
-		{
+	
+	static private class AbstractContext {
 		String contig;
 		int genomicPosition1=0;
-		String transcriptName;
 		Allele refAllele;
 		Allele altAllele;
+		int sorting_id;
+		}
+	
+	static private class Variant extends AbstractContext
+		{
+		String transcriptName;
 		int position_in_cdna=-1;
 		String wildCodon=null;
 		String mutCodon=null;
-		int sorting_id;
 		
 		Variant()
 			{
@@ -172,6 +208,33 @@ public class VCFStopCodon extends AbstractVCFStopCodon
 			}
 		int positionInCodon() { return  position_in_cdna%3;}
 		int codonStart() { return this.position_in_cdna - this.positionInCodon();}
+		
+		public String getInfo(final Variant other) {
+			return String.join("|",
+					// common data
+					"CHROM",this.contig,
+					"POS",String.valueOf(genomicPosition1),
+					"REF",this.refAllele.getBaseString(),
+					"TRANSCRIPT",this.transcriptName,
+					"cDdnaPos",String.valueOf(this.position_in_cdna+1),
+					"CodonPos",String.valueOf(this.codonStart()+1),
+					"CodonWild",this.wildCodon,
+					"AAWild",new ProteinCharSequence(this.wildCodon).getString(),
+					
+					//data about this
+					"PosInCodon1",String.valueOf(this.positionInCodon()+1),
+					"Alt1",this.altAllele.getBaseString(),
+					"Codon1",this.mutCodon,
+					"AA1",new ProteinCharSequence(this.mutCodon).getString(),
+					
+					//data about the other
+					"PosInCodon2",String.valueOf(other.positionInCodon()+1),
+					"Alt2",other.altAllele.getBaseString(),
+					"Codon2",other.mutCodon,
+					"AA2",new ProteinCharSequence(other.mutCodon).getString()
+					);
+		}
+		
 		@Override
 		public String toString() {
 			return contig+"\t"+genomicPosition1+"\t"+refAllele.getBaseString()+"\t"+
@@ -234,7 +297,7 @@ public class VCFStopCodon extends AbstractVCFStopCodon
 		VariantComparator(SAMSequenceDictionary dict) {
 			this.dict = dict;
 		}
-		int contig(Variant v) { return dict.getSequenceIndex(v.contig);}
+		int contig(final Variant v) { return dict.getSequenceIndex(v.contig);}
 		@Override
 		public int compare(Variant o1, Variant o2) {
 			int i= contig(o1) - contig(o2);
@@ -245,18 +308,75 @@ public class VCFStopCodon extends AbstractVCFStopCodon
 			if(i!=0) return i;
 			return o1.sorting_id - o2.sorting_id;
 			}
+		}
 	
+	
+	private static class Mutation extends AbstractContext
+		{
+		String info=null;
 		}
 
+	static private class MutationCodec extends AbstractDataCodec<Mutation>
+		{
+		@Override
+		public Mutation decode(DataInputStream dis) throws IOException {
+			String contig;
+			try {
+				contig = dis.readUTF();
+			} catch (Exception e) {
+				return null;
+				}
+			final Mutation variant = new Mutation();
+			variant.contig = contig;
+			variant.genomicPosition1 = dis.readInt();
+			variant.refAllele = Allele.create(dis.readUTF(), true);
+			variant.altAllele = Allele.create(dis.readUTF(), false);
+			variant.info = dis.readUTF();
+			variant.sorting_id = dis.readInt();
+			return variant;
+		}
 	
+		@Override
+		public void encode(final DataOutputStream dos, final Mutation v) throws IOException {
+			dos.writeUTF(v.contig);
+			dos.writeInt(v.genomicPosition1);
+			dos.writeUTF(v.refAllele.getBaseString());
+			dos.writeUTF(v.altAllele.getBaseString());
+			dos.writeUTF(v.info);
+			dos.writeInt(v.sorting_id);
+		}
 	
-	static final String TAG="STREAMCODON";
+		@Override
+		public MutationCodec clone() {
+			return new MutationCodec();
+		}
+		
+		}
 	
-	
-	
-	protected Collection<Throwable> scan(final VcfIterator r) throws IOException {
-			
+static private class MutationComparator implements Comparator<Mutation>
+	{
+	final SAMSequenceDictionary dict;
+	MutationComparator(SAMSequenceDictionary dict) {
+		this.dict = dict;
+	}
+	int contig(final Mutation v) { return dict.getSequenceIndex(v.contig);}
+	@Override
+	public int compare(Mutation o1, Mutation o2) {
+		int i= contig(o1) - contig(o2);
+		if(i!=0) return i;
+		i= o1.genomicPosition1-o2.genomicPosition1;
+		if(i!=0) return i;
+		i =  o1.refAllele.compareTo(o2.refAllele);
+		if(i!=0) return i;
+		return o1.sorting_id - o2.sorting_id;
+		}
+	}
+
+	@Override
+	protected Collection<Throwable> doVcfToVcf(final String inputName,final VcfIterator r,final VariantContextWriter w) throws IOException {
+			SortingCollection<Mutation> mutations = null;
 			CloseableIterator<Variant> varIter = null;
+			CloseableIterator<Mutation> mutIter = null;
 			try {
 			LOG.info("opening REF:"+referenceFile);
 			
@@ -297,10 +417,19 @@ public class VCFStopCodon extends AbstractVCFStopCodon
 				}
 			progress.finish();
 			this.variants.doneAdding();
-			final GeneticCode geneticCode= GeneticCode.getStandard();
+			
+			mutations = SortingCollection.newInstance(Mutation.class,
+					new MutationCodec(),
+					new MutationComparator(dict),
+					super.getMaxRecordsInRam(),
+					super.getTmpDirectories()
+					);
+			mutations.setDestructiveIteration(true);
+			
+			
 			
 			varIter = this.variants.iterator();
-			ArrayList<Variant> buffer= new ArrayList<>();
+			final ArrayList<Variant> buffer= new ArrayList<>();
 			for(;;)
 				{
 				Variant variant = null;
@@ -324,30 +453,37 @@ public class VCFStopCodon extends AbstractVCFStopCodon
 								throw new IllegalStateException();
 								}
 							
-							StringBuilder combinedCodon = new StringBuilder(v1.wildCodon);
+							final StringBuilder combinedCodon = new StringBuilder(v1.wildCodon);
 							combinedCodon.setCharAt(v1.positionInCodon(), v1.mutCodon.charAt(v1.positionInCodon()));
 							combinedCodon.setCharAt(v2.positionInCodon(), v2.mutCodon.charAt(v2.positionInCodon()));
 							
-							final String pwild = new ProteinCharSequence(geneticCode, v1.wildCodon).getString();
-							final String p1 = new ProteinCharSequence(geneticCode, v1.mutCodon).getString();
-							final String p2 = new ProteinCharSequence(geneticCode, v2.mutCodon).getString();
-							final String pCombined = new ProteinCharSequence(geneticCode, combinedCodon).getString();
-							if(!pCombined.equals(pwild) &&
-								p1.equals(pwild) &&
-								p2.equals(pwild)
+							final String pwild = new ProteinCharSequence(v1.wildCodon).getString();
+							final String p1 = new ProteinCharSequence(v1.mutCodon).getString();
+							final String p2 = new ProteinCharSequence(v2.mutCodon).getString();
+							final String pCombined = new ProteinCharSequence(combinedCodon).getString();
+							if(
+								(!pCombined.equals(pwild) && p1.equals(pwild) && p2.equals(pwild)) ||//new variant created
+								(!pCombined.equals(p1) && !pCombined.equals(p2) && !pCombined.equals(pwild)) //new amino acid (higher consequence ?)
 								) {
-								System.out.print(v1.toString());
-								System.out.print("\t");
-								System.out.print(p1);
-								System.out.print("\t");
-								System.out.print(v2.toString());
-								System.out.print("\t");
-								System.out.print(p2);
-								System.out.print("\t");
-								System.out.print(pCombined);
-								System.out.println();
-
-
+								final String infoSuffix="|"+String.join("|", "CombinedCodon",combinedCodon,"ComibinedAA",pCombined);
+								
+								final Mutation m1 = new Mutation();
+								m1.contig = v1.contig;
+								m1.genomicPosition1 = v1.genomicPosition1;
+								m1.refAllele = v1.refAllele;
+								m1.altAllele = v1.altAllele;
+								m1.info = v1.getInfo(v2)+ infoSuffix;
+								m1.sorting_id = ID_GENERATOR++;
+								mutations.add(m1);
+								
+								final Mutation m2 = new Mutation();
+								m2.contig = v2.contig;
+								m2.genomicPosition1 = v2.genomicPosition1;
+								m2.refAllele = v2.refAllele;
+								m2.altAllele = v2.altAllele;
+								m2.info = v2.getInfo(v1) + infoSuffix;
+								m2.sorting_id = ID_GENERATOR++;
+								mutations.add(m2);
 							}
 							
 							}
@@ -359,6 +495,52 @@ public class VCFStopCodon extends AbstractVCFStopCodon
 				buffer.add(variant);
 				}
 			
+			mutations.doneAdding();
+			varIter.close();varIter=null;
+			variants.cleanup();variants=null;
+			final ArrayList<Mutation> mBuffer= new ArrayList<>();
+			
+			final VCFHeader header2 = new VCFHeader();
+			header.setSequenceDictionary(header.getSequenceDictionary());
+			final VCFInfoHeaderLine infoHeaderLine = new VCFInfoHeaderLine("CODONVARIANT",VCFHeaderLineCount.UNBOUNDED,VCFHeaderLineType.String,
+					"todo");
+			super.addMetaData(header2);
+			header2.addMetaDataLine(infoHeaderLine);
+			w.writeHeader(header2);
+			
+			mutIter = mutations.iterator();
+			for(;;)
+				{
+				Mutation variant = null;
+				if(mutIter.hasNext())
+					{
+					variant = mutIter.next();
+					}
+				if(variant==null || !(!mBuffer.isEmpty() && mBuffer.get(0).contig.equals(variant.contig) &&  mBuffer.get(0).refAllele.equals(variant.refAllele)))
+					{
+					if(!mBuffer.isEmpty()) {
+					final Mutation first  = mBuffer.get(0);
+					final Set<Allele> alleles = new HashSet<>();
+					final Set<String> info = new HashSet<>();
+					alleles.add(first.refAllele);
+					final VariantContextBuilder vcb=new VariantContextBuilder();
+					vcb.chr(first.contig);
+					vcb.start(first.genomicPosition1);
+					vcb.stop(first.genomicPosition1 + first.refAllele.length()-1);
+					for(final Mutation m:mBuffer){
+						alleles.add(m.altAllele);
+					}
+					vcb.attribute(infoHeaderLine.getID(), new ArrayList<String>(info));
+					w.add(vcb.make());
+					}
+					mBuffer.clear();
+					if(variant==null) break;
+					}
+				mBuffer.add(variant);
+				}
+			mutIter.close();
+			mutations.cleanup();mutations=null;
+			
 			
 			return RETURN_OK;
 			}
@@ -369,8 +551,10 @@ public class VCFStopCodon extends AbstractVCFStopCodon
 		finally
 			{
 			CloserUtil.close(this.indexedFastaSequenceFile);
+			CloserUtil.close(mutIter);
 			CloserUtil.close(varIter);
 			if(this.variants!=null) this.variants.cleanup();
+			if(mutations!=null) mutations.cleanup();
 			this.variants=null;
 			}
 		
@@ -387,10 +571,19 @@ public class VCFStopCodon extends AbstractVCFStopCodon
 			LOG.info("getting genomic Sequence for "+gene.getContig());
 			genomicSequence=new GenomicSequence(this.indexedFastaSequenceFile, gene.getContig());
 			}
-				
+		final int positionContext0 = ctx.getStart() -1;
+		int cdna_length = 3;//get cDNA length including stop codon: so, we're starting from 3
+		for(int exon_index=0;exon_index< gene.getExonCount();++exon_index)
+			{
+			final KnownGene.Exon exon= gene.getExon(exon_index);
+			final int eStart = Math.max(exon.getStart(),gene.getCdsStart());
+			final int eEnd =   Math.min(exon.getEnd(),gene.getCdsEnd());
+			cdna_length += (eStart < eEnd ? eEnd-eStart : 0);
+			}
+		
 		Variant variant=null;
     		
-		final StringBuilder wildRNA=new StringBuilder();
+		final StringBuilder wildRNA=new StringBuilder(cdna_length);
 		final MutedSequence mutRNA=new MutedSequence(wildRNA);
     	
 		if(gene.isPositiveStrand())
@@ -399,21 +592,21 @@ public class VCFStopCodon extends AbstractVCFStopCodon
 	    		{
 	    		final KnownGene.Exon exon= gene.getExon(exon_index);
 	    		for(int i= exon.getStart();
-						i< exon.getEnd();
+						i< exon.getEnd() && wildRNA.length()< cdna_length;
 						++i)
 					{
-					if(i< gene.getCdsStart()) continue;
-					if(i>=gene.getCdsEnd()) break;
+					if(i<  gene.getCdsStart()) continue;
+					//if(i>= gene.getCdsEnd()) break; no, we"re using cdna_length
 
 					wildRNA.append(genomicSequence.charAt(i));
 					
-					if(variant==null && ctx.getStart()-1==i)
+					if(variant==null && positionContext0 ==i)
 						{
 						variant = new Variant(ctx,allele,gene);
 						variant.sorting_id = ID_GENERATOR++;
 						variant.position_in_cdna=wildRNA.length()-1;
 						char mutBase= allele.getBaseString().charAt(0);
-						mutRNA.put( wildRNA.length()-1, mutBase 	);
+						mutRNA.setMutation( wildRNA.length()-1, wildRNA.length(),""+mutBase 	);
 						}
 					}
 	    		}
@@ -426,21 +619,21 @@ public class VCFStopCodon extends AbstractVCFStopCodon
 				final KnownGene.Exon exon= gene.getExon(exon_index);
 				
 				for(int i= exon.getEnd()-1;
-					    i>= exon.getStart();
+					    i>= exon.getStart() && wildRNA.length()< cdna_length;
 					--i)
 					{
 					if(i>= gene.getCdsEnd()) continue;
-					if(i<  gene.getCdsStart()) break;
+					//if(i<  gene.getCdsStart()) break; no we're using cdna_length;
 	
 					wildRNA.append(AcidNucleics.complement(genomicSequence.charAt(i)));
 					
-					if(variant==null && ctx.getStart()-1==i)
+					if(variant==null && positionContext0 ==i)
 						{
 						variant = new Variant(ctx,allele,gene);
 						variant.sorting_id = ID_GENERATOR++;
 						variant.position_in_cdna=wildRNA.length()-1;
 						char mutBase=AcidNucleics.complement(allele.getBaseString().charAt(0));
-						mutRNA.put( wildRNA.length()-1, mutBase 	);
+						mutRNA.setMutation( wildRNA.length()-1, wildRNA.length(),""+mutBase 	);
 						}
     				}
     			--exon_index;
@@ -478,16 +671,7 @@ public class VCFStopCodon extends AbstractVCFStopCodon
 			{
 			return wrapException("Undefined kgURI. option: -"+OPTION_KGURI);
 			}
-		VcfIterator iter=null;
-		try {
-			iter = super.openVcfIterator(inputName);
-			return scan(iter);
-		} catch (Exception e) {
-			return wrapException(e);
-			} finally
-			{
-			CloserUtil.close(iter);	
-			}
+		return doVcfToVcf(inputName);
 		}
 	
 	public static void main(String[] args)
