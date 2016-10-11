1bdd027edb207e2e16d753b7b5d62e59f3453e45
Pierre Lindenbaum <plindenbaum@yahoo.fr>
2016-10-12 00:05:59 +0200

    cont

--- src/main/java/com/github/lindenb/jvarkit/util/command/Argument.java
+++ src/main/java/com/github/lindenb/jvarkit/util/command/Argument.java
@@ -1,3 +1,31 @@
+/*
+The MIT License (MIT)
+
+Copyright (c) 2016 Pierre Lindenbaum
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
+
+
+History:
+* 2016 creation
+
+*/
 package com.github.lindenb.jvarkit.util.command;
 import static java.lang.annotation.ElementType.FIELD;
 import static java.lang.annotation.RetentionPolicy.RUNTIME;
@@ -12,10 +40,12 @@ import java.lang.annotation.Target;
 @Inherited
 @Documented
 public @interface Argument {
-	public String opt();
+	public String opt() default "";
 	public String longopt() default "";
 	public String description() default "";
+	public String argname() default "ARG";
 	public String regex() default "";
 	public String category() default "";
 	public boolean hidden() default false;
+	public int nargs() default 1;
 }
--- src/main/java/com/github/lindenb/jvarkit/util/command/Arguments.java
+++ src/main/java/com/github/lindenb/jvarkit/util/command/Arguments.java
@@ -1,92 +1,103 @@
 package com.github.lindenb.jvarkit.util.command;
 
-import java.lang.reflect.Constructor;
+import java.io.File;
+import java.io.PrintWriter;
 import java.lang.reflect.Field;
 import java.util.ArrayList;
 import java.util.Arrays;
-import java.util.HashSet;
+import java.util.Collection;
 import java.util.List;
-import java.util.Set;
+import java.util.function.Predicate;
+
 
 public class Arguments {
-@Argument(opt="")
-private final List<Object> objects=new ArrayList<>();
+@Argument
+private static boolean print_hidden_options=false;
 
-public Arguments register(final Object...objs) {
-	if(objs==null || objs.length==0) return this;
-	for(final Object obj:objs)
-		{
-		if(obj==null) continue;
-		boolean f=false;
-		for(final Object o: this.objects) if(o==obj){ f=true; break;}
-		if(!f) this.objects.add(obj);
+private final List<ArgumentDef> argumentdefs = new ArrayList<>();
+private Predicate<ArgumentDef> argumentFilter=null;
+public Arguments() {
+	register(Arguments.class);
+	}
+
+public Arguments register(final Object obj) {
+	if(obj!=null) {
+		register(obj,obj.getClass());
 		}
 	return this;
 	}
 
+public Arguments register(final Class<?> clazz) {
+	register(null,clazz);
+	return this;
+	}
+
+public Arguments filter(Predicate<ArgumentDef> filter) {
+	this.argumentFilter  = filter;
+	int i=0;
+	while(this.argumentFilter!=null && i< this.argumentdefs.size()) {
+		if( !this.argumentFilter.test(this.argumentdefs.get(i))) {
+			this.argumentdefs.remove(i);
+			}
+		else
+			{
+			++i;
+			}
+		}
+	return this;
+	}
+
+
+private void register(final Object obj,final Class<?> clazz) {
+	for(final Field field: clazz.getDeclaredFields()) {
+		if(obj==null && !java.lang.reflect.Modifier.isStatic(field.getModifiers())) continue;
+		if(obj!=null && java.lang.reflect.Modifier.isStatic(field.getModifiers())) continue;
+		if(java.lang.reflect.Modifier.isFinal(field.getModifiers())) continue;
+		final Argument argument  = field.getAnnotation(Argument.class);
+		if(argument==null) continue;
+		final ArgumentDef def = new  ArgumentDef(obj, field,argument);
+		if( argumentFilter!=null && !argumentFilter.test(def)) continue;
+		if( argumentdefs.contains(def) ) {
+			System.err.println("Argument already registered: "+def);
+			continue;
+			}
+		this.argumentdefs.add(def);
+		}
+	final Class<?> parentClass = clazz.getSuperclass();
+	if(parentClass==null || parentClass == Object.class) return;
+	register(obj,parentClass);
+	
+	}
+
 List<String> parseOptions(final String argcargv[]) {
 	final List<String> args = new ArrayList<>(Arrays.asList(argcargv));
-	final Set<String> visited=new HashSet<>();
-	scanAll(new ArgScanner() {
-		@Override
-		void process(Object o, Field f, Argument arg) {
-			boolean multiple=false;
-			boolean has_arg =false;
-			
-			List<String> search=new ArrayList<>();
-			if(!(arg.opt()==null || arg.opt().isEmpty())) {
-				search.add("-"+arg.opt());
-				}
-			if(!(arg.longopt()==null || arg.longopt().isEmpty())) {
-				search.add("--"+arg.longopt());
-				}
-			for(String opt: search) {
-				int optind = -1;
-				for(int i=0;i< args.size();++i) {
-					String x = args.get(i);
-					if(!x.startsWith(opt)) continue;
-					int colon = x.indexOf(":");
-					String left=(colon==-1?opt:opt.substring(colon+1));
-					String right=(colon==-1?opt:opt.substring(colon+1));
-					optind=i;
-					break;
-				}
-				if(optind==-1) continue;
-				if(!multiple && visited.contains(opt)) {
-					throw new RuntimeException("Option used twice");
-					}
-				if(has_arg) {
-					Constructor<?> ctor = f.getType().getConstructor(String.class);
-					Object v = ctor.newInstance(args.get(optind+1));
-					f.set(o, v);
-					}
-				visited.addAll(search);
-				}
-			}
-		});
-	
+	for(final ArgumentDef def: this.argumentdefs) {
+		
+		}
 	return args;
 	}
 
-private void scanAll(final ArgScanner scanner) {
-	for(final Object o: this.objects) {
-		scanObject(o,o.getClass(),scanner);
-		}
-	}
-private void scanObject(final Object o,final Class<?> clazz,final ArgScanner scanner)
+public void printHelp(final PrintWriter w) {
+w.print(this.argumentdefs);
+w.flush();
+}
+
+
+private static class Test {
+@Argument
+boolean f1;
+@Argument
+Boolean f2;
+@Argument
+Collection<File> f3 = new ArrayList<>();
+}
+
+public static void main(String[] args)
 	{
-	for(final Field field :clazz.getDeclaredFields()) {
-		final Argument argument = field.getAnnotation(Argument.class);
-		if(argument==null) continue;
-		scanner.process(o, field, argument);
-		}
-	final Class<?> parent = clazz.getSuperclass();
-	if(parent==null || parent == Object.class) return;
-	scanObject(o,parent,scanner);
+	args=new String[]{};
+	Arguments arguments = new Arguments().register(new Test());
+	arguments.printHelp(new PrintWriter(System.out));
+	for(ArgumentDef tt:arguments.argumentdefs)
+		System.err.println(tt);
 	}
-
-private abstract class ArgScanner {
-	abstract void process(Object o,Field f,final Argument argument);
-	}
-
 }
