7fd3f7f6a9576ffde4d46eaaffa652d265e6c38d
Pierre Lindenbaum <plindenbaum@yahoo.fr>
2017-07-12 10:16:50 +0200

    bioalcidaejdk

---
+++ docs/BioAlcidaeJdk.md
@@ -0,0 +1,227 @@
+# BioAlcidaeJdk
+
+java-based version of awk for bioinformatics
+
+
+## Usage
+
+```
+Usage: bioalcidaejdk [options] Files
+  Options:
+    --body
+      user's code is the whole body of the filter class, not just the 'apply' 
+      method. 
+      Default: false
+    -e, --expression
+      inline java expression
+    -F, --format
+      force format: one of VCF BAM SAM FASTQ
+    -h, --help
+      print help and exit
+    --helpFormat
+      What kind of help
+      Possible Values: [usage, markdown, xml]
+    --nocode
+       Don't show the generated code
+      Default: false
+    -o, --output
+      Output file. Optional . Default: stdout
+    -f, --scriptfile
+      java body file
+    --version
+      print version and exit
+
+```
+
+
+## Keywords
+
+ * sam
+ * bam
+ * vcf
+ * javascript
+ * jdk
+
+
+## Compilation
+
+### Requirements / Dependencies
+
+* java compiler SDK 1.8 http://www.oracle.com/technetwork/java/index.html (**NOT the old java 1.7 or 1.6**) . Please check that this java is in the `${PATH}`. Setting JAVA_HOME is not enough : (e.g: https://github.com/lindenb/jvarkit/issues/23 )
+* GNU Make >= 3.81
+* curl/wget
+* git
+* xsltproc http://xmlsoft.org/XSLT/xsltproc2.html (tested with "libxml 20706, libxslt 10126 and libexslt 815")
+
+
+### Download and Compile
+
+```bash
+$ git clone "https://github.com/lindenb/jvarkit.git"
+$ cd jvarkit
+$ make bioalcidaejdk
+```
+
+The *.jar libraries are not included in the main jar file, so you shouldn't move them (https://github.com/lindenb/jvarkit/issues/15#issuecomment-140099011 ).
+The required libraries will be downloaded and installed in the `dist` directory.
+
+### edit 'local.mk' (optional)
+
+The a file **local.mk** can be created edited to override/add some definitions.
+
+For example it can be used to set the HTTP proxy:
+
+```
+http.proxy.host=your.host.com
+http.proxy.port=124567
+```
+## Source code 
+
+[https://github.com/lindenb/jvarkit/tree/master/src/main/java/com/github/lindenb/jvarkit/tools/bioalcidae/BioAlcidaeJdk.java
+](https://github.com/lindenb/jvarkit/tree/master/src/main/java/com/github/lindenb/jvarkit/tools/bioalcidae/BioAlcidaeJdk.java
+)
+## Contribute
+
+- Issue Tracker: [http://github.com/lindenb/jvarkit/issues](http://github.com/lindenb/jvarkit/issues)
+- Source Code: [http://github.com/lindenb/jvarkit](http://github.com/lindenb/jvarkit)
+
+## License
+
+The project is licensed under the MIT license.
+
+## Citing
+
+Should you cite **bioalcidaejdk** ? [https://github.com/mr-c/shouldacite/blob/master/should-I-cite-this-software.md](https://github.com/mr-c/shouldacite/blob/master/should-I-cite-this-software.md)
+
+The current reference is:
+
+[http://dx.doi.org/10.6084/m9.figshare.1425030](http://dx.doi.org/10.6084/m9.figshare.1425030)
+
+> Lindenbaum, Pierre (2015): JVarkit: java-based utilities for Bioinformatics. figshare.
+> [http://dx.doi.org/10.6084/m9.figshare.1425030](http://dx.doi.org/10.6084/m9.figshare.1425030)
+
+
+Bioinformatics file java-based reformatter. Something like awk for VCF, BAM, SAM...
+
+This program takes as input a VCF or a BAM on stdin or as a file.
+The user provides a piece of java code that will be compiled at runtime in memory an executed.
+
+## Base classes 
+
+At the time of writing, we have:
+
+```java
+    public static abstract class AbstractHandler
+    	{
+    	protected PrintStream out = System.out;
+    	protected String inputFile = null;
+    	// called at begin
+    	public void initialize() {}
+    	// called at end
+    	public void dispose() {}
+    	// users MUST implement that function
+    	public abstract void execute() throws Exception;
+    	}
+    
+    public static abstract class VcfHandler extends AbstractHandler
+		{
+    	protected VcfTools tools = null;
+    	protected VCFHeader header = null;
+    	protected VcfIterator iter = null;
+		public Stream<VariantContext> stream()
+			{
+			return StreamSupport.stream(
+					new IterableAdapter<VariantContext>(this.iter).spliterator(),
+					false);
+			}		}
+
+    public static abstract class SAMHandler extends AbstractHandler
+		{
+		protected SamReader in=null;
+		protected SAMFileHeader header=null;
+		protected SAMRecordIterator iter=null;
+		public Stream<SAMRecord> stream()
+			{
+			return StreamSupport.stream(
+					new IterableAdapter<SAMRecord>(this.iter).spliterator(),
+					false);
+			}
+		}
+```
+
+## VCF 
+
+when reading a VCF, a new class extending `VcfHandler`will be compiled. The user's code will be inserted as:
+
+```
+     1  import java.util.*;
+     2  import java.util.stream.*;
+     3  import java.util.function.*;
+     4  import htsjdk.samtools.util.*;
+     5  import htsjdk.variant.variantcontext.*;
+     6  import htsjdk.variant.vcf.*;
+     7  import javax.annotation.Generated;
+     8  @Generated(value="BioAlcidaeJdk",date="2017-07-12T10:00:49+0200")
+     9  public class VcfFilterJdkCustom970827757 extends com.github.lindenb.jvarkit.tools.bioalcidae.BioAlcidaeJdk.VcfHandler {
+    10    public VcfFilterJdkCustom970827757() {
+    11    }
+    12    @Override
+    13    public void execute() throws Exception {
+    14    
+	15     // user's code is inserted here <=======================
+    16     
+    17     }
+    18  }
+```
+
+## SAM
+
+when reading a SAM/BAM, a new class extending `SAMHandler`will be compiled. The user's code will be inserted as:
+
+
+```java
+     1  import java.util.*;
+     2  import java.util.stream.*;
+     3  import java.util.function.*;
+     4  import htsjdk.samtools.util.*;
+     5  import htsjdk.variant.variantcontext.*;
+     6  import htsjdk.variant.vcf.*;
+     7  import javax.annotation.Generated;
+     8  @Generated(value="BioAlcidaeJdk",date="2017-07-12T10:09:20+0200")
+     9  public class VcfFilterJdkCustom1694491176 extends com.github.lindenb.jvarkit.tools.bioalcidae.BioAlcidaeJdk.SAMHandler {
+    10    public VcfFilterJdkCustom1694491176() {
+    11    }
+    12    @Override
+    13    public void execute() throws Exception {
+    14    
+	15     // user's code is inserted here <=======================
+    16     
+    17     }
+    18  }
+```
+
+## Examples
+
+### Example:
+
+count the SNP having a ID in a VCF file:
+
+```
+$ java -jar dist/bioalcidaejdk.jar -e 'System.err.println(stream().filter(V->V.isSNP()).filter(V->V.hasID()).count());' input.vcf.gz
+
+953
+```
+
+### Example:
+
+count the mapped SAM Reads in a BAM file:
+
+```
+$ java -jar dist/bioalcidaejdk.jar -e 'System.err.println(stream().filter(R->!R.getReadUnmappedFlag()).count());'input.bam
+5518 
+```
+
+
+
+
+
---
+++ src/main/java/com/github/lindenb/jvarkit/tools/bioalcidae/BioAlcidaeJdk.java
@@ -0,0 +1,484 @@
+/*
+The MIT License (MIT)
+
+Copyright (c) 2014 Pierre Lindenbaum
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
+
+
+History:
+* 2015 creation
+
+*/
+package com.github.lindenb.jvarkit.tools.bioalcidae;
+
+
+import java.io.File;
+import java.io.PrintStream;
+import java.io.PrintWriter;
+import java.io.StringWriter;
+import java.lang.reflect.Constructor;
+import java.util.Date;
+import java.util.List;
+import java.util.Random;
+import java.util.stream.Stream;
+import java.util.stream.StreamSupport;
+
+import com.beust.jcommander.Parameter;
+import com.github.lindenb.jvarkit.lang.InMemoryCompiler;
+import com.github.lindenb.jvarkit.tools.vcffilterjs.VcfFilterJdk;
+import com.github.lindenb.jvarkit.util.jcommander.Launcher;
+import com.github.lindenb.jvarkit.util.jcommander.Program;
+import com.github.lindenb.jvarkit.util.log.Logger;
+import com.github.lindenb.jvarkit.util.vcf.VcfIterator;
+import com.github.lindenb.jvarkit.util.vcf.VcfTools;
+
+import htsjdk.samtools.SAMFileHeader;
+import htsjdk.samtools.SAMRecord;
+import htsjdk.samtools.SAMRecordIterator;
+import htsjdk.samtools.SamReader;
+import htsjdk.samtools.util.CloserUtil;
+import htsjdk.samtools.util.IOUtil;
+import htsjdk.samtools.util.Iso8601Date;
+import htsjdk.samtools.util.IterableAdapter;
+import htsjdk.samtools.util.StringUtil;
+import htsjdk.variant.variantcontext.VariantContext;
+import htsjdk.variant.vcf.VCFHeader;
+
+/**
+
+BEGIN_DOC
+
+Bioinformatics file java-based reformatter. Something like awk for VCF, BAM, SAM...
+
+This program takes as input a VCF or a BAM on stdin or as a file.
+The user provides a piece of java code that will be compiled at runtime in memory an executed.
+
+## Base classes 
+
+At the time of writing, we have:
+
+```java
+    public static abstract class AbstractHandler
+    	{
+    	protected PrintStream out = System.out;
+    	protected String inputFile = null;
+    	// called at begin
+    	public void initialize() {}
+    	// called at end
+    	public void dispose() {}
+    	// users MUST implement that function
+    	public abstract void execute() throws Exception;
+    	}
+    
+    public static abstract class VcfHandler extends AbstractHandler
+		{
+    	protected VcfTools tools = null;
+    	protected VCFHeader header = null;
+    	protected VcfIterator iter = null;
+		public Stream<VariantContext> stream()
+			{
+			return StreamSupport.stream(
+					new IterableAdapter<VariantContext>(this.iter).spliterator(),
+					false);
+			}		}
+
+    public static abstract class SAMHandler extends AbstractHandler
+		{
+		protected SamReader in=null;
+		protected SAMFileHeader header=null;
+		protected SAMRecordIterator iter=null;
+		public Stream<SAMRecord> stream()
+			{
+			return StreamSupport.stream(
+					new IterableAdapter<SAMRecord>(this.iter).spliterator(),
+					false);
+			}
+		}
+```
+
+## VCF 
+
+when reading a VCF, a new class extending `VcfHandler`will be compiled. The user's code will be inserted as:
+
+```
+     1  import java.util.*;
+     2  import java.util.stream.*;
+     3  import java.util.function.*;
+     4  import htsjdk.samtools.util.*;
+     5  import htsjdk.variant.variantcontext.*;
+     6  import htsjdk.variant.vcf.*;
+     7  import javax.annotation.Generated;
+     8  @Generated(value="BioAlcidaeJdk",date="2017-07-12T10:00:49+0200")
+     9  public class VcfFilterJdkCustom970827757 extends com.github.lindenb.jvarkit.tools.bioalcidae.BioAlcidaeJdk.VcfHandler {
+    10    public VcfFilterJdkCustom970827757() {
+    11    }
+    12    @Override
+    13    public void execute() throws Exception {
+    14    
+	15     // user's code is inserted here <=======================
+    16     
+    17     }
+    18  }
+```
+
+## SAM
+
+when reading a SAM/BAM, a new class extending `SAMHandler`will be compiled. The user's code will be inserted as:
+
+
+```java
+     1  import java.util.*;
+     2  import java.util.stream.*;
+     3  import java.util.function.*;
+     4  import htsjdk.samtools.util.*;
+     5  import htsjdk.variant.variantcontext.*;
+     6  import htsjdk.variant.vcf.*;
+     7  import javax.annotation.Generated;
+     8  @Generated(value="BioAlcidaeJdk",date="2017-07-12T10:09:20+0200")
+     9  public class VcfFilterJdkCustom1694491176 extends com.github.lindenb.jvarkit.tools.bioalcidae.BioAlcidaeJdk.SAMHandler {
+    10    public VcfFilterJdkCustom1694491176() {
+    11    }
+    12    @Override
+    13    public void execute() throws Exception {
+    14    
+	15     // user's code is inserted here <=======================
+    16     
+    17     }
+    18  }
+```
+
+## Examples
+
+### Example:
+
+count the SNP having a ID in a VCF file:
+
+```
+$ java -jar dist/bioalcidaejdk.jar -e 'System.err.println(stream().filter(V->V.isSNP()).filter(V->V.hasID()).count());' input.vcf.gz
+
+953
+```
+
+### Example:
+
+count the mapped SAM Reads in a BAM file:
+
+```
+$ java -jar dist/bioalcidaejdk.jar -e 'System.err.println(stream().filter(R->!R.getReadUnmappedFlag()).count());'input.bam
+5518 
+```
+
+
+
+
+END_DOC
+*/
+
+
+@Program(name="bioalcidaejdk",
+	description="java-based version of awk for bioinformatics",
+	keywords={"sam","bam","vcf","javascript","jdk"}
+	)
+public class BioAlcidaeJdk
+	extends Launcher
+	{
+	private static final Logger LOG = Logger.build(BioAlcidaeJdk.class).make();
+
+
+	@Parameter(names={"-o","--output"},description=OPT_OUPUT_FILE_OR_STDOUT)
+	private File outputFile = null;
+
+	@Parameter(names={"-F","--format"},description="force format: one of VCF BAM SAM FASTQ")
+	private String formatString = null;
+
+    @Parameter(names={"-f","--scriptfile"},description="java body file")
+    private File scriptFile=null;
+    @Parameter(names={"-e","--expression"},description="inline java expression")
+    private String scriptExpr=null;
+	@Parameter(names={"--nocode"},description=" Don't show the generated code")
+	private boolean hideGeneratedCode=false;
+	@Parameter(names={"--body"},description="user's code is the whole body of the filter class, not just the 'apply' method.")
+	private boolean user_code_is_body=false;
+
+    public static abstract class AbstractHandler
+    	{
+    	protected PrintStream out = System.out;
+    	protected String inputFile = null;
+    	public void initialize() {}
+    	public void dispose() {}
+    	public abstract void execute() throws Exception;
+    	}
+    
+    public static abstract class VcfHandler extends AbstractHandler
+		{
+    	protected VcfTools tools = null;
+    	protected VCFHeader header = null;
+    	protected VcfIterator iter = null;
+		public Stream<VariantContext> stream()
+			{
+			return StreamSupport.stream(
+					new IterableAdapter<VariantContext>(this.iter).spliterator(),
+					false);
+			}		}
+
+    public static abstract class SAMHandler extends AbstractHandler
+		{
+		protected SamReader in=null;
+		protected SAMFileHeader header=null;
+		protected SAMRecordIterator iter=null;
+		public Stream<SAMRecord> stream()
+			{
+			return StreamSupport.stream(
+					new IterableAdapter<SAMRecord>(this.iter).spliterator(),
+					false);
+			}
+		}
+
+	private enum FORMAT {
+		VCF{
+			@Override
+			boolean canAs(String src) {
+				return src!=null && (src.endsWith(".vcf") || src.endsWith(".vcf.gz") );
+			}
+			},
+		SAM{
+			@Override
+			boolean canAs(String src) {
+				return src!=null && (src.endsWith(".sam") || src.endsWith(".bam") );
+			}
+			},
+		BAM{
+			@Override
+			boolean canAs(String src) {
+				return src!=null && (src.endsWith(".sam") || src.endsWith(".bam") );
+			}},
+		FASTA{
+			@Override
+			boolean canAs(String src) {
+				return src!=null && (src.endsWith(".fa") || src.endsWith(".fasta") || src.endsWith(".fa.gz") || src.endsWith(".fasta.gz")  );
+			}
+			},
+		FASTQ{
+			@Override
+			boolean canAs(String src) {
+				return src!=null && (src.endsWith(".fq") || src.endsWith(".fastq") || src.endsWith(".fq.gz") || src.endsWith(".fastq.gz")  );
+			}
+			};
+
+		abstract boolean canAs(String src);
+		};
+		
+		private FORMAT format= null;
+
+	
+	@Override
+	public int doWork(final List<String> args) {
+		
+		if(this.formatString!=null)
+			{
+			try {
+				this.format=FORMAT.valueOf(this.formatString.toUpperCase());
+				} catch (Exception err) {
+				LOG.error(err);
+				return -1;
+				}
+			}
+		if(this.scriptFile!=null && !StringUtil.isBlank(this.scriptExpr))
+			{
+			LOG.error("script file and expression both defined");
+			return -1;
+			}
+		
+		if(this.scriptFile==null && StringUtil.isBlank(this.scriptExpr))
+			{
+			LOG.error("script file or expression missing");
+			return -1;
+			}
+		try
+			{
+			final String inputFile = oneFileOrNull(args);
+			if(inputFile==null && this.format==null)
+				{
+				LOG.error("Format must be specified when input is stdin");
+				return -1;
+				}
+			if(inputFile!=null && this.format==null)
+				{
+				for(final FORMAT t:FORMAT.values())
+					{
+					if(t.canAs(inputFile))
+						{
+						this.format=t;
+						break;
+						}
+					}
+				if(this.format==null)
+					{
+					LOG.error("Cannot get file format for "+inputFile+". Please specifiy using option -F");
+					return -1;
+					}
+				}
+			
+			
+			
+			final Random rand= new  Random(System.currentTimeMillis());
+			final String javaClassName =VcfFilterJdk.class.getSimpleName()+
+					"Custom"+ Math.abs(rand.nextInt());
+			
+			final String baseClass;
+			switch(this.format)
+				{
+				case BAM: case SAM:
+					{
+					baseClass = SAMHandler.class.getName().replace('$', '.');
+					break;
+					}
+				case VCF:
+					{
+					baseClass = VcfHandler.class.getName().replace('$', '.');
+					break;
+					}
+				default: throw new IllegalStateException("Not implemented: "+this.format);
+				}
+			
+			final String code;
+			
+			if(this.scriptFile!=null)
+				{
+				code = IOUtil.slurp(this.scriptFile);
+				}
+			else
+				{
+				code = this.scriptExpr;
+				}
+			
+			
+			final StringWriter codeWriter=new StringWriter();
+			final PrintWriter pw = new PrintWriter(codeWriter);
+			pw.println("import java.util.*;");
+			pw.println("import java.util.stream.*;");
+			pw.println("import java.util.function.*;");
+			pw.println("import htsjdk.samtools.util.*;");
+			pw.println("import htsjdk.variant.variantcontext.*;");
+			pw.println("import htsjdk.variant.vcf.*;");
+			pw.println("import javax.annotation.Generated;");
+
+			pw.println("@Generated(value=\""+BioAlcidaeJdk.class.getSimpleName()+"\",date=\""+ new Iso8601Date(new Date()) +"\")");
+			pw.println("public class "+javaClassName+" extends "+ baseClass +" {");
+			
+			pw.println("  public "+javaClassName+"() {");
+			pw.println("  }");
+			
+			
+			if(user_code_is_body)
+				{
+				pw.println("   /** user's code starts here */");
+				pw.println(code);
+				pw.println(    "/** user's code ends here */");
+				}
+			else
+				{
+				pw.println("  @Override");
+				pw.println("  public void execute() throws Exception {");
+				pw.println("   /** user's code starts here */");
+				pw.println(code);
+				pw.println(    "/** user's code ends here */");
+				pw.println("   }");
+				}
+			pw.println("}");
+			pw.flush();
+			
+			
+			if(!hideGeneratedCode)
+				{
+				LOG.debug(" Compiling :\n" + InMemoryCompiler.beautifyCode(codeWriter.toString()));
+				}
+
+			final InMemoryCompiler inMemoryCompiler = new InMemoryCompiler();
+			final Class<?> compiledClass = inMemoryCompiler.compileClass(
+					javaClassName,
+					codeWriter.toString()
+					);
+			final Constructor<?> ctor=compiledClass.getDeclaredConstructor();
+			AbstractHandler handlerInstance= (AbstractHandler)ctor.newInstance();
+			handlerInstance.out = super.openFileOrStdoutAsPrintStream(outputFile);
+			handlerInstance.inputFile = inputFile;
+			
+			switch(this.format)
+				{
+				case BAM: case SAM:
+					{
+					final SAMHandler samHandler = SAMHandler.class.cast(handlerInstance);
+					samHandler.in = super.openSamReader(inputFile);
+					samHandler.header = samHandler.in.getFileHeader();
+					samHandler.iter = samHandler.in.iterator();
+					break;
+					}
+				case VCF:
+					{
+					final VcfHandler vcfHandler = VcfHandler.class.cast(handlerInstance);
+					vcfHandler.iter = super.openVcfIterator(inputFile);
+					vcfHandler.header = vcfHandler.iter.getHeader();
+					vcfHandler.tools = new VcfTools(vcfHandler.header);
+					break;
+					}
+				default: throw new IllegalStateException("Not implemented: "+this.format);
+				}
+			
+			try
+				{
+				handlerInstance.initialize();
+				handlerInstance.execute();
+				
+				}
+			catch(final Throwable err)
+				{
+				LOG.error(err);
+				return -1;
+				}
+			finally
+				{
+				handlerInstance.dispose();
+				CloserUtil.close(handlerInstance.out);
+				handlerInstance.out=null;
+				}			
+			return 0;
+			}
+		catch(final Exception err)
+			{
+			LOG.error(err);
+			return -1;
+			}
+		finally
+			{
+			
+			}
+		}
+	
+	
+	
+	
+	
+	
+	
+	public static void main(String[] args) {
+		new BioAlcidaeJdk().instanceMainWithExit(args);
+	}
+}
