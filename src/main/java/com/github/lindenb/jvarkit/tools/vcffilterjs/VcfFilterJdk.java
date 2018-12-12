39b1ee186062c277e3ac62f50f45fc39234a396e
lindenb <plindenbaum@yahoo.fr>
2018-12-12 17:33:10 +0100

    https://www.biostars.org/p/354126/

--- src/main/java/com/github/lindenb/jvarkit/tools/vcffilterjs/VcfFilterJdk.java
+++ src/main/java/com/github/lindenb/jvarkit/tools/vcffilterjs/VcfFilterJdk.java
@@ -414,7 +414,7 @@ END_DOC
 		keywords={"vcf","filter","java","jdk"},
 		biostars={266201,269854,277820,250212,284083,292710,293314,295902,296145,302217,
 				304979,310155,317388,319148,327035,337645,343569,
-				347173,351205,351404
+				347173,351205,351404,354126
 				},
 		references="\"bioalcidae, samjs and vcffilterjs: object-oriented formatters and filters for bioinformatics files\" . Bioinformatics, 2017. Pierre Lindenbaum & Richard Redon  [https://doi.org/10.1093/bioinformatics/btx734](https://doi.org/10.1093/bioinformatics/btx734)."
 		)
