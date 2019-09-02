c03aabb4acf40f1db72164a6bff18c04d350f492
lindenb <plindenbaum@yahoo.fr>
2019-09-02 09:20:34 +0200

    vcfburdengtf

--- src/main/java/com/github/lindenb/jvarkit/tools/burden/VcfBurdenGtf.java
+++ src/main/java/com/github/lindenb/jvarkit/tools/burden/VcfBurdenGtf.java
@@ -28,6 +28,7 @@ import java.io.PrintWriter;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.util.ArrayList;
+import java.util.BitSet;
 import java.util.Collections;
 import java.util.HashSet;
 import java.util.Iterator;
@@ -58,14 +59,17 @@ import com.github.lindenb.jvarkit.util.jcommander.Launcher;
 import com.github.lindenb.jvarkit.util.jcommander.Program;
 import com.github.lindenb.jvarkit.util.log.Logger;
 import com.github.lindenb.jvarkit.util.log.ProgressFactory;
+import com.github.lindenb.jvarkit.util.samtools.ContigDictComparator;
 import com.github.lindenb.jvarkit.util.vcf.JexlVariantPredicate;
 import com.github.lindenb.jvarkit.util.vcf.VCFUtils;
 
 import htsjdk.samtools.SAMSequenceDictionary;
+import htsjdk.samtools.SAMSequenceRecord;
 import htsjdk.samtools.util.CloserUtil;
 import htsjdk.samtools.util.Interval;
 import htsjdk.samtools.util.IntervalTree;
 import htsjdk.samtools.util.Locatable;
+import htsjdk.samtools.util.StringUtil;
 import htsjdk.variant.variantcontext.Allele;
 import htsjdk.variant.variantcontext.Genotype;
 import htsjdk.variant.variantcontext.VariantContext;
@@ -80,7 +84,7 @@ import htsjdk.variant.vcf.VCFInfoHeaderLine;
 description="Run Burden On different part of transcripts",
 keywords={"vcf","burden","gtf","case","control"},
 creationDate="20190806",
-modificationDate="20190808",
+modificationDate="20190809",
 generate_doc=false
 )
 public class VcfBurdenGtf
@@ -100,7 +104,10 @@ extends Launcher
 	private double fisherTreshold = 1e-5;
 	@Parameter(names={"-save-vcf","--save-vcf"},description="Save Matching variants into that VCF.")
 	private Path outputVcfPath = null;
+	@Parameter(names={"-intergenic","--intergenic"},description="Ignored if empty. If it is '*' run only on all intergenic regions found in the GTF. Otherwise, run only on intergenic regions on chromosome specified by the parameter.")
+	private String intergenic_contig = null;
 
+	
 	private Pedigree pedigree = null;
 	private Set<Sample> cases = null;
 	private Set<Sample> controls = null;
@@ -110,7 +117,7 @@ extends Launcher
 		SV0,AT_LEAST_ONE_VARIANT
 		}
 	
-	
+		
 	private class SubPartOfTranscript implements Locatable {
 		private List<Locatable> intervals = new ArrayList<>();
 		private final Transcript transcript;
@@ -145,7 +152,57 @@ extends Launcher
 
 	}
 
+	private static class FisherResult {
+		double p_value = 0;
+		int affected_alt = 0;
+		int affected_hom = 0;
+		int unaffected_alt = 0;
+		int unaffected_hom = 0;
+
+	}
 	
+	private FisherResult runFisher(
+		final List<VariantContext> variants	
+		) {
+		final Map<Sample,SuperVariant> indi2supervariant = new HashMap<>(this.cases.size() + this.controls.size());
+		this.cases.stream().forEach(S->indi2supervariant.put(S,SuperVariant.SV0));
+		this.controls.stream().forEach(S->indi2supervariant.put(S,SuperVariant.SV0));
+		
+		for(final VariantContext ctx:variants) {
+			final Allele observed_alt = ctx.getAltAlleleWithHighestAlleleCount();
+			for(final Sample sample : indi2supervariant.keySet() ) {
+				if(indi2supervariant.get(sample)==SuperVariant.AT_LEAST_ONE_VARIANT) continue;
+				final Genotype g = ctx.getGenotype(sample.getId());	
+				if(g==null || g.isHomRef() || g.isNoCall()) continue;
+				if( g.getAlleles().stream().anyMatch(A->A.equals(observed_alt))) {
+					indi2supervariant.put(sample,SuperVariant.AT_LEAST_ONE_VARIANT);
+					break;
+					}
+			}// end for sample
+		}//end of forVariant
+		final FisherResult fisher = new FisherResult();
+		
+		for(final Sample sample : indi2supervariant.keySet() ) {
+			final SuperVariant superVariant = indi2supervariant.get(sample);
+			if(superVariant==SuperVariant.SV0 ) {
+				if(sample.isAffected()) fisher.affected_hom++;
+				else fisher.unaffected_hom++;
+				}
+			else // AT_LEAST_ONE_VARIANT 
+				{
+				if(sample.isAffected()) fisher.affected_alt++;
+				else fisher.unaffected_alt++;
+				}
+		}//end of sample
+
+		
+		final FisherExactTest test = FisherExactTest.compute(
+				fisher.affected_alt, fisher.affected_hom, 
+				fisher.unaffected_alt, fisher.unaffected_hom
+				);
+		fisher.p_value = test.getAsDouble();
+		return fisher;
+		}
 
 	private boolean accept(final VariantContext ctx) {
 		if(!variantFilter.test(ctx))return false;
@@ -233,8 +290,44 @@ extends Launcher
 			pw.println();
 
 			
+			final List<Gene> all_genes =  gtfReader.getAllGenes().
+					stream().
+					filter(G->StringUtil.isBlank(this.intergenic_contig) || this.intergenic_contig.equals("*") || this.intergenic_contig.equals(G.getContig()) ).
+					sorted(new ContigDictComparator(vcfDict).createLocatableComparator()).
+					collect(Collectors.toCollection(ArrayList::new));
+			
+			final List<Interval> all_intergenic = new ArrayList<>();
+			
+			if(!StringUtil.isBlank(this.intergenic_contig)) {
+				for(final SAMSequenceRecord ssr: vcfDict.getSequences()) {
+					if(!(this.intergenic_contig.equals("*") || this.intergenic_contig.equals(ssr.getSequenceName()))) continue;
+					final BitSet filled = new BitSet(ssr.getSequenceLength()+2);
+					
+					all_genes.stream().
+						filter(G->G.getContig().equals(ssr.getSequenceName())).
+						forEach(G->filled.set(G.getStart(), 1 /* bit set is 0 based */ + Math.min(G.getEnd(),ssr.getSequenceLength())));
+					int i=1;
+					while(i< ssr.getSequenceLength()) {
+						if(filled.get(i)) {
+							i++;
+							continue;
+							}
+						int j=i;
+						while(j< ssr.getSequenceLength() && !filled.get(j)) {
+							j++;
+							}
+						all_intergenic.add(new Interval(ssr.getSequenceName(),i,j));
+						i=j+1;
+						}
+					all_genes.removeIf(G->G.getContig().equals(ssr.getSequenceName()));
+					}
+				all_genes.clear();
+				}
+			
 			final ProgressFactory.Watcher<Gene> progress = ProgressFactory.newInstance().logger(LOG).dictionary(vcfDict).build();
-			for(final Gene gene : gtfReader.getAllGenes()) {
+			
+			/* run genes */
+			for(final Gene gene : all_genes) {
 				progress.apply(gene);
 				final IntervalTree<VariantContext> intervalTree = new IntervalTree<>();
 				vcfReader.query(gene).stream().
@@ -330,47 +423,8 @@ extends Launcher
 							}
 						if(variants.isEmpty()) continue;
 
-						final Map<Sample,SuperVariant> indi2supervariant = new HashMap<>(this.cases.size() + this.controls.size());
-						this.cases.stream().forEach(S->indi2supervariant.put(S,SuperVariant.SV0));
-						this.controls.stream().forEach(S->indi2supervariant.put(S,SuperVariant.SV0));
-						
-						for(final VariantContext ctx:variants) {
-							final Allele observed_alt = ctx.getAltAlleleWithHighestAlleleCount();
-							for(final Sample sample : indi2supervariant.keySet() ) {
-								if(indi2supervariant.get(sample)==SuperVariant.AT_LEAST_ONE_VARIANT) continue;
-								final Genotype g = ctx.getGenotype(sample.getId());	
-								if(g==null || g.isHomRef() || g.isNoCall()) continue;
-								if( g.getAlleles().stream().anyMatch(A->A.equals(observed_alt))) {
-									indi2supervariant.put(sample,SuperVariant.AT_LEAST_ONE_VARIANT);
-									break;
-									}
-							}// end for sample
-						}//end of forVariant
-
-						int affected_alt = 0;
-						int affected_hom = 0;
-						int unaffected_alt = 0;
-						int unaffected_hom = 0;
-						for(final Sample sample : indi2supervariant.keySet() ) {
-							final SuperVariant superVariant = indi2supervariant.get(sample);
-							if(superVariant==SuperVariant.SV0 ) {
-								if(sample.isAffected()) affected_hom++;
-								else unaffected_hom++;
-								}
-							else // AT_LEAST_ONE_VARIANT 
-								{
-								if(sample.isAffected()) affected_alt++;
-								else unaffected_alt++;
-								}
-						}//end of sample
-
-						
-						final FisherExactTest fisher = FisherExactTest.compute(
-								affected_alt, affected_hom, 
-								unaffected_alt, unaffected_hom
-								);
-						final double p_value=fisher.getAsDouble();
-						if(p_value> this.fisherTreshold) continue;
+						final FisherResult fisher = runFisher(variants);
+						if(fisher.p_value> this.fisherTreshold) continue;
 						
 						if(vcw!=null) {
 							for(final VariantContext ctx:variants) {
@@ -402,15 +456,15 @@ extends Launcher
 						pw.print("\t");
 						pw.print(part.intervals.stream().map(R->String.valueOf(R.getStart())+"-"+R.getEnd()).collect(Collectors.joining(";")));
 						pw.print("\t");
-						pw.print(p_value);
+						pw.print(fisher.p_value);
 						pw.print("\t");
-						pw.print(affected_alt);
+						pw.print(fisher.affected_alt);
 						pw.print("\t");
-						pw.print(affected_hom);
+						pw.print(fisher.affected_hom);
 						pw.print("\t");
-						pw.print(unaffected_alt);
+						pw.print(fisher.unaffected_alt);
 						pw.print("\t");
-						pw.print(unaffected_hom);
+						pw.print(fisher.unaffected_hom);
 						pw.print("\t");
 						pw.print(variants.size());
 						pw.println();
@@ -419,6 +473,87 @@ extends Launcher
 					}
 				}
 			progress.close();
+			
+			final ProgressFactory.Watcher<Interval> progress2 = ProgressFactory.newInstance().logger(LOG).dictionary(vcfDict).build();
+
+			/** scan intergenics ... */
+			for(final Interval intergenic : all_intergenic) {
+				progress2.apply(intergenic);
+				final int intergenic_window_size=2000;
+				final int intergenic_window_shifr=100;
+				final List<Interval> parts = new ArrayList<>();
+
+				if(intergenic.getLengthOnReference()<=intergenic_window_size) continue;
+				int start_pos=intergenic.getStart();
+				while(start_pos + intergenic_window_size  <= intergenic.getEnd())
+					{
+					int xend = Math.min(intergenic.getEnd(), start_pos+intergenic_window_size-1);
+					int xstart = xend-intergenic_window_size-1;
+					parts.add(new Interval(intergenic.getContig(),xstart,xend));
+					start_pos +=intergenic_window_shifr;
+					}
+					
+					
+				for(final Interval part: parts) {
+					final List<VariantContext> variants = vcfReader.query(part).
+							stream().
+							filter(V->accept(V)).
+							collect(Collectors.toList());
+					
+					if(variants.isEmpty()) continue;
+	
+					final FisherResult fisher = runFisher(variants);
+					if(fisher.p_value> this.fisherTreshold) continue;
+					
+					final String label = "intergenic_"+part.getStart()+"_"+part.getEnd();
+					if(vcw!=null) {
+						for(final VariantContext ctx:variants) {
+							vcw.add(new VariantContextBuilder(ctx).
+									attribute(BURDEN_KEY, VCFUtils.escapeInfoField(label)).
+									make());
+						}
+					}
+					
+					pw.print(part.getContig());
+					pw.print("\t");
+					pw.print(part.getStart()-1);
+					pw.print("\t");
+					pw.print(part.getEnd());
+					pw.print("\t");
+					pw.print(label);
+					pw.print("\t");
+					pw.print(part.getLengthOnReference());
+					pw.print("\t");
+					pw.print(".");
+					pw.print("\t");
+					pw.print("intergenic");
+					pw.print("\t");
+					pw.print(".");
+					pw.print("\t");
+					pw.print(".");
+					pw.print("\t");
+					pw.print(".");
+					pw.print("\t");
+					pw.print(""+part.getStart()+"-"+part.getEnd());
+					pw.print("\t");
+					pw.print(fisher.p_value);
+					pw.print("\t");
+					pw.print(fisher.affected_alt);
+					pw.print("\t");
+					pw.print(fisher.affected_hom);
+					pw.print("\t");
+					pw.print(fisher.unaffected_alt);
+					pw.print("\t");
+					pw.print(fisher.unaffected_hom);
+					pw.print("\t");
+					pw.print(variants.size());
+					pw.println();
+					}
+					
+				}
+			progress2.close();
+
+			
 			
 			if(vcw!=null) {
 				vcw.close();
