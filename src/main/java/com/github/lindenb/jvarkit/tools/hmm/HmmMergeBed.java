b2284f5ad50a373481bf1b64a2cdcc57c354d9a3
Pierre Lindenbaum <plindenbaum@yahoo.fr>
2019-11-09 12:53:20 +0100

    hmmmerge

--- src/main/java/com/github/lindenb/jvarkit/tools/hmm/HmmMergeBed.java
+++ src/main/java/com/github/lindenb/jvarkit/tools/hmm/HmmMergeBed.java
@@ -52,18 +52,27 @@ import htsjdk.samtools.util.MergingIterator;
 import htsjdk.samtools.util.RuntimeIOException;
 import htsjdk.samtools.util.StringUtil;
 
+/**
+BEGIN_DOC
+
+## input
+
+input is a set of sorted BED file , or a file with the '.list' suffix containing the paths to the bed file. 
+
+END_DOC
+*/
 @Program(name="hmmmergebed",
-	description="Pour Julien. Merge hmm bed",
+	description="For @154sns. Merge hmm bed",
 	keywords={"bed","merge"},
 	creationDate="20191108",
-	modificationDate="20191108"
+	modificationDate="20191109"
 	)
 public class HmmMergeBed extends Launcher {
 	private static final Logger LOG = Logger.build(HmmMergeBed.class).make();
 	
 	@Parameter(names={"-o","--output"},description=OPT_OUPUT_FILE_OR_STDOUT)
 	private Path outputFile = null;
-	@Parameter(names={"-R","--reference","--dict"},description=DICTIONARY_SOURCE)
+	@Parameter(names={"-R","--reference","--dict"},description="dictionary if data are sorted on a REF order." +DICTIONARY_SOURCE)
 	private final Path dictPath = null;
 	
 	private static class Base implements Locatable
@@ -124,12 +133,15 @@ public class HmmMergeBed extends Launcher {
 			this.delegate.close();
 			this.bedline=null;
 			}
-		
 		}
+	
 	private static class BedLineIterator extends AbstractIterator<BedLine> implements Closeable {
 		final BedLineCodec codec = new BedLineCodec();
+		private BedLine prevRec = null;
 		private BufferedReader br ;
-		private BedLineIterator(final Path path) throws IOException{
+		private final Path path;
+		private BedLineIterator(final Path path) throws IOException {
+			this.path = path;
 			this.br = IOUtils.openPathForBufferedReading(path);
 			}
 		@Override
@@ -147,6 +159,10 @@ public class HmmMergeBed extends Launcher {
 						}
 					final BedLine bed = this.codec.decode(line);
 					if(bed==null) continue;
+					if(prevRec!=null && prevRec.overlaps(bed)) {
+						throw new IOException("got two bed overlapping "+bed+" "+prevRec+" in "+this.path);
+						}
+					prevRec=bed;
 					return bed;
 					}
 				}
@@ -162,12 +178,89 @@ public class HmmMergeBed extends Launcher {
 				}
 			}
 		}
+	
+	private class BaseMerge implements Locatable
+		{
+		String contig;
+		int start;
+		int end;
+		String opcode;
+		
+		@Override
+		public String getContig() {
+			return contig;
+			}
+		@Override
+		public int getStart() {
+			return start;
+			}
+		@Override
+		public int getEnd() {
+			return end;
+			}
+		}
+	private class BaseMergerIterator extends AbstractIterator<BaseMerge> implements CloseableIterator<BaseMerge> {
+		private final List<CloseableIterator<Base>> baseiterators;
+		private final MergingIterator<Base> mergingIter;
+		private final EqualRangeIterator<Base> equalIter;
+		BaseMergerIterator(final List<CloseableIterator<Base>> baseiterators,final Comparator<Base> locCompare) {
+			this.baseiterators = baseiterators ;
+			this.mergingIter = new MergingIterator<>(locCompare, baseiterators);
+			this.equalIter = new EqualRangeIterator<>(mergingIter, locCompare);
+ 			}
+		String getOpcode(final  List<Base> s) {
+			return "*";
+			}
+		
+		@Override
+		protected BaseMerge advance() {
+			if(!equalIter.hasNext()) return null;
+			final List<Base> L = equalIter.next();
+			final Base first = L.get(0);
+			final BaseMerge bm = new BaseMerge();
+			bm.contig = first.getContig();
+			bm.start = first.getPos();
+			bm.end = first.getPos();
+			bm.opcode = getOpcode(L);
+			while(equalIter.hasNext()) {
+				final List<Base> L2 = equalIter.peek();
+				final Base second = L2.get(0);
+				if(!second.getContig().equals(bm.contig)) break;
+				if(bm.end+1 != second.getPos()) break;
+				if(!bm.opcode.equals(getOpcode(L2))) break;
+				equalIter.next();//consumme
+				bm.end = second.getPos();
+				}
+			return bm;
+			}
+		@Override
+		public void close() {
+			this.equalIter.close();
+			this.mergingIter.close();
+			for(CloseableIterator<?> iter:baseiterators) iter.close();
+			}
+		}
+	
 	@Override
 	public int doWork(final List<String> args) {
 		PrintWriter pw = null;
 		try {
-			final SAMSequenceDictionary dict = SequenceDictionaryUtils.extractRequired(this.dictPath);
-			final Comparator<Base> locCompare = new ContigDictComparator(dict).createLocatableComparator();
+			final Comparator<Base> locCompare;
+			
+			if(this.dictPath!=null) {
+				final SAMSequenceDictionary dict = SequenceDictionaryUtils.extractRequired(this.dictPath);
+				locCompare = new ContigDictComparator(dict).createLocatableComparator();
+				}
+			else
+				{
+				locCompare = (A,B)->{
+					int i= A.getContig().compareTo(B.getContig());
+					if(i!=0) return i;
+					i = Integer.compare(A.getStart(),B.getStart());
+					if(i!=0) return i;
+					return Integer.compare(A.getEnd(),B.getEnd());
+					};
+				}
 			final List<Path> paths = IOUtils.unrollPaths(args);
 			if(paths.isEmpty()) {
 				LOG.error("empty input");
@@ -181,22 +274,28 @@ public class HmmMergeBed extends Launcher {
 				final BedLineToBaseIterator iter2 = new BedLineToBaseIterator(iter1);
 				baseiterators.add(iter2);
 				}
-			final MergingIterator<Base> mergingIter = new MergingIterator<>(locCompare, baseiterators);
-			final EqualRangeIterator<Base> equalIter = new EqualRangeIterator<>(mergingIter, locCompare);
 			
 			
 			pw = super.openPathOrStdoutAsPrintWriter(this.outputFile);
 			
-			while(equalIter.hasNext()) {
-				final List<Base> base = equalIter.next();
+			final BaseMergerIterator bmiter = new BaseMergerIterator(baseiterators, locCompare);
+			
+			while(bmiter.hasNext()) {
+				final BaseMerge bm = bmiter.next();
+				pw.print(bm.contig);
+				pw.print('\t');
+				pw.print(bm.start-1);
+				pw.print('\t');
+				pw.print(bm.end);
+				pw.print('\t');
+				pw.print(bm.opcode);
+				pw.println();
 				}
-			equalIter.close();
-			mergingIter.close();
+			bmiter.close();
 			
 			pw.flush();
 			pw.close();
 			
-			for(CloseableIterator<?> iter:baseiterators) iter.close();
 			return 0;
 		} catch(final Throwable err) {
 			LOG.error(err);
