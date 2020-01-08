6208516e7d4263c87d11cc9632e320b94b8c399b
lindenb <plindenbaum@yahoo.fr>
2020-01-08 11:18:28 +0100

    change copyright

--- src/main/java/com/github/lindenb/jvarkit/tools/structvar/VcfStretchOfGt.java
+++ src/main/java/com/github/lindenb/jvarkit/tools/structvar/VcfStretchOfGt.java
@@ -1,7 +1,7 @@
 /*
 The MIT License (MIT)
 
-Copyright (c) 2019 Pierre Lindenbaum
+Copyright (c) 2020 Pierre Lindenbaum
 
 Permission is hereby granted, free of charge, to any person obtaining a copy
 of this software and associated documentation files (the "Software"), to deal
@@ -24,8 +24,11 @@ SOFTWARE.
 */
 package com.github.lindenb.jvarkit.tools.structvar;
 
-import java.io.File;
 import java.io.PrintWriter;
+import java.nio.file.Path;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
 import java.util.HashSet;
 import java.util.List;
 import java.util.Set;
@@ -33,14 +36,18 @@ import java.util.function.Predicate;
 import java.util.stream.Collectors;
 
 import com.beust.jcommander.Parameter;
-import com.github.lindenb.jvarkit.util.Pedigree;
+import com.github.lindenb.jvarkit.lang.StringUtils;
+import com.github.lindenb.jvarkit.pedigree.Pedigree;
+import com.github.lindenb.jvarkit.pedigree.PedigreeParser;
 import com.github.lindenb.jvarkit.util.jcommander.Launcher;
 import com.github.lindenb.jvarkit.util.jcommander.Program;
 import com.github.lindenb.jvarkit.util.log.Logger;
 import com.github.lindenb.jvarkit.util.log.ProgressFactory;
+
 import htsjdk.variant.vcf.VCFIterator;
 
 import htsjdk.samtools.util.CloserUtil;
+import htsjdk.samtools.util.CoordMath;
 import htsjdk.variant.variantcontext.Genotype;
 import htsjdk.variant.variantcontext.VariantContext;
 import htsjdk.variant.vcf.VCFHeader;
@@ -64,18 +71,24 @@ END_DOC
 */
 @Program(name="vcfstrechofgt",
 description="Try to finds deletion by searching strech of HOM_REF/HOM_VAR/NO_CALL Genotypes.",
-keywords={"vcf","deletion"}
+keywords={"vcf","deletion"},
+creationDate="20190103",
+modificationDate="20200108"
 )
 public class VcfStretchOfGt extends Launcher
 	{
 	private static final Logger LOG = Logger.build(VcfStretchOfGt.class).make();
 
 	@Parameter(names={"-o","--output"},description=OPT_OUPUT_FILE_OR_STDOUT)
-	private File outputFile = null;
-	@Parameter(names={"-p","--pedigree"},description=Pedigree.OPT_DESCRIPTION)
-	private File pedigreeFile = null;
-	
-	private class Stretch
+	private Path outputFile = null;
+	@Parameter(names={"-p","--pedigree"},description="If defined, the tool will use the affected sample and find strech where all affected could be a DEL." + PedigreeParser.OPT_DESC)
+	private Path pedigreePath = null;
+	@Parameter(names={"-a","--affected"},description="Same as option --pedigree but provide the name of the samples using a comma/space/semicolon separated string")
+	private String affectedStr = null;
+	@Parameter(names={"-nc","--no-call"},description="Do not accept NO_CALL genotypes.")
+	private boolean exclude_no_call = false;
+
+	private static class Stretch
 		{
 		String contig;
 		int start;
@@ -84,23 +97,85 @@ public class VcfStretchOfGt extends Launcher
 		double sumAvgDp=0.0;
 		int countOthers=0;
 		}
-	private void dump(final PrintWriter w,final Stretch data) {
-		if(data.countOthers>=data.countVariants) return;
-		w.print(data.contig);
-		w.print('\t');
-		w.print(data.start-1);
-		w.print('\t');
-		w.print(data.end);
-		w.print('\t');
-		w.print(1+data.end-data.start);
-		w.print('\t');
-		w.print(data.countVariants);
-		w.print('\t');
-		w.print(data.sumAvgDp/data.countVariants);
-		w.print('\t');
-		w.print(data.countOthers);
-		w.println();
-	}
+
+	
+	private class SampleSet {
+		final Set<String> affected;
+		final Set<String> otherSamples;
+		Stretch current = null;
+		final Predicate<Genotype> acceptGenotype;
+		
+		SampleSet(final Set<String> all_samples,final Set<String> affected) {
+			this.affected = affected;
+			this.otherSamples = new HashSet<>(all_samples);
+			this.otherSamples.removeAll(this.affected);
+			this.acceptGenotype=G->G.isHomRef() || G.isHomVar()|| (G.isNoCall() && !exclude_no_call);
+			}
+		
+		private void dump(final PrintWriter w) {
+			if(current==null) return ;
+			if(current.countOthers>=current.countVariants) return;
+			w.print(current.contig);
+			w.print('\t');
+			w.print(current.start-1);
+			w.print('\t');
+			w.print(current.end);
+			w.print('\t');
+			w.print(CoordMath.getLength(current.start, current.end));
+			w.print('\t');
+			w.print(String.join(";",this.affected));
+			w.print('\t');
+			w.print(current.countVariants);
+			w.print('\t');
+			w.print(current.sumAvgDp/current.countVariants);
+			w.print('\t');
+			w.print(current.countOthers);
+			w.println();
+		}
+		
+		private void visit(final PrintWriter w,final VariantContext ctx) {
+			if(current!=null && !current.contig.equals(ctx.getContig())) {
+				dump(w);
+				current=null;
+				}
+			final boolean ok=
+					affected.stream().
+					map(S->ctx.getGenotype(S)).
+					allMatch(acceptGenotype);
+			
+			final boolean otherOk=
+					otherSamples.stream().
+					map(S->ctx.getGenotype(S)).
+					allMatch(acceptGenotype);
+			
+			
+			
+			if(!ok) {
+				if(current!=null) dump(w);
+				current=null;
+				}
+			else if(current==null) {
+				current=new Stretch();
+				current.contig=ctx.getContig();
+				current.start= ctx.getStart();
+				}
+			
+			//do this for any Stretch
+			if(current!=null) {
+				current.end= ctx.getEnd();
+				current.countVariants++;
+				current.sumAvgDp += ctx.getGenotypes().stream().
+						filter(G->G.hasDP() && affected.contains(G.getSampleName())).
+						mapToInt(G->G.getDP()).
+						average().
+						orElse(0.0);
+				current.countOthers += (otherOk?1:0);
+				}
+			}
+		}
+	
+	
+	
 	
 	@Override
 	public int doWork(final List<String> args) {
@@ -112,25 +187,59 @@ public class VcfStretchOfGt extends Launcher
 			if(!header.hasGenotypingData()) {
 				LOG.error("No genotype in input");
 				return -1;
-			}
-			final Pedigree pedigree = (this.pedigreeFile==null?
-					Pedigree.newParser().parse(header):
-					Pedigree.newParser().parse(this.pedigreeFile)
-					);
-			pedigree.verifyPersonsHaveUniqueNames();
-			final Set<String> affected = pedigree.getAffected().
-					stream().
-					map(P->P.getId()).
-					filter(ID->header.getSampleNameToOffset().containsKey(ID)).
-					collect(Collectors.toSet());
-			final Set<String> otherSamples = new HashSet<>(header.getSampleNamesInOrder());
-			otherSamples.removeAll(affected);
-			
-			if(affected.isEmpty()) {
-				LOG.error("No affected sample in VCF");
-				return -1;
 				}
-			w = super.openFileOrStdoutAsPrintWriter(this.outputFile);
+			final Set<String> vcf_samples = new HashSet<>(header.getSampleNamesInOrder());
+			
+			final List<SampleSet> all_sample_set = new ArrayList<>();
+			
+			if(!StringUtils.isBlank(this.affectedStr)) {
+				
+				final Set<String> affected = Arrays.stream(this.affectedStr.split("[ ,;]+")).
+						filter(S->!StringUtils.isBlank(S)).
+						filter(S->vcf_samples.contains(S)).
+						collect(Collectors.toSet());
+				
+				if(affected.isEmpty()) {
+					LOG.error("No affected sample in string "+this.affectedStr);
+					return -1;
+					}
+				
+				/* at least 2 because singleton sample are created later */
+				if(affected.size()>1) {
+					all_sample_set.add(new SampleSet(vcf_samples,affected));
+					}
+				}
+			
+			if(this.pedigreePath!=null) {
+				final Pedigree pedigree =  new PedigreeParser().parse(this.pedigreePath);
+				
+				final Set<String> affected = pedigree.
+						getAffectedSamples().
+						stream().
+						map(P->P.getId()).
+						filter(ID->header.getSampleNameToOffset().containsKey(ID)).
+						collect(Collectors.toSet());
+				
+				if(affected.isEmpty()) {
+					LOG.error("No affected sample in pedigree "+this.pedigreePath);
+					return -1;
+					}
+				
+				/* at least 2 because singleton sample are created later */
+				if(affected.size()>1) {
+					all_sample_set.add(new SampleSet(vcf_samples,affected));
+					}
+				}
+			
+			for(final String sn:vcf_samples) {
+				final SampleSet sampleSet = new SampleSet(vcf_samples,Collections.singleton(sn));
+				all_sample_set.add(sampleSet);
+			}
+			
+		
+			
+			
+			w = super.openPathOrStdoutAsPrintWriter(this.outputFile);
 			
 			w.print("#chrom");
 			w.print('\t');
@@ -140,6 +249,8 @@ public class VcfStretchOfGt extends Launcher
 			w.print('\t');
 			w.print("length");
 			w.print('\t');
+			w.print("samples");
+			w.print('\t');
 			w.print("count.affected.variants");
 			w.print('\t');
 			w.print("average.affected.depth");
@@ -148,55 +259,19 @@ public class VcfStretchOfGt extends Launcher
 			w.println();
 			
 			final ProgressFactory.Watcher<VariantContext> progress = ProgressFactory.newInstance().dictionary(header).logger(LOG).build();
-			Stretch current=null;
 			
-			final Predicate<Genotype> acceptGenotype=G->G.isHomRef() || G.isHomVar()|| G.isNoCall();
 			
 			while(iter.hasNext()) {
 				final VariantContext ctx=progress.apply(iter.next());
-				if(current!=null && !current.contig.equals(ctx.getContig())) {
-					dump(w,current);
-					current=null;
-					}
-				boolean ok=
-						affected.stream().
-						map(S->ctx.getGenotype(S)).
-						allMatch(acceptGenotype);
-				
-				boolean otherOk=
-						otherSamples.stream().
-						map(S->ctx.getGenotype(S)).
-						allMatch(acceptGenotype);
-				
-				if(!ok) {
-					if(current!=null) dump(w,current);
-					current=null;
-					}
-				else if(current==null) {
-					current=new Stretch();
-					current.contig=ctx.getContig();
-					current.start= ctx.getStart();
-					}
-				
-				//do this for any Stretch
-				if(current!=null) {
-					current.end= ctx.getEnd();
-					current.countVariants++;
-					current.sumAvgDp += ctx.getGenotypes().stream().
-							filter(G->G.hasDP() && affected.contains(G.getSampleName())).
-							mapToInt(G->G.getDP()).
-							average().
-							orElse(0.0);
-					current.countOthers += (otherOk?1:0);
-					}
+				for(final SampleSet snSet:all_sample_set) snSet.visit(w, ctx);
 				}
-			if(current!=null) dump(w,current);
+			for(final SampleSet snSet:all_sample_set) snSet.dump(w);
 			w.flush();w.close();w=null;
 			iter.close();iter=null;
 			progress.close();
 			return 0;
 			}
-		catch(final Exception err) {
+		catch(final Throwable err) {
 			LOG.error(err);
 			return -1;
 			}
