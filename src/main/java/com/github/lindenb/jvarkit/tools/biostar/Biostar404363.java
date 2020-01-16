09104e194a53d32f805e9b5f7c80e94a32280d5e
lindenb <plindenbaum@yahoo.fr>
2020-01-16 12:15:55 +0100

    gradle url changed

--- src/main/java/com/github/lindenb/jvarkit/tools/biostar/Biostar404363.java
+++ src/main/java/com/github/lindenb/jvarkit/tools/biostar/Biostar404363.java
@@ -127,7 +127,7 @@ END_DOC
 @Program(name="biostar404363",
 	keywords={"sam","bam","variant"},
 	description="introduce artificial mutation SNV in bam",
-	biostars=404363,
+	biostars= {404363,416897},
 	creationDate="20191023",
 	modificationDate="20191024"
 	)
