1c2b8df318ad911108581aa979cfa89fbb29102c
lindenb <plindenbaum@yahoo.fr>
2020-01-27 17:38:24 +0100

    cont

--- src/main/java/com/github/lindenb/jvarkit/tools/structvar/CnvSlidingWindow.java
+++ src/main/java/com/github/lindenb/jvarkit/tools/structvar/CnvSlidingWindow.java
@@ -1,3 +1,28 @@
+/*
+The MIT License (MIT)
+
+Copyright (c) 2020 Pierre Lindenbaum
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
+
+
+*/
 package com.github.lindenb.jvarkit.tools.structvar;
 
 import java.io.BufferedReader;
@@ -9,6 +34,7 @@ import java.io.IOException;
 import java.nio.file.Path;
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Collections;
 import java.util.HashSet;
 import java.util.List;
 import java.util.Set;
@@ -30,8 +56,10 @@ import com.github.lindenb.jvarkit.util.bio.bed.BedLineCodec;
 import com.github.lindenb.jvarkit.util.bio.fasta.ContigNameConverter;
 import com.github.lindenb.jvarkit.util.iterator.EqualRangeIterator;
 import com.github.lindenb.jvarkit.util.jcommander.Launcher;
+import com.github.lindenb.jvarkit.util.jcommander.Program;
 import com.github.lindenb.jvarkit.util.log.Logger;
 import com.github.lindenb.jvarkit.util.picard.AbstractDataCodec;
+import com.github.lindenb.jvarkit.util.samtools.ContigDictComparator;
 import com.github.lindenb.jvarkit.variant.variantcontext.writer.WritingVariantsDelegate;
 
 import htsjdk.samtools.Cigar;
@@ -60,14 +88,28 @@ import htsjdk.variant.vcf.VCFConstants;
 import htsjdk.variant.vcf.VCFHeader;
 import htsjdk.variant.vcf.VCFHeaderLine;
 import htsjdk.variant.vcf.VCFStandardHeaderLines;
+/**
+BEGIN_DOC
 
+
+END_DOC 
+ */
+@Program(
+	name="cnvslidingwindow",
+	description="detect CNV by sliding window.",
+	keywords={"vcf","cnv","bam"},
+	creationDate="20200127",
+	modificationDate="20200127",
+	generate_doc=false
+	)
 public class CnvSlidingWindow extends Launcher {
 	private static final Logger LOG = Logger.build( CnvSlidingWindow.class).make();
 	@Parameter(names={"-o","--output"},description=OPT_OUPUT_FILE_OR_STDOUT)
 	private Path outputFile = null;
 	@Parameter(names={"-R","--reference"},description=INDEXED_FASTA_REFERENCE_DESCRIPTION,required=true)
 	private Path refPath = null;
-	@Parameter(names={"-X","--exclude"},description="Exclude Bed.")
+	@Parameter(names={"-X","--exclude","--gaps","--gap"},description="gap file. A bed file containing region to exclude in the genome. "
+			+ "E.g: http://hgdownload.cse.ucsc.edu/goldenPath/hg19/database/gap.txt.gz ")
 	private Path excludeBedPath = null;
 	@Parameter(names={"-xp","--exclude-pattern"},description="Exclude Chromosomes matching the following regex")
 	private String excludeRegex="^GL\\d|^chrEBV$|^hs37d5$|^NC|_random$|Un_|^HLA\\-|_alt$|hap\\d$";
@@ -75,6 +117,8 @@ public class CnvSlidingWindow extends Launcher {
 	private String windowDefs="500;100;1kb;100;2kb;100;3kb;100;5kb;100;6kb;100;7kb;100;8kb;100;9kb;100;10kb;1000;";
 	@Parameter(names={"-x","--extend"},description="extend window by this factor.")
 	private double extend = 0.3;
+	@Parameter(names={"--treshold"},description="treshold",hidden=true)
+	private double treshold = 0.05;
 	@ParametersDelegate
 	private WritingSortingCollection sorting = new WritingSortingCollection();
 	@ParametersDelegate
@@ -170,27 +214,49 @@ private class Sample
 private static double median(final double array[],int len) {
 	if(len==0) return Double.NaN;
 	Arrays.sort(array,0,len);
-	int n = len/2;
+	int mid_x = len/2;
 	if(len%2==0) {
-		return (array[n]+array[n+1])/2.0;
+		return (array[mid_x-1]+array[mid_x])/2.0;
 	} else {
-		return array[n];
+		return array[mid_x];
 	}
 	
 }
 
-private static boolean between(double v,double base,double dy) {
-	return v>=base-dy && v<=base+dy;
+
+private static List<Locatable> split(final Locatable contig,final Locatable gap)
+	{
+	if(!contig.overlaps(gap)) throw new IllegalStateException();
+	if(contig.getLengthOnReference()==0) return Collections.emptyList();
+	if(gap.contains(contig)) return Collections.emptyList();
+	final List<Locatable> intervals1 = new ArrayList<>(2);
+	
+	if(contig.getStart() < gap.getStart())
+		{
+		final Locatable left = new SimpleInterval(contig.getContig(),contig.getStart(),gap.getStart()-1);
+		if(left.getLengthOnReference()>0) intervals1.add(left);
+		}
+	if(contig.getEnd() > gap.getEnd())
+		{
+		final Locatable right = new SimpleInterval(contig.getContig(),gap.getEnd()+1,contig.getEnd());
+		if(right.getLengthOnReference()>0) intervals1.add(right);
+		}
+	
+	return intervals1;
+	}
+
+private  boolean between(double v,double base) {
+	return v>=base-this.treshold && v<=base+this.treshold;
 }
 
 private static final int CNV_UNDEFINED=-999999;
 private int getCNVIndex(double normalizedDepth) {
 	if(Double.isNaN(normalizedDepth)) return CNV_UNDEFINED;
-	if(between(normalizedDepth,0,0.1)) return 0;
-	if(between(normalizedDepth,1.5,0.1)) return 1;
-	if(between(normalizedDepth,2.0,0.1)) return 2;
-	if(between(normalizedDepth,-1.5,0.1)) return -1;
-	if(between(normalizedDepth,0.0,0.1)) return -2;
+	if(between(normalizedDepth,0)) return 0;
+	if(between(normalizedDepth,1.5)) return 1;
+	if(between(normalizedDepth,2.0)) return 2;
+	if(between(normalizedDepth,-1.5)) return -1;
+	if(between(normalizedDepth,0.0)) return -2;
 	return CNV_UNDEFINED;
 	}
 
@@ -230,8 +296,10 @@ public int doWork(final List<String> args) {
 			if(sampleNames.contains(sample.name))
 				{
 				LOG.error("duplicate sample "+sample.name);
+				sample.close();
 				return -1;
 				}
+			sampleNames.add(sample.name);
 			samples.add(sample);
 			SequenceUtil.assertSequenceDictionariesEqual(dict, sample.dict);
 			}
@@ -253,25 +321,30 @@ public int doWork(final List<String> args) {
 					map(B->new SimpleInterval(ctgConverter.apply(B.getContig()), B.getStart(), B.getEnd())).
 					collect(Collectors.toList())
 					;
-			final List<Locatable> contigs2  = new ArrayList<>();		
-			for(final Locatable ctg:contigs) {
-				int start = ctg.getStart();
-				int end = start;
-				while(end<ctg.getEnd()) {
-					for(Locatable xrgn: exclude) {
-						if(!xrgn.overlaps(ctg)) continue;
-						start = xrgn.getEnd();
+				
+				boolean done=false;
+				while(!done) {
+					done = true;
+					int i=0;
+					while(i< contigs.size()) {
+						final Locatable contig = contigs.get(i);
+						final Locatable overlapper = exclude.stream().filter(EX->EX.overlaps(contig)).findAny().orElse(null);
+						if(overlapper!=null)  {
+							contigs.remove(i);
+							contigs.addAll(split(contig, overlapper));
+							done= false;
+							}
+						else
+							{
+							i++;
+							}
 						}
-					
-					
 					}
 				}
-			
-			contigs.clear();
-			contigs.addAll(contigs2);
 			}
-			
-		}
+		contigs.sort(new ContigDictComparator(dict).createLocatableComparator());
+
+		
 		final Allele ref_allele = Allele.create("N", true);
 		final Allele dup_allele = Allele.create("<DUP>", false);
 		final Allele del_allele = Allele.create("<DEL>", false);
@@ -288,6 +361,7 @@ public int doWork(final List<String> args) {
 		
 		final Set<VCFHeaderLine> metaData = new HashSet<>();
 		VCFStandardHeaderLines.addStandardFormatLines(metaData, true, VCFConstants.GENOTYPE_KEY);
+		VCFStandardHeaderLines.addStandardInfoLines(metaData, true, VCFConstants.END_KEY);
 		VCFHeader header = new VCFHeader(metaData,sampleNames);
 		header.setSequenceDictionary(dict);
 		JVarkitVersion.getInstance().addMetaData(this, header);
@@ -297,6 +371,7 @@ public int doWork(final List<String> args) {
 
 		
 		for(final Locatable contig : contigs) {
+			
 			System.gc();
 			final short array[] = new short[contig.getLengthOnReference()];
 			SortingCollection<Gt> sorter = SortingCollection.newInstance(
@@ -317,6 +392,8 @@ public int doWork(final List<String> args) {
 						{
 						final SAMRecord rec = iter.next();
 						if(rec.getReadUnmappedFlag()) continue;
+						if(rec.getDuplicateReadFlag()) continue;
+						if(rec.isSecondaryOrSupplementary()) continue;
 						if(rec.getReadFailsVendorQualityCheckFlag()) continue;
 						final Cigar cigar = rec.getCigar();
 						if(cigar==null || cigar.isEmpty()) continue;
@@ -330,12 +407,13 @@ public int doWork(final List<String> args) {
 								{
 								if(op.consumesReadBases())
 									{
-									for(int i=0;i< ce.getLength() &&
-											refPos-1+i < array.length &&
-											array[refPos-1+i]!=Short.MAX_VALUE
-											;i++)
+									for(int i=0;i< ce.getLength();i++)
 										{
-										array[refPos-1+i]++;
+										final int idx = refPos-contig.getStart()+i;
+										if(idx<0) continue;
+										if(idx>=array.length) break;
+										if(array[idx]==Short.MAX_VALUE) continue;
+										array[idx]++;
 										}
 									}
 								refPos+=ce.getLength();
@@ -344,44 +422,52 @@ public int doWork(final List<String> args) {
 						}
 					}
 				
-				for(int i=0;i< windows_array.length;i+=2) {
-					final int window_size = windows_array[i+0];
+				for(int widx=0;widx< windows_array.length;widx+=2) {
+					final int window_size = windows_array[widx+0];
 					final int extend = (int)Math.ceil(window_size * this.extend);
 					if(extend<=0) continue;
 					if(window_size> contig.getLengthOnReference()) continue;
-					final int window_shift = windows_array[i+1];
+					
+					
+					final int window_shift = windows_array[widx+1];
+					
+					LOG.info(contig+" "+window_size+"+-"+extend+";"+window_shift+" "+sampleBam.name);
+
+					
 					final double bound_array[] = new double[extend+extend];
 					final double coverage[] = new double[window_size];
 					
 					for(int pos1 = contig.getStart();
-							pos1 + window_size + extend + extend <= contig.getEnd();
+							pos1 + window_size + extend + extend <  contig.getEnd();
 							pos1 += window_shift) {
-						int n=0;
 						
-						for(int x=0;x<extend;i++) {
-							final int idx = pos1 + x;
-							bound_array[n]= array[idx];
-							n++;
+						int n=0;
+						for(int x=0;x<extend;x++) {
+							final int idx = pos1 - contig.getStart() + x;
+							if(idx<0 || idx >= array.length) throw new IndexOutOfBoundsException(""+idx+" "+array.length+" "+contig.getLengthOnReference()+" "+x);
+							if(n<0 || n>=bound_array.length) throw new IndexOutOfBoundsException(""+n+" "+bound_array.length+" "+contig.getLengthOnReference()+" "+x);
+							bound_array[n++] = array[idx];
 						}
 						
 						
-						for(int x=0;x<extend;i++) {
-							final int idx = pos1 + extend + window_size + x;
-							bound_array[n]= array[idx];
-							n++;
+						for(int x=0;x<extend;x++) {
+							final int idx = pos1 -  contig.getStart() + extend + window_size + x;
+							bound_array[n++]= array[idx];
 						}
 						
 						
 						int n2=0;
-						for(int x=0;x<window_size;i++) {
-							final int idx = pos1 + extend + x;
+						for(int x=0;x<window_size;x++) {
+							final int idx = pos1 -  contig.getStart() + extend + x;
 							coverage[n2]= array[idx];
 							n2++;
 						}
 											
 						final double median = median(bound_array,n);
-						for(int x=0;x< n2;x++) {
-							coverage[x] = coverage[x]/median;
+						if(median>0) {
+							for(int x=0;x< n2;x++) {
+								coverage[x] = coverage[x]/median;
+							}
 						}
 						
 						final double norm_depth =  median(coverage,n2);
@@ -400,24 +486,28 @@ public int doWork(final List<String> args) {
 			sorter.setDestructiveIteration(true);
 			
 			try(CloseableIterator<Gt> iter=sorter.iterator()) {
-				EqualRangeIterator<Gt> eq=new EqualRangeIterator<>(iter, (A,B)->A.compare0(B));
+				final EqualRangeIterator<Gt> eq=new EqualRangeIterator<>(iter, (A,B)->A.compare0(B));
 				while(eq.hasNext()) {
 					final List<Gt> row = eq.next();
 					if(row.isEmpty()) continue;
 					final Gt first = row.get(0);
 					final Set<Allele> alleles = new HashSet<>();
-					row.stream().flatMap(GT->cnv2allele.apply(GT.cnv).stream()).forEach(CNV->alleles.add(CNV));
+					row.stream().
+						flatMap(GT->cnv2allele.apply(GT.cnv).stream()).
+						forEach(CNV->alleles.add(CNV));
 					
 					alleles.add(ref_allele);
 					
-					if(alleles.size()<=1) continue;
+					if(alleles.size()<2) continue;
 					
 					final VariantContextBuilder vcb = new VariantContextBuilder(null, contig.getContig(),first.start,first.end, alleles);
+					vcb.attribute(VCFConstants.END_KEY, first.end);
 					final List<Genotype> genotypes = new ArrayList<>(samples.size());
 					for(final Gt gt:row) {
 						final GenotypeBuilder gb=new GenotypeBuilder(samples.get(gt.sample_idx).name,cnv2allele.apply(gt.cnv));
 						genotypes.add(gb.make());
 					}
+					vcb.genotypes(genotypes);
 					vcw.add(vcb.make());
 				}
 				eq.close();
@@ -429,7 +519,7 @@ public int doWork(final List<String> args) {
 		vcw.close();
 		return 0;
 		}
-	catch(Throwable err)
+	catch(final Throwable err)
 		{
 		LOG.error(err);
 		return -1;
