5e442cd205f58258cd2e2f3dc46d0529b4ee6115
lindenb <plindenbaum@yahoo.fr>
2020-06-02 23:00:37 +0200

    cont

--- src/main/java/com/github/lindenb/jvarkit/tools/structvar/VcfPostProcessSV.java
+++ src/main/java/com/github/lindenb/jvarkit/tools/structvar/VcfPostProcessSV.java
@@ -26,7 +26,9 @@ package com.github.lindenb.jvarkit.tools.structvar;
 
 import java.util.Arrays;
 import java.util.Comparator;
+import java.util.ArrayList;
 import java.util.List;
+import java.util.Collections;
 import java.util.stream.Collectors;
 
 import com.beust.jcommander.ParametersDelegate;
@@ -36,12 +38,12 @@ import com.github.lindenb.jvarkit.lang.CharSplitter;
 import com.github.lindenb.jvarkit.lang.StringUtils;
 import com.github.lindenb.jvarkit.util.Counter;
 import com.github.lindenb.jvarkit.util.JVarkitVersion;
-import com.github.lindenb.jvarkit.util.iterator.EqualRangeIterator;
 import com.github.lindenb.jvarkit.util.jcommander.Program;
 import com.github.lindenb.jvarkit.util.log.Logger;
 import com.github.lindenb.jvarkit.variant.variantcontext.Breakend;
 
 import htsjdk.samtools.util.CloseableIterator;
+import htsjdk.samtools.util.PeekIterator;
 import htsjdk.samtools.util.CoordMath;
 import htsjdk.samtools.util.SortingCollection;
 import htsjdk.variant.variantcontext.Allele;
@@ -130,8 +132,7 @@ public class VcfPostProcessSV extends OnePassVcfLauncher {
 				LOG.error("Cannot find INFO for mate-id (was "+this.keys+")");
 				return -1;
 				}
-			
-			LOG.info("reading input.");
+			LOG.info("Mate key is '" + mateID +"'. Reading input.");
 						
 			final Comparator<VariantContext> comparator = (A,B)->   A.getAttributeAsString(mateID, "").compareTo(B.getAttributeAsString(mateID, ""));
 			
@@ -166,18 +167,36 @@ public class VcfPostProcessSV extends OnePassVcfLauncher {
 			sorter1.setDestructiveIteration(true);
 			
 			CloseableIterator<VariantContext> iter2=sorter1.iterator();
-			final EqualRangeIterator<VariantContext> equal_range = new EqualRangeIterator<>(iter2, comparator);
+			PeekIterator<VariantContext> peek = new PeekIterator<>(iter2);
 						
-			while(equal_range.hasNext()) {
-				final List<VariantContext> variants = equal_range.next();
+			while(peek.hasNext()) {
+				final VariantContext first = peek.next();
+				final List<VariantContext> variants = new ArrayList<>();
+				variants.add(first);
+				while(peek.hasNext()) {
+					final VariantContext second = peek.peek();
+					if(first.hasID() && first.getID().equals(second.getAttributeAsString(mateID,""))) {
+						variants.add(peek.next());
+					} else if(second.hasID() && second.getID().equals(first.getAttributeAsString(mateID,""))) {
+						variants.add(peek.next());
+					} else if(first.getAttributeAsString(mateID,"").equals(second.getAttributeAsString(mateID,""))) {
+						variants.add(peek.next());
+					} else {
+					break;
+					}
+				}
+
+
 				if(variants.size()!=2) {
-				counter.incr("Not a pair of Mate.", variants.size());
-				for(final VariantContext ctx:variants) {
+					counter.incr("Not a pair of Mate ("+variants.size()+")", variants.size());
+					for(final VariantContext ctx:variants) {
 						sorter2.add(ctx);
 						}
 					continue;
 					}
 				
+				Collections.sort(variants,(A,B)->Integer.compare(A.getStart(),B.getStart()));
+
 				final VariantContext ctx1 = variants.get(0);
 				final VariantContext ctx2 = variants.get(1);
 				if(!(ctx1.getNAlleles()==2 && ctx1.getNAlleles()==2)) {
@@ -194,16 +213,18 @@ public class VcfPostProcessSV extends OnePassVcfLauncher {
 					sorter2.add(ctx1);
 					sorter2.add(ctx2);
 					}
-				/* should be the same breakend */
+				/* should be the same breakend 
+				difference can be large use CIPOS / CIEND ?
 				if( !ctx1.getContig().equals(brk2.getContig()) ||
 					!ctx2.getContig().equals(brk1.getContig()) ||
-					ctx1.getStart()!=brk2.getStart()  ||
-					ctx2.getStart()!=brk1.getStart()) {
+					Math.abs(ctx1.getStart()-brk2.getStart())>1  ||
+					Math.abs(ctx2.getStart()-brk1.getStart())>1) {
 					counter.incr("mate is not the same position.",2L);
 					sorter2.add(ctx1);
 					sorter2.add(ctx2);
 					continue;
 					}
+				*/
 				final VariantContextBuilder vcb1 = new VariantContextBuilder(ctx1);						
 				final VariantContextBuilder vcb2 = new VariantContextBuilder(ctx2);
 
@@ -216,19 +237,16 @@ public class VcfPostProcessSV extends OnePassVcfLauncher {
 					continue;
 					}
 					
-				if(ctx1.getStart()>ctx2.getStart()) {
-					counter.incr("mate start < ctx start.",2L);
-					sorter2.add(ctx1);
-					sorter2.add(ctx2);
-					continue;
-					}
-				
 						
 				final Allele ctx1_alt = ctx1.getAlleles().get(1);
 				final List<Allele> alleles = Arrays.asList(ctx1.getReference(),Allele.create("<"+this.newAlternateSymbol.trim()+">",false));
 				vcb1.attribute(VCFConstants.SVTYPE,this.newAlternateSymbol.trim());
 				vcb1.alleles(alleles);
-				vcb1.attribute("SVLEN",CoordMath.getLength(ctx1.getEnd(),ctx2.getStart()));
+				final int ctx_end = Math.max(ctx1.getEnd(),ctx2.getEnd());
+				vcb1.stop(ctx_end);
+				vcb1.attribute("END",ctx_end);
+				vcb1.attribute("SVLEN",CoordMath.getLength(ctx1.getStart(),ctx_end));
+				vcb1.rmAttribute(mateID);
 				vcb1.genotypes(ctx1.getGenotypes().stream().map(GT->new GenotypeBuilder(GT).
 						alleles(GT.getAlleles().
 							stream().
@@ -239,7 +257,6 @@ public class VcfPostProcessSV extends OnePassVcfLauncher {
 				sorter2.add(vcb1.make());
 				counter.incr("Two BND variants converted.",2L);
 				}
-			equal_range.close();
 			iter2.close();
 			sorter1.cleanup();
 			sorter1= null;
