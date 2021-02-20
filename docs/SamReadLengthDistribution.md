5d263ebdafdb8bc29ead444cf9b62e409b9fb61a
Pierre Lindenbaum <plindenbaum@yahoo.fr>
2021-02-20 14:09:32 +0100

    cont

--- docs/SamReadLengthDistribution.md
+++ docs/SamReadLengthDistribution.md
@@ -2,7 +2,7 @@
 
 ![Last commit](https://img.shields.io/github/last-commit/lindenb/jvarkit.png)
 
-Sam read length distribution
+Sam read/insert length distribution
 
 
 ## Usage
@@ -20,18 +20,29 @@ Usage: samreadlengthdistribution [options] Files
       print help and exit
     --helpFormat
       What kind of help. One of [usage,markdown,xml].
+    -Q, --mapq
+      min MAPQ
+      Default: 0
     -o, --output
       Output file. Optional . Default: stdout
+    -R, --reference
+      For reading CRAM. Indexed fasta Reference file. This file must be 
+      indexed with samtools faidx and with picard CreateSequenceDictionary
+    --regions
+      Limit analysis to this interval. A source of intervals. The following 
+      suffixes are recognized: vcf, vcf.gz bed, bed.gz, gtf, gff, gff.gz, 
+      gtf.gz.Otherwise it could be an empty string (no interval) or a list of 
+      plain interval separated by '[ \t\n;,]'
     --version
       print version and exit
     -w, --windows
-      A 'range of integers' is a list of integers in ascending order separated 
-      with semicolons.
+      Histogram windows. A 'range of integers' is a list of integers in 
+      ascending order separated with semicolons.
       Default: [[-Inf/0[, [0/10[, [10/20[, [20/30[, [30/40[, [40/50[, [50/100[, [100/150[, [150/200[, [200/250[, [250/300[, [300/350[, [350/400[, [400/450[, [450/500[, [500/1000[, [1000/Inf[]
     -m
-      method, how should I get the read length ?
+      method, how should I get the read/insert length ?
       Default: SEQ_LENGTH
-      Possible Values: [SEQ_LENGTH, CIGAR_REF_LENGTH, CIGAR_PADDED_REF_LENGTH]
+      Possible Values: [SEQ_LENGTH, CIGAR_REF_LENGTH, CIGAR_PADDED_REF_LENGTH, INSERT_LENGTH]
 
 ```
 
@@ -59,6 +70,11 @@ $ ./gradlew samreadlengthdistribution
 ```
 
 The java jar file will be installed in the `dist` directory.
+
+
+## Creation Date
+
+20160922
 
 ## Source code 
 
@@ -103,26 +119,13 @@ input is a set of bam file or a file with suffix '.list' containing the path to 
 ## Example
 
 ```
-$ java -jar dist/samreadlengthdistribution.jar src/test/resources/S*.bam
-
-#ReadLength     S1      S2      S3      S4      S5
-[-Inf/0[	0	0	0	0	0
-[0/10[	0	0	0	0	0
-[10/20[	0	0	0	0	0
-[20/30[	0	0	0	0	0
-[30/40[	0	0	0	0	0
-[40/50[	0	0	0	0	0
-[50/100[	1998	1998	1998	1998	1998
-[100/150[	0	0	0	0	0
-[150/200[	0	0	0	0	0
-[200/250[	0	0	0	0	0
-[250/300[	0	0	0	0	0
-[300/350[	0	0	0	0	0
-[350/400[	0	0	0	0	0
-[400/450[	0	0	0	0	0
-[450/500[	0	0	0	0	0
-[500/1000[	0	0	0	0	0
-[1000/Inf[	0	0	0	0	0
+$ java -jar dist/samreadlengthdistribution.jar -m INSERT_LENGTH src/test/resources/S*.bam 
+#sample	COUNT-INSERT_LENGTH	MIN-INSERT_LENGTH	MAX-INSERT_LENGTH	AVG-INSERT_LENGTH	MEDIAN-INSERT_LENGTH	[-Inf/0[	[0/10[	[10/20[	[20/30[	[30/40[	[40/50[	[50/100[	[100/150[	[150/200[	[200/250[[250/300[	[300/350[	[350/400[	[400/450[	[450/500[	[500/1000[	[1000/Inf[
+S1	999	310	696	501.31	502.00	0	0	0	0	0	0	0	0	0	003	18	115	336	527	0
+S2	999	322	667	500.69	501.00	0	0	0	0	0	0	0	0	0	002	17	138	332	510	0
+S3	999	322	667	500.69	501.00	0	0	0	0	0	0	0	0	0	002	17	138	332	510	0
+S4	999	368	654	500.76	501.00	0	0	0	0	0	0	0	0	0	000	23	138	327	511	0
+S5	999	333	641	499.78	501.00	0	0	0	0	0	0	0	0	0	004	26	126	330	513	0
 ```
 
 
--- src/main/java/com/github/lindenb/jvarkit/tools/misc/SamReadLengthDistribution.java
+++ src/main/java/com/github/lindenb/jvarkit/tools/misc/SamReadLengthDistribution.java
@@ -22,9 +22,6 @@ OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 SOFTWARE.
 
 
-History:
-* 2014 creation
-
 */
 package com.github.lindenb.jvarkit.tools.misc;
 
@@ -33,8 +30,8 @@ import java.io.PrintWriter;
 import java.nio.file.Path;
 import java.util.List;
 import java.util.Map;
-import java.util.Set;
 import java.util.TreeMap;
+import java.util.stream.Collectors;
 
 import htsjdk.samtools.Cigar;
 import htsjdk.samtools.SAMRecord;
@@ -42,14 +39,20 @@ import htsjdk.samtools.SAMRecordIterator;
 import htsjdk.samtools.SamInputResource;
 import htsjdk.samtools.SamReader;
 import htsjdk.samtools.SamReaderFactory;
-import htsjdk.samtools.util.CloserUtil;
+import htsjdk.samtools.filter.FilteringSamIterator;
+import htsjdk.samtools.filter.IntervalFilter;
+import htsjdk.samtools.util.CloseableIterator;
+import htsjdk.samtools.util.Interval;
 
 import com.beust.jcommander.Parameter;
 import com.github.lindenb.jvarkit.io.IOUtils;
 import com.github.lindenb.jvarkit.lang.StringUtils;
+import com.github.lindenb.jvarkit.math.DiscreteMedian;
 import com.github.lindenb.jvarkit.math.RangeOfIntegers;
-import com.github.lindenb.jvarkit.util.Counter;
+import com.github.lindenb.jvarkit.samtools.util.IntervalListProvider;
+import com.github.lindenb.jvarkit.util.bio.SequenceDictionaryUtils;
 import com.github.lindenb.jvarkit.util.jcommander.Launcher;
+import com.github.lindenb.jvarkit.util.jcommander.NoSplitter;
 import com.github.lindenb.jvarkit.util.jcommander.Program;
 import com.github.lindenb.jvarkit.util.log.Logger;
 import com.github.lindenb.jvarkit.util.samtools.SAMRecordPartition;
@@ -70,35 +73,23 @@ input is a set of bam file or a file with suffix '.list' containing the path to 
 ## Example
 
 ```
-$ java -jar dist/samreadlengthdistribution.jar src/test/resources/S*.bam
-
-#ReadLength     S1      S2      S3      S4      S5
-[-Inf/0[	0	0	0	0	0
-[0/10[	0	0	0	0	0
-[10/20[	0	0	0	0	0
-[20/30[	0	0	0	0	0
-[30/40[	0	0	0	0	0
-[40/50[	0	0	0	0	0
-[50/100[	1998	1998	1998	1998	1998
-[100/150[	0	0	0	0	0
-[150/200[	0	0	0	0	0
-[200/250[	0	0	0	0	0
-[250/300[	0	0	0	0	0
-[300/350[	0	0	0	0	0
-[350/400[	0	0	0	0	0
-[400/450[	0	0	0	0	0
-[450/500[	0	0	0	0	0
-[500/1000[	0	0	0	0	0
-[1000/Inf[	0	0	0	0	0
+$ java -jar dist/samreadlengthdistribution.jar -m INSERT_LENGTH src/test/resources/S*.bam 
+#sample	COUNT-INSERT_LENGTH	MIN-INSERT_LENGTH	MAX-INSERT_LENGTH	AVG-INSERT_LENGTH	MEDIAN-INSERT_LENGTH	[-Inf/0[	[0/10[	[10/20[	[20/30[	[30/40[	[40/50[	[50/100[	[100/150[	[150/200[	[200/250[[250/300[	[300/350[	[350/400[	[400/450[	[450/500[	[500/1000[	[1000/Inf[
+S1	999	310	696	501.31	502.00	0	0	0	0	0	0	0	0	0	003	18	115	336	527	0
+S2	999	322	667	500.69	501.00	0	0	0	0	0	0	0	0	0	002	17	138	332	510	0
+S3	999	322	667	500.69	501.00	0	0	0	0	0	0	0	0	0	002	17	138	332	510	0
+S4	999	368	654	500.76	501.00	0	0	0	0	0	0	0	0	0	000	23	138	327	511	0
+S5	999	333	641	499.78	501.00	0	0	0	0	0	0	0	0	0	004	26	126	330	513	0
 ```
 
 
 END_DOC
  */
 @Program(name="samreadlengthdistribution",
-description="Sam read length distribution",
+description="Sam read/insert length distribution",
 keywords={"sam","bam","histogram"},
-modificationDate="20191010"
+modificationDate="20200220",
+creationDate="20160922"
 )
 public class SamReadLengthDistribution extends Launcher
 	{
@@ -108,50 +99,79 @@ public class SamReadLengthDistribution extends Launcher
 		SEQ_LENGTH,
 		CIGAR_REF_LENGTH,
 		CIGAR_PADDED_REF_LENGTH,
+		INSERT_LENGTH
 	}
 	
 	@Parameter(names={"-o","--output"},description=OPT_OUPUT_FILE_OR_STDOUT)
 	private Path outputFile = null;
 	@Parameter(names={"--groupby"},description="Group Reads by. "+SAMRecordPartition.OPT_DESC)
 	private SAMRecordPartition partition=SAMRecordPartition.sample;
-	@Parameter(names={"-m"},description="method, how should I get the read length ?")
+	@Parameter(names={"-m"},description="method, how should I get the read/insert length ?")
 	private Method method = Method.SEQ_LENGTH;
-	@Parameter(names={"-w","--windows"},description=RangeOfIntegers.OPT_DESC,converter=RangeOfIntegers.StringConverter.class)
-	private RangeOfIntegers ranges = new RangeOfIntegers(0,10,20,30,40,50,100,150,200,250,300,350,400,450,500,1000);
 	@Parameter(names={"-R","--reference"},description="For reading CRAM. "+INDEXED_FASTA_REFERENCE_DESCRIPTION)
 	private Path faidx = null;
+	@Parameter(names={"-Q","--mapq"},description="min MAPQ")
+	private int mapq = 0;
+	@Parameter(names={"--regions"},description="Limit analysis to this interval. "+ IntervalListProvider.OPT_DESC,splitter=NoSplitter.class,converter=IntervalListProvider.StringConverter.class)
+	protected IntervalListProvider regionFiles = null;
+	@Parameter(names={"-w","--windows"},description="Histogram windows. " + RangeOfIntegers.OPT_DESC,converter=RangeOfIntegers.StringConverter.class,splitter=NoSplitter.class)
+	private RangeOfIntegers ranges = new RangeOfIntegers(0,10,20,30,40,50,100,150,200,250,300,350,400,450,500,1000);
 
-	private final Map<String,Counter<RangeOfIntegers.Range>> lengths=new TreeMap<>();
 
+	private final Map<String,DiscreteMedian<Integer>> sample2discreteMedian =new TreeMap<>();
+	
 	
     public SamReadLengthDistribution()
     	{
     	}		
 
-  
+    /** create input SAMRecord iterator */
+    private CloseableIterator<SAMRecord> openSamIterator(final SamReader sr) {
+    	
+    	if(this.regionFiles!=null) {
+    		this.regionFiles.dictionary(SequenceDictionaryUtils.extractRequired(sr.getFileHeader()));
+
+    		if(!sr.hasIndex()) {
+    			final List<Interval> L = this.regionFiles.stream().map(x->new Interval(x)).collect(Collectors.toList());
+    			final SAMRecordIterator st0 = sr.iterator();
+    			return new FilteringSamIterator(st0,new IntervalFilter(L, sr.getFileHeader()));
+    			}
+    		else
+    			{
+    			return sr.query(this.regionFiles.optimizedQueryIntervals(), false);
+    			}
+    		}
+    	return sr.iterator();
+    	}
     
-    private void scan(final SamReader in,String fname) throws IOException
+    private void scan(final SamReader in,Path pathName) throws IOException
     	{
-    	final String defName = fname+"#"+this.partition.name();
+    	final String defName = (pathName==null?"STDIN":pathName.toString())+"#"+this.partition.name();
     	in.getFileHeader().getReadGroups().
     		stream().
     		map(RG->this.partition.apply(RG)).
     		map(S->StringUtils.isBlank(S)?defName:S).
-    		filter(S->!this.lengths.containsKey(S)).
-    		forEach(S->this.lengths.put(S,new Counter<>()));
+    		filter(S->!this.sample2discreteMedian.containsKey(S)).
+    		forEach(S->this.sample2discreteMedian.put(S,new DiscreteMedian<Integer>()));
     	
     	
-    	final SAMRecordIterator iter=in.iterator();
+    	final CloseableIterator<SAMRecord> iter=openSamIterator(in);
     	while(iter.hasNext())
 			{
     		final SAMRecord rec = iter.next();
+    		if(rec.getReadFailsVendorQualityCheckFlag()) continue;
+    		if(rec.getDuplicateReadFlag()) continue;
+    		if(rec.isSecondaryOrSupplementary()) continue;
+    		if(!rec.getReadUnmappedFlag() && rec.getMappingQuality()<this.mapq) continue;
+    		
     		final String sampleName= this.partition.getPartion(rec,defName);
     		
-    		Counter<RangeOfIntegers.Range> counter = this.lengths.get(sampleName);
+    		DiscreteMedian<Integer> counter = this.sample2discreteMedian.get(sampleName);
     		if(counter==null) {
-    			counter =  new Counter<>();
-    			this.lengths.put(sampleName,counter);
+    			counter =  new DiscreteMedian<>();
+    			this.sample2discreteMedian.put(sampleName,counter);
     			}
+    		
     		
     		final int len;
     		switch(this.method) {
@@ -159,15 +179,24 @@ public class SamReadLengthDistribution extends Launcher
     			case CIGAR_REF_LENGTH: {
     				if(rec.getReadUnmappedFlag()) continue;
     				final Cigar c=rec.getCigar();
-    				if(c==null) continue;
+    				if(c==null || c.isEmpty()) continue;
     				len = c.getReferenceLength();
     				break;
     				}
     			case CIGAR_PADDED_REF_LENGTH: {
     				if(rec.getReadUnmappedFlag()) continue;
     				final Cigar c=rec.getCigar();
-    				if(c==null) continue;
+    				if(c==null || c.isEmpty()) continue;
     				len = c.getPaddedReferenceLength();
+    				break;
+    				}
+    			case INSERT_LENGTH: {
+    				if(rec.getReadUnmappedFlag()) continue;
+    				if(!rec.getReadPairedFlag()) continue;
+    				if(rec.getMateUnmappedFlag()) continue;
+    				if(!rec.getContig().equals(rec.getMateReferenceName())) continue;
+    				if(!rec.getFirstOfPairFlag()) continue;//ignore 2nd
+    				len = Math.abs(rec.getInferredInsertSize());
     				break;
     				}
     			default: throw new IllegalStateException("unsupported method " +this.method);
@@ -175,7 +204,7 @@ public class SamReadLengthDistribution extends Launcher
     		
     		
     		
-    		counter.incr(this.ranges.getRange(len));    		
+    		counter.add(len);    		
 			}
     	iter.close();
     	}
@@ -184,12 +213,11 @@ public class SamReadLengthDistribution extends Launcher
 	@Override
 	public int doWork(final List<String> inputs)
 		{
-		this.lengths.clear();
 		SamReader r= null;
-		PrintWriter out = null;
+		
 		try
 			{
-			final Set<String> args = IOUtils.unrollFiles(inputs);
+			final List<Path> args = IOUtils.unrollPaths(inputs);
 			final SamReaderFactory srf = super.createSamReaderFactory();
 			if(this.faidx!=null) srf.referenceSequence(this.faidx);
 			
@@ -197,45 +225,49 @@ public class SamReadLengthDistribution extends Launcher
 			if(args.isEmpty())
 				{
 				r = srf.open(SamInputResource.of(stdin()));
-				scan(r,"<STDIN>");
+				scan(r,null);
 				r.close();
 				}
 			else
 				{
-				for(final String filename: args)
+				for(final Path filename: args)
 					{
 					r = srf.open(SamInputResource.of(filename));
 					scan(r,filename);
 					r.close();
 					}
 				}
-			out=super.openPathOrStdoutAsPrintWriter(this.outputFile);
-			out.print("#ReadLength");
-			for(final String sample:this.lengths.keySet())
-				{
-				out.print("\t");
-				out.print(sample);
-				}
-			out.println();
-			
-					
-			
-			for(final RangeOfIntegers.Range L:this.ranges.getRanges()) {
-				out.print(L);
-				for(final String sample:this.lengths.keySet())
-					{
-					final Counter<RangeOfIntegers.Range> c=this.lengths.get(sample);
+			try(PrintWriter out=super.openPathOrStdoutAsPrintWriter(this.outputFile)) {
+				final String mtName = this.method.name();
+				out.print("#"+this.partition.name()+"\tCOUNT-"+mtName+"\tMIN-"+mtName+"\tMAX-"+mtName+"\tAVG-"+mtName+"\tMEDIAN-"+mtName);
+				for(final RangeOfIntegers.Range L:this.ranges.getRanges()) {
 					out.print("\t");
-					out.print(c.count(L));
+					out.print(L.toString());
 					}
 				out.println();
+				for(final String sample:this.sample2discreteMedian.keySet())
+					{
+					final DiscreteMedian<Integer> count = this.sample2discreteMedian.get(sample);
+					out.print(sample);
+					out.print("\t");
+					out.print(count.size());
+					out.print("\t");
+					out.print(count.isEmpty()?".":String.valueOf((int)count.getMin().getAsDouble()));
+					out.print("\t");
+					out.print(count.isEmpty()?".":String.valueOf((int)count.getMax().getAsDouble()));
+					out.print("\t");
+					out.print(count.isEmpty()?".":String.format("%.2f",count.getAverage().getAsDouble()));
+					out.print("\t");
+					out.print(count.isEmpty()?".":String.format("%.2f",count.getMedian().getAsDouble()));
+					for(final RangeOfIntegers.Range L:this.ranges.getRanges()) {
+						out.print("\t");
+						out.print(count.getAsDoubleStream().filter(I->L.contains((int)I)).count());
+						}
+					out.println();
+					}
+				out.flush();
 				}
-			
-			
-			out.flush();
-			out.close();
-			out = null;
-			return RETURN_OK;
+			return 0;
 			}
 		catch(final Throwable err)
 			{
@@ -244,7 +276,6 @@ public class SamReadLengthDistribution extends Launcher
 			}
 		finally
 			{
-			CloserUtil.close(out);
 			}
 		}
 	
