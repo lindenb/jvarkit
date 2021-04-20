0286e0e28ec4f76b060bd122d96badcff46368a6
Pierre Lindenbaum <plindenbaum@yahoo.fr>
2021-04-20 20:47:46 +0200

    cont

--- src/main/java/com/github/lindenb/jvarkit/tools/vcfviewgui/SwingBamCov.java
+++ src/main/java/com/github/lindenb/jvarkit/tools/vcfviewgui/SwingBamCov.java
@@ -50,7 +50,9 @@ import java.util.Collections;
 import java.util.List;
 import java.util.Optional;
 import java.util.OptionalDouble;
+import java.util.OptionalInt;
 import java.util.Vector;
+import java.util.function.ToDoubleFunction;
 import java.util.stream.Collectors;
 
 import javax.imageio.ImageIO;
@@ -65,6 +67,7 @@ import javax.swing.JMenuBar;
 import javax.swing.JMenuItem;
 import javax.swing.JOptionPane;
 import javax.swing.JPanel;
+import javax.swing.JProgressBar;
 import javax.swing.JScrollPane;
 import javax.swing.JSeparator;
 import javax.swing.JTabbedPane;
@@ -130,6 +133,9 @@ public class SwingBamCov extends Launcher
 			}
 		}
 	
+	
+
+	
 	@SuppressWarnings("serial")
 	private static class XFrame extends JFrame {
 		final Path referenceFile;
@@ -138,38 +144,244 @@ public class SwingBamCov extends Launcher
 		final JTable jtableBams;
 		final JPanel drawingArea;
 		final JTextField jtextFieldLocation;
+		final JTextField jtextFieldCap;
 		final int minMapq;
-		final SamReaderFactory srf;
+		Thread drawingThread = null;
+		BufferedImage offScreenImage = null;
+		JProgressBar progressBar = null;
 		
-		private class ZoomAction extends AbstractAction {
+		
+		
+		private abstract class ChangeViewAction extends AbstractAction {
 			final double factor;
-			ZoomAction(double factor) {
-				super("x"+factor);
+			ChangeViewAction(String title,double factor) {
+				super(title);
 				this.factor=factor;
+				}
+			@Override
+			public Object getValue(String key)
+				{
+				if(key.equals(AbstractAction.SHORT_DESCRIPTION)) return getShortDesc();
+				return super.getValue(key);
 				}
 			@Override
 			public void actionPerformed(final ActionEvent e)
 				{
 				final Optional<SimpleInterval> optR = getUserInterval();
 				if(!optR.isPresent()) return;
-				final SimpleInterval r= optR.get();
+				Locatable r= optR.get();
 				final SAMSequenceRecord ssr=dict.getSequence(r.getContig());
 				if(ssr==null) return;
+				r= change(ssr,r);
+				if(r==null) return;
+				jtextFieldLocation.setText(r.getContig()+":"+r.getStart()+"-"+r.getEnd());
+				XFrame.this.offScreenImage=null;
+				XFrame.this.drawingThread=null;
+				drawingArea.repaint();
+				}
+			abstract Locatable change(SAMSequenceRecord ssr,Locatable loc);
+			abstract String getShortDesc();
+			}
+		
+		
+		private class ZoomAction extends ChangeViewAction {
+			ZoomAction(double factor) {
+				super("x"+factor,factor);
+				}
+			@Override
+			Locatable change(SAMSequenceRecord ssr, Locatable r)
+				{
 				final double L= r.getLengthOnReference()/2.0*factor;
 				final double mid = r.getStart()+r.getLengthOnReference()/2;
 				int x1= (int)Math.max(1,mid-L);
 				int x2= (int)Math.min(mid+L,ssr.getLengthOnReference());
-				jtextFieldLocation.setText(r.getContig()+":"+x1+"-"+x2);
-				drawingArea.repaint();
+				return new SimpleInterval(r.getContig(),x1,x2);
+				}
+			@Override
+			String getShortDesc()
+				{
+				return "Scale by "+factor;
 				}
 			}
+		
+		private class ShiftAction extends ChangeViewAction {
+			ShiftAction(String title,double factor) {
+				super(title,factor);
+				}
+			@Override
+			Locatable change(SAMSequenceRecord ssr, Locatable r)
+				{
+				final double L= r.getLengthOnReference()*Math.abs(factor);
+				int x1,x2;
+				if(factor<0) {
+					x1 = Math.max(1,r.getStart() - (int)L);
+					x2 = Math.min(ssr.getLengthOnReference(),x1 + r.getLengthOnReference());
+					}
+				else
+					{
+					x2 = Math.min(ssr.getLengthOnReference(),r.getEnd() + (int)L);
+					x1 = Math.max(1,x2 -r.getLengthOnReference());
+					}
+				return new SimpleInterval(r.getContig(),x1,x2);
+				}
+			@Override
+			String getShortDesc()
+				{
+				return "Shift by "+factor;
+				}
+			}
+		
+		
+	private class DrawingThread extends Thread {
+			final BufferedImage img;
+			final SimpleInterval location;
+			final OptionalInt optCap;
+			final List<BamInfo> bamInfos;
+			final SamReaderFactory srf;
+			DrawingThread(
+					BufferedImage img,
+					final SimpleInterval location,
+					final OptionalInt optCap,
+					final List<BamInfo> bamInfos
+					) {
+				this.img = img;
+				this.location=location;
+				this.optCap = optCap;
+				this.bamInfos = bamInfos;
+				this.srf = SamReaderFactory.makeDefault().
+						referenceSequence(XFrame.this.referenceFile).
+						validationStringency(ValidationStringency.LENIENT);
+				}
+			@Override
+			public void run()
+				{
+				final Graphics2D g = this.img.createGraphics();
+				paintDrawingArea(g);
+				g.dispose();
+				}
+			
+			private boolean isCurrentThread() {
+				return XFrame.this.drawingThread==this &&
+					XFrame.this.offScreenImage==this.img;
+				}
+			private void repaintDrawingArea() {
+				if(!isCurrentThread()) return;
+				try {
+					SwingUtilities.invokeAndWait(()->{
+						XFrame.this.repaint();
+						});
+					} catch(final Throwable err) {LOG.error(err);}
+				}
+			
+			private void paintDrawingArea(final Graphics2D g) {
+				g.setRenderingHint(RenderingHints.KEY_ANTIALIASING, RenderingHints.VALUE_ANTIALIAS_ON);
+				g.setColor(Color.WHITE);
+				g.fillRect(0, 0, this.img.getWidth(), this.img.getHeight());
+				if(location==null) return;
+				final  int marginTop=12;
+				g.setColor(Color.DARK_GRAY);
+				g.drawString(location.toNiceString()+" length:"+ StringUtils.niceInt(location.getLengthOnReference())+"bp " +
+						SequenceDictionaryUtils.getBuildName(XFrame.this.dict).orElse(XFrame.this.referenceFile.getFileName().toString())+" MAPQ:"+XFrame.this.minMapq, 3, marginTop-1);
+				
+				final int ncols = (int)Math.max(1,Math.floor(Math.sqrt(this.bamInfos.size())));
+				final int nrows = (int)Math.max(1, Math.ceil(this.bamInfos.size()/(double)ncols));
+				final double wi  = this.img.getWidth()/ncols;
+				final double hi  = (this.img.getHeight()-marginTop)/nrows;
+				int nr=0;
+				int nc=0;
+				
+				for(final BamInfo bamPath: this.bamInfos) {
+					if(!isCurrentThread()) break;
+					paintBam(g,this.srf,bamPath,this.location,new Rectangle2D.Double(
+							nc*wi,
+							marginTop+nr*hi,
+							wi,
+							hi));
+					nc++;
+					if(nc==ncols) {
+						nc=0;
+						nr++;
+						}
+					}
+				if(!isCurrentThread()) return;
+				try {
+					SwingUtilities.invokeAndWait(()->{
+						XFrame.this.progressBar.setIndeterminate(false);
+						});
+					} catch(final Throwable err) {LOG.error(err);}
+				}
+		
+		private void paintBam(final Graphics2D g,
+				final SamReaderFactory srf,
+				final BamInfo bam,
+				final Locatable loc
+				,final Rectangle2D rect) {
+			try {
+				try(SamReader sr=this.srf.open(bam.bamPath)) {
+					if(!sr.hasIndex()) return;					
+					final CoverageFactory covFactory = new CoverageFactory().
+							setMappingQuality(XFrame.this.minMapq);
+					final CoverageFactory.SimpleCoverage cov = covFactory.getSimpleCoverage(sr, loc, bam.sample);
+					
+					final double[] depths = cov.scaleMedian((int)rect.getWidth());
+					final double maxDepth0 = Arrays.stream(depths).max().orElse(1.0);
+					final double maxDepth = this.optCap.isPresent()?
+							Math.min(optCap.getAsInt(), maxDepth0):
+							maxDepth0;
+					final ToDoubleFunction<Double> toYPixel = DP ->{
+						double y = rect.getMaxY() - (DP/maxDepth)*rect.getHeight();
+						return y;
+					};
+					
+					
+					final GeneralPath gp = new GeneralPath();
+					gp.moveTo(rect.getX(), rect.getMaxY());
+					for(int i=0;i< depths.length;i++) {
+						double y = toYPixel.applyAsDouble(depths[i]);
+						if(y<rect.getY()) y = rect.getY();//capping
+						gp.lineTo(rect.getX()+i, y );
+						}
+					gp.lineTo(rect.getMaxX(),rect.getMaxY());
+					gp.closePath();
+					g.setColor(Color.LIGHT_GRAY);
+					g.fill(gp);
+					
+					OptionalDouble mean = cov.getMedian();
+					if(mean.isPresent()) {
+						g.setColor(Color.RED);
+						final double y = toYPixel.applyAsDouble(mean.getAsDouble());
+						if(y>=rect.getY()) g.draw(new Line2D.Double(rect.getX(), y, rect.getMaxX(), y));
+						}
+					
+					mean = cov.getAverage();
+					if(mean.isPresent()) {
+						g.setColor(Color.GREEN);
+						final double y = toYPixel.applyAsDouble(mean.getAsDouble());
+						if(y>=rect.getY()) g.draw(new Line2D.Double(rect.getX(), y, rect.getMaxX(), y));
+						}
+					
+					g.setColor(Color.BLUE);
+					int fontSize=12;
+					g.setFont(new Font("Courier",Font.PLAIN,fontSize));
+					g.drawString(bam.sample+" (max DP: " + (int)maxDepth0+")",(int)rect.getX()+5,(int)rect.getMaxY()-5);
+					//frame
+					g.setColor(Color.DARK_GRAY);
+					g.draw(rect);
+					}
+				repaintDrawingArea();
+				}
+			catch(final Throwable err) {
+				LOG.error(err);
+				}
+			}
+		}
 		
 		
 		XFrame(final Path referenceFile,final List<Path> bamPaths,String defaultLoc,int minMapq) {
 			super.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
 			setTitle(SwingBamCov.class.getSimpleName());
 			this.referenceFile = referenceFile;
-			this.srf = SamReaderFactory.makeDefault().
+			final SamReaderFactory srf = SamReaderFactory.makeDefault().
 					referenceSequence(this.referenceFile).
 					validationStringency(ValidationStringency.LENIENT);
 			this.dict = SequenceDictionaryUtils.extractRequired(this.referenceFile);
@@ -177,7 +389,7 @@ public class SwingBamCov extends Launcher
 					collect(Collectors.toCollection(Vector::new));
 					new Vector<BamInfo>(bamPaths.size());
 			for(final BamInfo bi:this.bamPaths) {
-				try(SamReader sr=this.srf.open(bi.bamPath)) {
+				try(SamReader sr= srf.open(bi.bamPath)) {
 					final SAMFileHeader header = sr.getFileHeader();
 					bi.sample = header.getReadGroups().stream().
 							map(S->S.getSample()).
@@ -200,19 +412,21 @@ public class SwingBamCov extends Launcher
 			mainPane.add(topPane,BorderLayout.NORTH);
 			JLabel label = new JLabel("Location:", JLabel.RIGHT);
 			topPane.add(label);
-			jtextFieldLocation= new JTextField(defaultLoc,20);
+			this.jtextFieldLocation= new JTextField(defaultLoc,20);
 			if(StringUtil.isBlank(defaultLoc) &&  !dict.isEmpty()) {
 				final SAMSequenceRecord ssr= dict.getSequence(0);
-				jtextFieldLocation.setText(ssr.getSequenceName()+":1-1000");
+				this.jtextFieldLocation.setText(ssr.getSequenceName()+":1-1000");
 				}
 			
-			topPane.add(jtextFieldLocation);
-			label.setLabelFor(jtextFieldLocation);
+			topPane.add(this.jtextFieldLocation);
+			label.setLabelFor(this.jtextFieldLocation);
 			final Action actionGo = new AbstractAction("Plot")
 				{
 				@Override
 				public void actionPerformed(ActionEvent e)
 					{
+					offScreenImage=null;
+					drawingThread=null;
 					drawingArea.repaint();
 					}
 				};
@@ -220,10 +434,32 @@ public class SwingBamCov extends Launcher
 			JButton button = new JButton(actionGo);
 			topPane.add(button);
 			topPane.add(new JSeparator());
+			label = new JLabel("Cap:", JLabel.RIGHT);
+			topPane.add(label);
+			this.jtextFieldCap= new JTextField("",3);
+			topPane.add(this.jtextFieldCap);
+			label.setLabelFor(this.jtextFieldCap);
+			this.jtextFieldCap.addActionListener(actionGo);
+
+			
+			topPane.add(new JSeparator());
 			topPane.add(new JButton(new ZoomAction(0.5)));
 			topPane.add(new JButton(new ZoomAction(1.5)));
 			topPane.add(new JButton(new ZoomAction(2.0)));
 			topPane.add(new JButton(new ZoomAction(10)));
+			topPane.add(new JSeparator());
+			topPane.add(new JButton(new ShiftAction("<<<",-0.9)));
+			topPane.add(new JButton(new ShiftAction("<<",-0.5)));
+			topPane.add(new JButton(new ShiftAction("<",-0.1)));
+			topPane.add(new JButton(new ShiftAction(">",0.1)));
+			topPane.add(new JButton(new ShiftAction(">>",0.5)));
+			topPane.add(new JButton(new ShiftAction(">>>",0.9)));
+
+			final Panel botPane = new Panel(new FlowLayout(FlowLayout.TRAILING));
+			mainPane.add(botPane, BorderLayout.SOUTH);
+			this.progressBar=new JProgressBar();
+			botPane.add(this.progressBar);
+
 			
 			
 			this.drawingArea = new JPanel(true) {
@@ -328,9 +564,9 @@ public class SwingBamCov extends Launcher
 				return;
 				}
 			try {
-				BufferedImage img = new BufferedImage(drawingArea.getWidth(), drawingArea.getHeight(), BufferedImage.TYPE_INT_RGB);
-				Graphics2D g = img.createGraphics();
-				paintComponents(g);
+				final BufferedImage img = new BufferedImage(drawingArea.getWidth(), drawingArea.getHeight(), BufferedImage.TYPE_INT_RGB);
+				final Graphics2D g = img.createGraphics();
+				drawingArea.paintComponents(g);
 				g.dispose();
 				ImageIO.write(img,f.getName().toLowerCase().endsWith(".png")?"PNG":"JPG", f);
 				}
@@ -341,101 +577,43 @@ public class SwingBamCov extends Launcher
 			}
 		
 		private void paintDrawingArea(final Graphics2D g) {
-			g.setRenderingHint(RenderingHints.KEY_ANTIALIASING, RenderingHints.VALUE_ANTIALIAS_ON);
-			g.setColor(Color.WHITE);
-			g.fillRect(0, 0, drawingArea.getWidth(), drawingArea.getHeight());
-			int marginTop=12;
-			
-			final Optional<SimpleInterval> location = getUserInterval();
-			if(!location.isPresent()) return;
-			g.setColor(Color.DARK_GRAY);
-			g.drawString(location.get().toNiceString()+" length:"+ StringUtils.niceInt(location.get().getLengthOnReference())+"bp " +
-					SequenceDictionaryUtils.getBuildName(dict).orElse(referenceFile.getFileName().toString())+" MAPQ:"+this.minMapq, 3, marginTop-1);
-			
-			final List<BamInfo> paths;
-			if(this.jtableBams.getSelectedRowCount()==0) {
-				paths = this.bamPaths;
-				} else {
-				int[] selidx = this.jtableBams.getSelectedRows();
-				paths = new Vector<>(selidx.length);
-				for(int idx:selidx) {
-					paths.add(bamPaths.get(idx));
+			if(offScreenImage==null || offScreenImage.getWidth()!=this.drawingArea.getWidth() ||
+				offScreenImage.getHeight()!=this.drawingArea.getHeight())
+				{
+				final Optional<SimpleInterval> location = getUserInterval();
+				String capStr = this.jtextFieldCap.getText();
+				OptionalInt optCap = OptionalInt.empty();
+				if(StringUtils.isInteger(capStr)) {
+					optCap = OptionalInt.of(Integer.parseInt(capStr));
 					}
-				}
-			if(paths.isEmpty()) return;
-			int ncols = (int)Math.max(1,Math.floor(Math.sqrt(paths.size())));
-			int nrows = (int)Math.max(1, Math.ceil(paths.size()/(double)ncols));
-			double wi  = drawingArea.getWidth()/ncols;
-			double hi  = (drawingArea.getHeight()-marginTop)/nrows;
-			int nr=0;
-			int nc=0;
-			
-			for(final BamInfo bamPath: paths) {
-				paintBam(g,srf,bamPath,location.get(),new Rectangle2D.Double(
-						nc*wi,
-						marginTop+nr*hi,
-						wi,
-						hi));
-				nc++;
-				if(nc==ncols) {
-					nc=0;
-					nr++;
+				
+				
+				
+				offScreenImage = new BufferedImage(this.drawingArea.getWidth(), this.drawingArea.getHeight(), BufferedImage.TYPE_INT_RGB);
+				
+				final List<BamInfo> paths;
+				if(this.jtableBams.getSelectedRowCount()==0) {
+					paths = this.bamPaths;
+					} 
+				else {
+					final int[] selidx = this.jtableBams.getSelectedRows();
+					paths = new Vector<>(selidx.length);
+					for(int idx:selidx) {
+						paths.add(bamPaths.get(idx));
+						}
 					}
+				drawingThread = new DrawingThread(this.offScreenImage,
+						location.orElse(null),
+						optCap,
+						paths);
+				this.progressBar.setIndeterminate(true);
+				drawingThread.start();
 				}
-			}
-		
-		private void paintBam(final Graphics2D g,
-				final SamReaderFactory srf,
-				final BamInfo bam,
-				final Locatable loc
-				,final Rectangle2D rect) {
-			try {
-				try(SamReader sr=this.srf.open(bam.bamPath)) {
-					if(!sr.hasIndex()) return;					
-					final CoverageFactory covFactory = new CoverageFactory().
-							setMappingQuality(this.minMapq);
-					final CoverageFactory.SimpleCoverage cov = covFactory.getSimpleCoverage(sr, loc, bam.sample);
-					
-					final double[] depths = cov.scaleMedian((int)rect.getWidth());
-					final double maxDepth = Arrays.stream(depths).max().orElse(1.0);
-					final GeneralPath gp = new GeneralPath();
-					gp.moveTo(rect.getX(), rect.getMaxY());
-					for(int i=0;i< depths.length;i++) {
-						double y = rect.getMaxY() - (depths[i]/maxDepth)*rect.getHeight();
-						gp.lineTo(rect.getX()+i, y);
-						}
-					gp.lineTo(rect.getMaxX(),rect.getMaxY());
-					gp.closePath();
-					g.setColor(Color.LIGHT_GRAY);
-					g.fill(gp);
-					
-					OptionalDouble mean = cov.getMedian();
-					if(mean.isPresent()) {
-						g.setColor(Color.RED);
-						double y = rect.getMaxY() -(mean.getAsDouble()/maxDepth)*rect.getHeight();
-						g.draw(new Line2D.Double(rect.getX(), y, rect.getMaxX(), y));
-						}
-					
-					mean = cov.getAverage();
-					if(mean.isPresent()) {
-						g.setColor(Color.GREEN);
-						double y = rect.getMaxY() -(mean.getAsDouble()/maxDepth)*rect.getHeight();
-						g.draw(new Line2D.Double(rect.getX(), y, rect.getMaxX(), y));
-						}
-					
-					g.setColor(Color.BLUE);
-					int fontSize=12;
-					g.setFont(new Font("Courier",Font.PLAIN,fontSize));
-					g.drawString(bam.sample+"(max DP: " + (int)maxDepth+")",(int)rect.getX()+5,(int)rect.getMaxY()-5);
-					//frame
-					g.setColor(Color.DARK_GRAY);
-					g.draw(rect);
-					}
+			else
+				{
+				g.drawImage(this.offScreenImage,0,0,null);
 				}
-			catch(final Throwable err) {
-				LOG.error(err);
-				}
-			}
+			}		
 		}
 	
 	@Override
