a1d756018bff6e16c3937641ef13c94842656fa9
Pierre Lindenbaum <plindenbaum@yahoo.fr>
2021-05-04 17:36:01 +0200

    cont

--- src/main/java/com/github/lindenb/jvarkit/tools/vcfviewgui/SwingVcfView.java
+++ src/main/java/com/github/lindenb/jvarkit/tools/vcfviewgui/SwingVcfView.java
@@ -26,17 +26,15 @@ SOFTWARE.
 package com.github.lindenb.jvarkit.tools.vcfviewgui;
 
 import java.awt.BorderLayout;
-import java.awt.Component;
 import java.awt.Dimension;
 import java.awt.FlowLayout;
 import java.awt.Panel;
 import java.awt.Toolkit;
 import java.awt.event.ActionEvent;
-import java.awt.event.MouseAdapter;
-import java.awt.event.MouseEvent;
 import java.awt.event.WindowAdapter;
 import java.awt.event.WindowEvent;
 import java.io.File;
+import java.io.PrintWriter;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.util.ArrayList;
@@ -60,6 +58,7 @@ import javax.swing.JLabel;
 import javax.swing.JMenu;
 import javax.swing.JMenuBar;
 import javax.swing.JMenuItem;
+import javax.swing.JOptionPane;
 import javax.swing.JPanel;
 import javax.swing.JScrollPane;
 import javax.swing.JSeparator;
@@ -70,9 +69,10 @@ import javax.swing.JTextField;
 import javax.swing.ListSelectionModel;
 import javax.swing.SwingUtilities;
 import javax.swing.border.EmptyBorder;
-
+import javax.swing.table.TableModel;
 
 import com.beust.jcommander.Parameter;
+import com.github.lindenb.jvarkit.io.IOUtils;
 import com.github.lindenb.jvarkit.lang.StringUtils;
 import com.github.lindenb.jvarkit.pedigree.Pedigree;
 import com.github.lindenb.jvarkit.pedigree.PedigreeParser;
@@ -157,7 +157,6 @@ public class SwingVcfView extends Launcher
 		final GenotypeTypeTableModel genotypeTypeTableModel;
 		final Map<VariantContext.Type, JCheckBox> variantType2cbox = new HashMap<>();
 		final Map<GenotypeType, JCheckBox> genotypeType2cbox = new HashMap<>();
-		JTable lastSelectedJtable = null;
 		
 		XFrame(final Path vcfPath,String defaultLoc,
 				final int limit_number_variant,
@@ -174,23 +173,6 @@ public class SwingVcfView extends Launcher
 			
 			final JPanel mainPane = new JPanel(new BorderLayout(5, 5));
 			mainPane.setBorder(new EmptyBorder(5, 5, 5, 5));
-			mainPane.addMouseListener(new MouseAdapter()
-				{
-				@Override
-				public void mouseClicked(MouseEvent e)
-					{
-					Component c= e.getComponent();
-					while(c!=null) {
-						if(c instanceof JTable) {
-							lastSelectedJtable = JTable.class.cast(c);
-							break;
-							}
-						if(c.getParent()==null) break;
-						if(!(c.getParent() instanceof Component)) break;
-						c = Component.class.cast(c.getParent());
-						}
-					}
-				});
 			this.setContentPane(mainPane);
 			final Panel topPane = new Panel(new FlowLayout(FlowLayout.LEADING));
 			mainPane.add(topPane,BorderLayout.NORTH);
@@ -222,6 +204,7 @@ public class SwingVcfView extends Launcher
 			}
 			for(VariantContext.Type vtype: VariantContext.Type.values()) {
 				final JCheckBox cb = new JCheckBox(vtype.name(),true);
+				cb.setToolTipText("Filter Variants of type "+ vtype);
 				topPane.add(cb);
 				cb.addActionListener(AE->refreshInterval());
 				this.variantType2cbox.put(vtype, cb);
@@ -229,6 +212,7 @@ public class SwingVcfView extends Launcher
 			topPane.add(new JSeparator());
 			label= new JLabel("JEXL:");
 			this.jtextFieldGEXL = new JTextField(10);
+			this.jtextFieldGEXL.setToolTipText("Filter variants using a JEXL expression.");
 			label.setLabelFor(this.jtextFieldGEXL);
 			topPane.add(label);
 			topPane.add(this.jtextFieldGEXL);
@@ -248,14 +232,15 @@ public class SwingVcfView extends Launcher
 			this.variantTable =  new JTable(this.swingVariantsTableModel);
 			this.swingVCFGenotypesTableModel = new SwingVCFGenotypesTableModel(this.pedigree);
 			final JTable genotypeTable =  new JTable(this.swingVCFGenotypesTableModel);
+			genotypeTable.setAutoResizeMode(JTable.AUTO_RESIZE_OFF);
 			
 			final JPanel gtPane = new JPanel(new BorderLayout(1,1));
-			gtPane.setBorder(BorderFactory.createTitledBorder("Genotypes"));
 			final JPanel topPane2 = new JPanel(new FlowLayout(FlowLayout.LEADING));
 			gtPane.add(topPane2,BorderLayout.NORTH);
 			
 			for(GenotypeType gttype: GenotypeType.values()) {
 				final JCheckBox cb = new JCheckBox(gttype.name(),true);
+				cb.setToolTipText("Filter Genotype of type "+gttype);
 				topPane2.add(cb);
 				cb.addActionListener(AE->refreshTables());
 				this.genotypeType2cbox.put(gttype, cb);
@@ -267,15 +252,20 @@ public class SwingVcfView extends Launcher
 			label.setLabelFor(this.txtFieldSample);
 			topPane2.add(this.txtFieldSample);
 			this.txtFieldSample.addActionListener(AE->refreshTables());
+			topPane2.add(new JSeparator());
+			JButton save = new JButton("*");
+			save.addActionListener(AE->saveTable("Genotypes",genotypeTable));
+			save.setPreferredSize(new Dimension(5,5));
+			topPane2.add(save);
 			
 			gtPane.add(new JScrollPane(genotypeTable),BorderLayout.CENTER);
 			
 			final JSplitPane split1 = new JSplitPane(
 					JSplitPane.HORIZONTAL_SPLIT,
-					new JScrollPane(variantTable),
+					wrapTable("Variants",variantTable),
 					gtPane
 					);
-			
+			split1.setResizeWeight(0.5);
 			
 			this.swingInfoTableModel = new SwingVCFInfoTableModel();
 			
@@ -286,11 +276,17 @@ public class SwingVcfView extends Launcher
 			
 			
 			final JTabbedPane tabbed2 = new JTabbedPane();
-			tabbed2.addTab("INFO",new JScrollPane(new JScrollPane( new JTable(this.swingInfoTableModel))));
-			tabbed2.addTab("Types",new JScrollPane(new JScrollPane(new JTable(this.genotypeTypeTableModel))));
-			tabbed2.addTab("SnpEff:ANN",new JScrollPane(new JTable(this.swingAnnPredictionTableModel)));
-			tabbed2.addTab("VEP",new JScrollPane(new JTable(this.swingVepPredictionTableModel)));
-			tabbed2.addTab("bcftools:BCSQ",new JScrollPane(new JTable(this.swingBcsqPredictionTableModel)));
+			tabbed2.addTab("INFO",wrapTable("INFO",new JTable(this.swingInfoTableModel)));
+			tabbed2.addTab("Types",wrapTable("Types",new JTable(this.genotypeTypeTableModel)));
+			final JTable snpEffTable  = new JTable(this.swingAnnPredictionTableModel);
+			snpEffTable.setAutoResizeMode(JTable.AUTO_RESIZE_OFF);
+			tabbed2.addTab("SnpEff:ANN",wrapTable("ANN",snpEffTable));
+			final JTable vepTable = new JTable(this.swingVepPredictionTableModel);
+			vepTable.setAutoResizeMode(JTable.AUTO_RESIZE_OFF);
+			tabbed2.addTab("VEP",wrapTable("VEP",vepTable));
+			final JTable bcfTable  = new JTable(this.swingBcsqPredictionTableModel);
+			bcfTable.setAutoResizeMode(JTable.AUTO_RESIZE_OFF);
+			tabbed2.addTab("bcftools:BCSQ",wrapTable("BCSQ", bcfTable));
 			if(this.pedigree!=null && !this.pedigree.getTrios().isEmpty()) {
 				this.swingTrioTableModel = new SwingTrioTableModel(this.pedigree);
 				tabbed2.addTab("TRIOS",new JScrollPane(new JTable(this.swingTrioTableModel)));
@@ -305,17 +301,19 @@ public class SwingVcfView extends Launcher
 					split1,
 					tabbed2
 					);
+			split2.setResizeWeight(0.5);
+			
 			pane2.add(split2,BorderLayout.CENTER);
 			this.variantTable.setSelectionMode(ListSelectionModel.SINGLE_SELECTION);
 			this.variantTable.getSelectionModel().addListSelectionListener(E->refreshTables());
 			
 			tabPane.addTab("Main", pane2);
 			
-			tabPane.addTab("INFO", new JScrollPane( new JTable(new SwingVCFInfoHeaderLineTableModel(header))));
-			tabPane.addTab("FORMAT", new JScrollPane( new JTable(new SwingVCFFormatHeaderLineTableModel(header))));
-			tabPane.addTab("FILTER", new JScrollPane( new JTable(new SwingVCFFilterHeaderLineTableModel(header))));
+			tabPane.addTab("INFO", wrapTable("INFO", new JTable(new SwingVCFInfoHeaderLineTableModel(header))));
+			tabPane.addTab("FORMAT",wrapTable("FILTER",new JTable(new SwingVCFFormatHeaderLineTableModel(header))));
+			tabPane.addTab("FILTER",wrapTable("FILTER",new JTable(new SwingVCFFilterHeaderLineTableModel(header))));
 			if(this.pedigree!=null) {
-				tabPane.addTab("Pedigree", new JScrollPane( new JTable(new SwingPedigreeTableModel(this.pedigree))));
+				tabPane.addTab("Pedigree", wrapTable("Pedigree", new JTable(new SwingPedigreeTableModel(this.pedigree))));
 			}
 			
 			
@@ -340,14 +338,6 @@ public class SwingVcfView extends Launcher
 			setJMenuBar(menuBar);
 			final JMenu menu = new JMenu("File");
 			menuBar.add(menu);
-			menu.add(new JMenuItem(new AbstractAction("Save Table as...")
-				{
-				@Override
-				public void actionPerformed(ActionEvent e)
-					{
-					saveTable(lastSelectedJtable);
-					}
-				}));
 			menu.add(actionGo);
 			menu.add(new JSeparator());
 			menu.add(new JMenuItem(new AbstractAction("Quit")
@@ -461,13 +451,51 @@ public class SwingVcfView extends Launcher
 		
 		this.swingInfoTableModel.setAttributes(hash);
 		}
-	private void saveTable(JTable jtable) {
+		
+	private JPanel wrapTable(final String title,final JTable t) {
+		final JPanel pane = new JPanel(new BorderLayout(1,1));
+		final JPanel top = new JPanel(new FlowLayout(FlowLayout.TRAILING,1,1));
+		final JButton save = new JButton("[+]");
+		save.setPreferredSize(new Dimension(7,7));
+		save.setToolTipText("Save Table "+title+"...");
+		save.addActionListener(A->saveTable(title,t));
+		top.add(save);
+		
+		if(!StringUtils.isBlank(title)) {
+			pane.setBorder(BorderFactory.createTitledBorder(title));
+			}
+		pane.add(top,BorderLayout.NORTH);
+		pane.add(new JScrollPane(t),BorderLayout.CENTER);
+		
+		return pane;
+	}
+		
+	private void saveTable(final String title,final JTable jtable) {
 		if(jtable==null) return;
 		try {
-			JFileChooser fc = new JFileChooser();
+			final JFileChooser fc = new JFileChooser();
+			if(!StringUtils.isBlank(title)) {
+				fc.setSelectedFile(new File(title.replaceAll("[ \t]", "")+".tsv"));
+				}
 			if(fc.showSaveDialog(jtable)!=JFileChooser.APPROVE_OPTION) return;
 			final File f = fc.getSelectedFile();
-			System.err.println("ok");
+			if(f.exists() && JOptionPane.showConfirmDialog(jtable, "File "+f.getName()+" exists. Overwrite ?","Overwrite",JOptionPane.OK_CANCEL_OPTION)!=JOptionPane.OK_OPTION) return;
+			try(PrintWriter pw = IOUtils.openFileForPrintWriter(f)) {
+				final TableModel tm = jtable.getModel();
+				for(int x=0;x< tm.getColumnCount();x++) {
+					pw.print(x>0?"\t":"");
+					pw.print(tm.getColumnName(x));
+					}
+				pw.println();
+				for(int y=0;y< tm.getRowCount();y++) {
+					for(int x=0;x< tm.getColumnCount();x++) {
+						pw.print(x>0?"\t":"");
+						pw.print(tm.getValueAt(y,x));
+						}
+					pw.println();
+					}
+				pw.flush();
+				}
 			}
 		catch(Throwable err) {
 			ThrowablePane.show(jtable, err);
--- src/main/java/com/github/lindenb/jvarkit/variant/swing/SwingVepPredictionTableModel.java
+++ src/main/java/com/github/lindenb/jvarkit/variant/swing/SwingVepPredictionTableModel.java
@@ -1,3 +1,28 @@
+/*
+The MIT License (MIT)
+
+Copyright (c) 2021 Pierre Lindenbaum
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
+
+
+*/
 package com.github.lindenb.jvarkit.variant.swing;
 
 import java.util.ArrayList;
@@ -5,9 +30,6 @@ import java.util.Collections;
 import java.util.List;
 
 import com.github.lindenb.jvarkit.util.swing.AbstractGenericTable;
-import com.github.lindenb.jvarkit.util.vcf.predictions.AnnPredictionParser;
-import com.github.lindenb.jvarkit.util.vcf.predictions.AnnPredictionParser.AnnPrediction;
-import com.github.lindenb.jvarkit.util.vcf.predictions.AnnPredictionParserFactory;
 import com.github.lindenb.jvarkit.util.vcf.predictions.VepPredictionParser;
 import com.github.lindenb.jvarkit.util.vcf.predictions.VepPredictionParser.VepPrediction;
 import com.github.lindenb.jvarkit.util.vcf.predictions.VepPredictionParserFactory;
