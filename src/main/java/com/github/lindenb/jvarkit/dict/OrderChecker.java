7f5415fcc2c79316e8a30fdf5f00cbb46aaa264e
Pierre Lindenbaum <plindenbaum@yahoo.fr>
2022-01-25 09:17:06 +0100

    vcfroh

--- src/main/java/com/github/lindenb/jvarkit/dict/OrderChecker.java
+++ src/main/java/com/github/lindenb/jvarkit/dict/OrderChecker.java
@@ -34,6 +34,11 @@ import htsjdk.samtools.SAMSequenceDictionary;
 import htsjdk.samtools.SAMSequenceRecord;
 import htsjdk.samtools.util.Locatable;
 
+/**
+ *
+ * Check Locatable are following a goog order (chrom/start)
+ * 
+ */
 public class OrderChecker<T extends Locatable> implements UnaryOperator<T>{
 private final Set<String> contig_seen;
 private final SAMSequenceDictionary dict;
@@ -41,14 +46,21 @@ private final boolean strictDictOrder;
 private String prev_contig = null;
 private int prev_start = -1;
 
+/** doesn't care about a dictionay, check items are ordered and chroms don't appear twice */
 public OrderChecker() {
 	this(null,false);
 }
 
+/** check items are sorted and chromosomes appear in the same order than in the dict */
 public OrderChecker(final SAMSequenceDictionary dict) {
 	this(dict,true);
-}
+	}
 
+/**
+ * 
+ * @param dict the sequence dictionary
+ * @param strictDictOrder if true, chromosomes should appear in the very same order than in dict
+ */
 public OrderChecker(final SAMSequenceDictionary dict,boolean strictDictOrder) {
 	this.dict = (dict.isEmpty()?null:dict);
 	this.strictDictOrder = strictDictOrder;
@@ -102,8 +114,8 @@ public T apply(final T t) {
 				}
 			this.contig_seen.add(contig);
 			}
-		this.prev_contig = t.getContig();
-		this.prev_start = t.getStart();		
+		this.prev_contig = contig;
+		this.prev_start = t.getStart();
 		}
 	else if (this.prev_start > t.getStart()) {
 		throwError(t);
--- src/main/java/com/github/lindenb/jvarkit/tools/roh/VcfROH.java
+++ src/main/java/com/github/lindenb/jvarkit/tools/roh/VcfROH.java
@@ -33,8 +33,10 @@ import java.util.List;
 import java.util.stream.Collectors;
 
 import com.beust.jcommander.Parameter;
+import com.github.lindenb.jvarkit.bed.BedLineReader;
 import com.github.lindenb.jvarkit.dict.OrderChecker;
 import com.github.lindenb.jvarkit.lang.StringUtils;
+import com.github.lindenb.jvarkit.samtools.util.SimpleInterval;
 import com.github.lindenb.jvarkit.util.Counter;
 import com.github.lindenb.jvarkit.util.JVarkitVersion;
 import com.github.lindenb.jvarkit.util.bio.DistanceParser;
@@ -48,7 +50,7 @@ import htsjdk.samtools.SAMFileHeader;
 import htsjdk.samtools.SAMReadGroupRecord;
 import htsjdk.samtools.SAMSequenceDictionary;
 import htsjdk.samtools.SAMTextHeaderCodec;
-import htsjdk.samtools.util.CoordMath;
+import htsjdk.samtools.util.IntervalTreeMap;
 import htsjdk.variant.variantcontext.Allele;
 import htsjdk.variant.variantcontext.Genotype;
 import htsjdk.variant.variantcontext.GenotypeType;
@@ -93,6 +95,8 @@ public class VcfROH extends Launcher
 	private int min_block_length = 1_000;
 	@Parameter(names={"--min-score"},description="Minimum block score.")
 	private double min_block_score = 0.0;
+	@Parameter(names={"--regions-file"},description="Limit to intervals overlapping that BED file.")
+	private Path intervalBedPath = null;
 
 
 	private class Sample {
@@ -101,6 +105,7 @@ public class VcfROH extends Launcher
 		String prev_contig = null;
 		int start = -1;
 		int end  = -1;
+		double max_score=0;
 		double score=0;
 		int count = 0;
 		Counter<GenotypeType> countTypes = new Counter<>();
@@ -108,23 +113,25 @@ public class VcfROH extends Launcher
 			this.name= sn;
 			this.index = index;
 			}
-		private void dump(final PrintWriter w) {
+		private void dump(final PrintWriter w,final IntervalTreeMap<?> treemap) {
 			if(StringUtils.isBlank(this.prev_contig)) return;
 			if(this.start>=this.end) return;
 			
-			if(this.score < min_block_score) return;
+			if(this.max_score < min_block_score) return;
 			if(this.count < min_variants_count) return;
-			final int length = CoordMath.getLength(this.start, this.end);
-			if( length < min_block_length) return;
+			final SimpleInterval r = new SimpleInterval(this.prev_contig, this.start,this.end);
+			if( r.getLengthOnReference() < min_block_length) return;
+			if(treemap!=null && !treemap.containsOverlapping(r)) {
+				return;
+				}
 			
-			
-			w.print(this.prev_contig);
+			w.print(r.getContig());
 			w.print("\t");
-			w.print(this.start + (output_as_bed?-1:0));
+			w.print(r.getStart() + (output_as_bed?-1:0));
 			w.print("\t");
-			w.print(this.end);
+			w.print(r.getEnd());
 			w.print("\t");
-			w.print(length);
+			w.print(r.getLengthOnReference());
 			w.print("\t");
 			w.print(this.name);
 			w.print("\t");
@@ -132,11 +139,11 @@ public class VcfROH extends Launcher
 			w.print("\t");
 			w.print(Arrays.stream(GenotypeType.values()).filter(GT->countTypes.count(GT)>0L).map(GT->GT.name()+":"+countTypes.count(GT)).collect(Collectors.joining(";")));
 			w.print("\t");
-			w.print(this.score);
+			w.print(this.max_score);
 			w.println();
 		}
 		
-		void visit(final PrintWriter w,final VariantContext ctx) {
+		void visit(final PrintWriter w,final VariantContext ctx,final IntervalTreeMap<Boolean> treemap) {
 			final Genotype gt = ctx.getGenotype(this.index);
 			final boolean last_is_hom;
 			// haploid
@@ -165,20 +172,22 @@ public class VcfROH extends Launcher
 				}
 			
 			if(!ctx.getContig().equals(this.prev_contig) || this.score+dx <= 0) {
-				dump(w);
+				dump(w,treemap);
 				this.prev_contig = ctx.getContig();
 				this.start = ctx.getStart();
 				this.score  = 0;
+				this.max_score = 0;
 				this.count  = 0;
 				this.countTypes = new Counter<>();
 				}
 			this.end = ctx.getEnd();
 			this.score += dx;
+			this.max_score = Math.max(this.score,this.max_score);
 			this.count++;
 			this.countTypes.incr(gt.getType());
 			}
-		void finish(final PrintWriter w) {
-			dump(w);
+		void finish(final PrintWriter w,final IntervalTreeMap<Boolean> treemap) {
+			dump(w,treemap);
 		}
 	}
 	
@@ -200,6 +209,15 @@ public class VcfROH extends Launcher
 					samples.add(sample);
 				}
 				
+				final IntervalTreeMap<Boolean> treemap;
+				if (this.intervalBedPath!=null) {
+					try(BedLineReader br = new BedLineReader(this.intervalBedPath)) {
+						treemap = br.toIntervalTreeMap(B->Boolean.TRUE);
+					}
+				} else {
+					treemap = null;
+				}
+				
 				try(PrintWriter w =  super.openPathOrStdoutAsPrintWriter(this.output)) {
 					if(!this.output_as_bed) {
 						final SAMFileHeader samHeader = new SAMFileHeader(dict);
@@ -215,9 +233,9 @@ public class VcfROH extends Launcher
 						}
 					while(iter.hasNext()) {
 						final VariantContext ctx = checker.apply(iter.next());
-						for(Sample sample:samples) sample.visit(w,ctx);
+						for(Sample sample:samples) sample.visit(w,ctx,treemap);
 						}
-					for(Sample sample:samples) sample.finish(w);
+					for(Sample sample:samples) sample.finish(w,treemap);
 					w.flush();
 					}
 
