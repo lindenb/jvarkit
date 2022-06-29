9453396c4a18245d8e005a2463389f28801dbc74
Pierre Lindenbaum <plindenbaum@yahoo.fr>
2022-06-29 12:19:12 +0200

    gtf2bed

---
+++ src/main/java/com/github/lindenb/jvarkit/tools/gtf/GtfToBed.java
@@ -0,0 +1,271 @@
+/*
+The MIT License (MIT)
+
+Copyright (c) 2022 Pierre Lindenbaum
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
+
+*/
+package com.github.lindenb.jvarkit.tools.gtf;
+
+import java.io.BufferedReader;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.LinkedHashSet;
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
+
+import com.beust.jcommander.Parameter;
+import com.github.lindenb.jvarkit.io.IOUtils;
+import com.github.lindenb.jvarkit.lang.CharSplitter;
+import com.github.lindenb.jvarkit.lang.StringUtils;
+import com.github.lindenb.jvarkit.util.bio.SequenceDictionaryUtils;
+import com.github.lindenb.jvarkit.util.bio.fasta.ContigNameConverter;
+import com.github.lindenb.jvarkit.util.jcommander.Launcher;
+import com.github.lindenb.jvarkit.util.jcommander.Program;
+import com.github.lindenb.jvarkit.util.log.Logger;
+
+/**
+
+BEGIN_DOC
+
+```
+```
+
+END_DOC
+*/
+@Program(
+		name="gtf2bed",
+		description="Convert GTF/GFF3 to BED.",
+		creationDate="20220629",
+		modificationDate="20220629",
+		keywords= {"gtf","gff","gff3","bed"}
+		)
+public class GtfToBed
+	extends Launcher {
+	private static final Logger LOG = Logger.build(GtfToBed.class).make();
+	
+	private final static String MY_COLS="gtf.source,gtf.feature,gtf.score,gtf.strand,gtf.frame";
+
+	private final static String GTF_COLS = "ccds_id,exon_id,exon_number,exon_version,gene_biotype,gene_id,gene_name,gene_source,"
+		+ "gene_version,havana_transcript,havana_transcript_version,protein_id,protein_version,tag,"
+		+ "transcript_biotype,transcript_id,transcript_name,transcript_source,transcript_version";
+	private final static String GFF_COLS= 
+		"Alias,biotype,ccdsid,constitutive,description,ensembl_end_phase,"
+			+ "ensembl_phase,exon_id,external_name,gene_id,havana_transcript,"
+			+ "havana_version,ID,logic_name,Name,Parent,protein_id,rank,tag,"
+			+ "transcript_id,version"
+			;
+	private final String ALL_COLS = MY_COLS+","+GTF_COLS+","+GFF_COLS;
+
+	
+	@Parameter(names={"-o","--output"},description= OPT_OUPUT_FILE_OR_STDOUT)
+	private Path outputFile = null;
+	@Parameter(names={"-R","--reference"},description= INDEXED_FASTA_REFERENCE_DESCRIPTION)
+	private Path faidx = null;
+
+	@Parameter(names={"-c","--columns"},description= "comma separated columns to be displayed")
+	private String columnStr=ALL_COLS;
+
+	private	final Set<String> columns = new LinkedHashSet<>();
+	private enum Type{ undefined,gtf,gff3}
+	private Type gtype = Type.undefined;
+
+	
+
+	private void put(final Map<String,String> map,final String key,final String value) {
+		if(key.isEmpty()) throw new IllegalArgumentException("key is empty ");
+		if(!this.columns.contains(key)) return;
+		if(map.put(key, value)!=null) {
+			throw new IllegalArgumentException("duplicate key "+key+" in "+map);
+			}
+		}
+
+	private Map<String,String> attributes(final String s) {
+		final Map<String,String> map= new HashMap<>();
+		if(s.equals(".")) return map;
+		int i=0;
+		
+		while(i< s.length()) {
+			/* skip ws */
+			while(i< s.length() && Character.isWhitespace(s.charAt(i))) i++;
+			final StringBuilder key=new StringBuilder();
+			while(i< s.length()) {
+				if(this.gtype.equals(Type.undefined)) {
+					if(Character.isWhitespace(s.charAt(i))) {
+						this.gtype = Type.gtf;
+						i++;
+						break;
+						}
+					else if(s.charAt(i)=='=') {
+						this.gtype = Type.gff3;
+						i++;
+						break;
+						}
+					}
+				if(this.gtype.equals(Type.gtf) && Character.isWhitespace(s.charAt(i))) {
+					i++;
+					break;
+					}
+				else if(this.gtype.equals(Type.gff3) && s.charAt(i)=='=') {
+					i++;
+					break;
+					}
+				key.append(s.charAt(i));
+				i++;
+				}
+			if(this.gtype.equals(Type.undefined)) throw new IllegalArgumentException("undefined gtf type with " + s);
+			/* skip ws */
+			while(i< s.length() && Character.isWhitespace(s.charAt(i))) i++;
+			if(i>=s.length()) throw new IllegalArgumentException("END of string after "+s.substring(0,i));
+			
+
+			boolean got_quote=false;
+			if(this.gtype.equals(Type.gtf) && s.charAt(i)=='\"') {
+				got_quote=true;
+				i++;
+				}
+			
+			
+			final StringBuilder value=new StringBuilder();
+			while(i< s.length() ) {
+				if(got_quote && this.gtype.equals(Type.gtf) && s.charAt(i)=='\"') break;
+				else if(!got_quote && this.gtype.equals(Type.gtf) && s.charAt(i)==';') break;
+				else if(!got_quote && this.gtype.equals(Type.gtf) && Character.isWhitespace(s.charAt(i))) break;
+				else if(this.gtype.equals(Type.gff3) && s.charAt(i)==';') break;
+				
+				if(s.charAt(i)=='\\') {
+					if(i+1>=s.length()) throw new IllegalArgumentException("unknown escape sequence after "+s.substring(0,i));
+					i++;
+					switch(s.charAt(i)) {
+						case '"': value.append("\"");break;
+						case '\'': value.append("\'");break;
+						case '\\': value.append("\\");break;
+						case 't': value.append("\t");break;
+						case 'n': value.append("\n");break;
+						default: throw new IllegalArgumentException("unknown escape sequence after "+s.substring(0,i));
+						}
+					}
+				else
+					{
+					value.append(s.charAt(i));
+					}
+				i++;
+				}
+			if(got_quote && this.gtype.equals(Type.gtf)) {
+				if(i>=s.length()) throw new IllegalArgumentException("expected quote after "+s.substring(0,i));
+				if(this.gtype.equals(Type.gtf) &&  s.charAt(i)!='\"')  throw new IllegalArgumentException("expected quote after "+s.substring(0,i));
+				i++;
+				}
+			this.put(map,key.toString(), value.toString());
+			
+			/* skip ws */
+			while(i< s.length() && Character.isWhitespace(s.charAt(i))) i++;
+			if(i<s.length()) {
+				if(s.charAt(i)!=';')  throw new IllegalArgumentException("expected semicolon after "+s.substring(0,i));
+				i++;
+				}
+			/* skip ws */
+			while(i< s.length() && Character.isWhitespace(s.charAt(i))) i++;
+			}
+		return map;
+		}
+
+	
+	@Override
+	public int doWork(final List<String> args)  {
+		try {
+			final String[] fixed_cols_prefixes=new String[]{"gtf.","gff.","gff3."};
+			final ContigNameConverter contigNameConverter =  
+					this.faidx==null?
+					ContigNameConverter.getIdentity():
+					ContigNameConverter.fromOneDictionary(SequenceDictionaryUtils.extractRequired(this.faidx))
+					;
+			this.columns.clear();
+			this.columns.addAll(Arrays.asList(this.columnStr.split("[, \t;\\|]+")));
+			this.columns.remove("");
+
+			final String input = oneFileOrNull(args);
+			try(InputStream in1 = input==null?System.in:Files.newInputStream(Paths.get(input))) {
+				final InputStream in = IOUtils.uncompress(in1);
+				final BufferedReader br = new BufferedReader(new InputStreamReader(in, "UTF-8"));
+				try(PrintWriter out = super.openPathOrStdoutAsPrintWriter(this.outputFile)) {
+				
+					out.println("#chrom\tstart\tend"+(this.columns.isEmpty()?"":"\t")+String.join("\t",this.columns));
+
+					String line;
+					while((line=br.readLine())!=null) {
+						if(line.startsWith("#") || StringUtils.isBlank(line)) continue;
+						final String[] tokens = CharSplitter.TAB.split(line);
+						if(tokens.length!=9) throw new IllegalArgumentException("expected 9 tokens in "+String.join("<\\t>", tokens));
+						
+						final String ctg = contigNameConverter.apply(tokens[0]);
+						if(StringUtils.isBlank(ctg)) continue;
+						
+						out.print(ctg);
+						out.print('\t');
+						out.print(Integer.parseInt(tokens[3])-1);
+						out.print('\t');
+						out.print(Integer.parseInt(tokens[4]));
+						
+						final Map<String,String> atts = attributes(tokens[8]);
+
+						for(String pfx:fixed_cols_prefixes) {
+							put(atts,pfx+"source",tokens[1]);
+							put(atts,pfx+"feature",tokens[2]);
+							put(atts,pfx+"score",tokens[5]);
+							put(atts,pfx+"strand",tokens[6]);
+							put(atts,pfx+"frame",tokens[7]);
+						}
+						
+						for(final String field:this.columns) {
+							out.print('\t');
+							out.print(atts.getOrDefault(field,"."));
+							}
+						out.println();
+						}
+					out.flush();
+					}
+				br.close();
+				in.close();
+				}
+		
+			return 0;
+			}
+		catch(final Throwable err) {
+			LOG.error(err);
+			return -1;
+			}
+		finally
+			{
+			}
+		}
+	
+	public static void main(final String[] args)
+		{
+		new GtfToBed().instanceMainWithExit(args);
+		}
+	}
