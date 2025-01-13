87067a32d19f758d0acab870edb9ebc108f3da0a
Pierre Lindenbaum <plindenbaum@yahoo.fr>
2025-01-13 16:46:34 +0100

    copyright 2025

--- src/main/java/com/github/lindenb/jvarkit/tools/misc/BamToSql.java
+++ src/main/java/com/github/lindenb/jvarkit/tools/misc/BamToSql.java
@@ -2,7 +2,7 @@
 /*
 The MIT License (MIT)
 
-Copyright (c) 2024 Pierre Lindenbaum
+Copyright (c) 2025 Pierre Lindenbaum
 
 Permission is hereby granted, free of charge, to any person obtaining a copy
 of this software and associated documentation files (the "Software"), to deal
--- src/main/java/com/github/lindenb/jvarkit/tools/misc/FastqRevComp.java
+++ src/main/java/com/github/lindenb/jvarkit/tools/misc/FastqRevComp.java
@@ -1,7 +1,7 @@
 /*
 The MIT License (MIT)
 
-Copyright (c) 2024 Pierre Lindenbaum PhD.
+Copyright (c) 2025 Pierre Lindenbaum PhD.
 
 Permission is hereby granted, free of charge, to any person obtaining a copy
 of this software and associated documentation files (the "Software"), to deal
